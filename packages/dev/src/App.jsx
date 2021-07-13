a722ba932ba3bece401d88e42c5dbfb1eefa813c
Zach Kelling <z@zeekay.io>
2021-07-13 18:37:52 -0400

    Hardcode contract addresses for now

--- packages/dev/src/App.jsx
+++ packages/dev/src/App.jsx
@@ -19,6 +19,7 @@ import {
   useOnBlock,
   useUserSigner,
 } from "./hooks";
+
 // import Hints from "./Hints";
 import { Subgraph } from "./views";
 
@@ -376,35 +377,29 @@ function App(props) {
 
         <Switch>
           <Route exact path="/">
-            // The <Contract/> component will automatically parse your ABI
-            // and give you a form to interact with it locally
             <Contract
               name="ZooToken"
               signer={userSigner}
               provider={localProvider}
-              address={address}
+              address={"0x5FbDB2315678afecb367f032d93F642f64180aa3"}
               blockExplorer={blockExplorer}
             />
           </Route>
           <Route exact path="/crypto-animal-admin">
-            // The <Contract/> component will automatically parse your ABI
-            // and give you a form to interact with it locally
             <Contract
               name="CryptoAnimalAdmin"
               signer={userSigner}
               provider={localProvider}
-              address={address}
+              address={"0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"}
               blockExplorer={blockExplorer}
             />
           </Route>
           <Route exact path="/crypto-animal">
-            // The <Contract/> component will automatically parse your ABI
-            // and give you a form to interact with it locally
             <Contract
               name="CryptoAnimal"
               signer={userSigner}
               provider={localProvider}
-              address={address}
+              address={"0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"}
               blockExplorer={blockExplorer}
             />
           </Route>
