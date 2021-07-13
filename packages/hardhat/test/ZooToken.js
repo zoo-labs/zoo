65411b27374dba9b0523592b0922f3e211cab5bd
Zach Kelling <z@zeekay.io>
2021-07-12 23:15:33 -0400

    Fix token test, upgrade hardhat

---
+++ packages/hardhat/test/ZooToken.js
@@ -0,0 +1,27 @@
+const { ethers } = require('hardhat')
+const { use, expect } = require('chai')
+const { solidity } = require('ethereum-waffle')
+
+use(solidity)
+
+describe('ZooToken', function () {
+  let token
+
+  it('should deploy ZooToken contract', async function () {
+    const [signer] = await ethers.getSigners()
+    const ZooToken = await ethers.getContractFactory('ZooToken')
+    token = await ZooToken.deploy('ZooToken', 'ZOO', 18, 2000000000, signer.address)
+  })
+
+  it('should have correct name and symbol and decimal', async () => {
+    const [signer] = await ethers.getSigners()
+    const ZooToken = await ethers.getContractFactory('ZooToken')
+    token = await ZooToken.deploy('ZooToken', 'ZOO', 18, 2000000000, signer.address)
+    const name = await token.name()
+    const symbol = await token.symbol()
+    const decimals = await token.decimals()
+    expect(name.valueOf()).to.eq('ZooToken')
+    expect(symbol.valueOf()).to.eq('ZOO')
+    expect(decimals.valueOf()).to.eq(18)
+  })
+})
