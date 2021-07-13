3ce7427848a2e35e737e47d563224784cc505c79
Zach Kelling <z@zeekay.io>
2021-07-13 11:10:57 -0400

    Add crypto animal deploy script

---
+++ packages/contracts/deploy/00_deploy_token.ts
@@ -0,0 +1,33 @@
+// deploy/00_deploy_token.js
+
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts()
+  const { deploy } = hre.deployments
+  const useProxy = !hre.network.live
+
+  // Proxy only in non-live network (localhost and hardhat network) enabling
+  // HCR (Hot Contract Replacement) in live network, proxy is disabled and
+  // constructor is invoked
+  await deploy('ZooToken', {
+    from: deployer,
+    args: ['ZooToken', 'ZOO', 18, 2000000000, deployer],
+    log: true,
+    proxy: useProxy && 'postUpgrade',
+  })
+
+  return !useProxy // When live network, record the script as executed to prevent rexecution
+}
+
+export default func
+func.id = 'deploy_zoo_token'
+func.tags = ['ZooToken'];
+
+// Tenderly verification
+// let verification = await tenderly.verify({
+//   name: contractName,
+//   address: contractAddress,
+//   network: targetNetwork,
+// })
---
+++ packages/contracts/deploy/02_deploy_crypto_animal.ts
@@ -0,0 +1,26 @@
+// deploy/02_deploy_cryptoanimal.js
+
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts()
+  const { deploy } = hre.deployments
+  const useProxy = !hre.network.live
+
+  // Proxy only in non-live network (localhost and hardhat network) enabling
+  // HCR (Hot Contract Replacement) in live network, proxy is disabled and
+  // constructor is invoked
+  await deploy('CryptoAnimal', {
+    from: deployer,
+    args: [],
+    log: true,
+    proxy: useProxy && 'postUpgrade',
+  })
+
+  return !useProxy // When live network, record the script as executed to prevent rexecution
+}
+
+export default func
+func.id = 'deploy_crypto_animal' // ID required to prevent reexecution
+func.tags = ['CryptoAnimal']
