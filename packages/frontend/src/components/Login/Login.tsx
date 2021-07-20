51503caf07a8a77cecdc937a85bec3d6e088f435
Ari Lerner <me@ari.io>
2021-07-20 16:16:48 -0700

    Updated to demonstrate using ethers

--- packages/frontend/src/components/Login/Login.tsx
+++ packages/frontend/src/components/Login/Login.tsx
@@ -1,16 +1,9 @@
-import { AbstractConnector } from '@web3-react/abstract-connector'
-import { useWeb3React } from '@web3-react/core'
-import { ConnectorNames, connectorsByName } from '../../connectors'
-import { useAppState } from '../../state'
+// const iconsMap = {
+//   [ConnectorNames.Metamask]: 'https://docs.metamask.io/metamask-fox.svg',
+//   [ConnectorNames.WalletConnect]: 'https://walletconnect.org/walletconnect-logo.svg',
+// }
 
-const iconsMap = {
-  [ConnectorNames.Metamask]: 'https://docs.metamask.io/metamask-fox.svg',
-  [ConnectorNames.WalletConnect]: 'https://walletconnect.org/walletconnect-logo.svg',
-}
-
-const Login = () => {
-  const { activatingConnector, setActivatingConnector } = useAppState()
-  const { connector, activate } = useWeb3React()
+export const Login = () => {
   return (
    <div>
      Flex this shit
@@ -18,4 +11,3 @@ const Login = () => {
   )
 }
 
-export { Login }
