981de9fdb33ebaf1b6bdf8ec2ab52796cf79cd4d
bmalkmus <bmalkmus23@gmail.com>
2021-07-22 12:54:55 -0700

    Login skelton created

--- packages/newFront/src/views/Account.tsx
+++ packages/newFront/src/views/Account.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-
-
 export default function Account() {
     return (
         <div>
--- packages/newFront/src/views/Login.tsx
+++ packages/newFront/src/views/Login.tsx
@@ -1,9 +1,37 @@
+import { useWeb3React } from '@web3-react/core'
+import { useWalletModal } from 'components'
+import useAuth from 'hooks/useAuth'
 import React from 'react'
+import styled from "styled-components"
+import { Login } from '../components/WalletModal/types'
 
-export default function Login() {
+interface Props {
+  account?: string
+  login: Login
+  logout: () => void
+}
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+    height: 100vh;
+`
+const Button = styled.button`
+`
+
+export default function LoginMain() {
+
+    const { account, chainId } = useWeb3React()
+    const { login, logout } = useAuth()
+
+    const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(login, logout, account, history)
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <img alt="CryptoZoo Logo" src="CryptoZooLogoFull.jpg"/>
+            <Button onClick={()=>onPresentConnectModal()}>
+                Log In
+            </Button>
+        </Container>
     )
 }
