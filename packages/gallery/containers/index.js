93c4a083563f8ee9aedff434b9d1f0e641e8dfdb
Zach Kelling <z@zeekay.io>
2021-07-21 20:18:03 -0400

    Remove extraneous packages; migrated to kitchen-sink branch

--- packages/gallery/containers/index.js
+++
@@ -1,9 +0,0 @@
-import web3 from "@containers/web3"; // Web3 provider
-
-// Global state provider
-export default function GlobalProvider({ children }) {
-  return <web3.Provider>{children}</web3.Provider>;
-}
-
-// Export individual containers
-export { web3 };
