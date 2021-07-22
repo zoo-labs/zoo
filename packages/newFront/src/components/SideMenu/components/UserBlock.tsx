c0d5c4068182fec264ff1f4d184b67ba7827bb81
bmalkmus <bmalkmus23@gmail.com>
2021-07-22 11:35:14 -0700

    React base app moved to different workspace

---
+++ packages/newFront/src/components/SideMenu/components/UserBlock.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import Button from '../../../components/Button/Button'
+import { useWalletModal } from '../../WalletModal'
+import { Login } from '../../WalletModal/types'
+
+interface Props {
+  account?: string
+  login: Login
+  logout: () => void
+}
+
+const UserBlock: React.FC<Props> = ({ account, login, logout }) => {
+  const history = useHistory()
+  const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(login, logout, account, history)
+  const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null
+  return (
+    <div>
+      {account ? (
+        <Button
+          scale="sm"
+          variant="tertiary"
+          onClick={() => {
+            onPresentAccountModal()
+          }}
+        >
+          {accountEllipsis}
+        </Button>
+      ) : (
+        <Button
+          scale="sm"
+          onClick={() => {
+            onPresentConnectModal()
+          }}
+        >
+          Connect
+        </Button>
+      )}
+    </div>
+  )
+}
+
+export default React.memo(UserBlock, (prevProps, nextProps) => prevProps.account === nextProps.account)
