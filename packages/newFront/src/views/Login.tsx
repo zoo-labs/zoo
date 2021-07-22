b7a3e8ac7045301fd50f1702ade70e43b3efb99a
yuki <uearngold@outlook.com>
2021-07-22 19:21:12 -0400

    fixed font style and align center

--- packages/newFront/src/views/Login.tsx
+++ packages/newFront/src/views/Login.tsx
@@ -1,6 +1,7 @@
 import { useWeb3React } from '@web3-react/core'
 import { useWalletModal } from 'components'
 import useAuth from 'hooks/useAuth'
+import { url } from 'inspector'
 import React from 'react'
 import styled from "styled-components"
 import { Login } from '../components/WalletModal/types'
@@ -14,8 +15,8 @@ interface Props {
 const Container = styled.div`
     display: flex;
     flex-direction: column;
-    background-color: black;
-    height: 100vh;
+    background-color: #030303;
+    height: calc(100vh - 63px);
 `
 const Button = styled.button`
 `
@@ -28,9 +29,10 @@ export default function LoginMain() {
     const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(login, logout, account, history)
     return (
         <Container>
-            <img alt="CryptoZoo Logo" src="CryptoZooLogoFull.jpg"/>
-            <Button onClick={()=>onPresentConnectModal()}>
-                Log In
+            <img alt="CryptoZoo Logo" src="CryptoZooLogoFull.jpg" style={{width: '300px', margin: 'auto auto 30px auto'}}/>
+            <Button onClick={()=>onPresentConnectModal()} style={{width: '200px', margin: '0px auto auto auto', backgroundColor: '#030303', border: '1px solid #ffffff', padding: '10px'}}>
+                <p style={{color: 'white', fontFamily: "'Mister Pixel 16 pt - Small Caps'", fontSize: 16}}>LOGIN WITH</p>
+                <p style={{color: 'white', fontFamily: "'Mister Pixel 16 pt - Small Caps'", fontSize: 16}}>METAMASK</p>
             </Button>
         </Container>
     )
