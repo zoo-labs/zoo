7bad14be2a49d317ca17c8c225665cea05d8576f
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2021-07-22 18:28:21 -0400

    Signers get 10k ZOO on deployment

--- packages/contracts/deploy/06_deploy_zoo_faucet.ts
+++ packages/contracts/deploy/06_deploy_zoo_faucet.ts
@@ -2,12 +2,17 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { getDeployerAddress } from '../lib/deploy_helper'
+import { ethers } from 'hardhat'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts } = hre
     const { deploy } = deployments
     const { deployer } = await getNamedAccounts()
     const OWNER_ADDRESS = await getDeployerAddress(hre)
+
+    let zooToken: any;
+
+    const signers = await ethers.getSigners()
 
     const useProxy = !hre.network.live
 
@@ -23,7 +28,36 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         // proxy: useProxy && 'postUpgrade',
     })
 
+    // Gets the ZooToken interface
+    const zooTokenFactory = await ethers.getContractFactory('ZooToken');
+
+    // Attaches the deployed ZooToken address to this instance of zooToken
+    zooToken = zooTokenFactory.attach(tokenAddress);
+
+    // Gets the ZooFaucet interface
+    const faucetFactory = await ethers.getContractFactory('ZooFaucet');
+
+    // Gets the deployed ZooFaucet address
+    const faucetAddress = (await deployments.get('ZooFaucet')).address;
+
+    // Attaches the deployed ZooFaucet address to this instance of faucet
+    const faucet = faucetFactory.attach(faucetAddress)
+
+    // Mints 100 million ZOO and allocates it to ZooFaucet
+    await zooToken.mint(faucet.address, 100000000);
+
+    for (var i = 0; i < signers.length; i++) {
+
+        // The 20 signer wallets get 10K ZOO on deployment
+        await faucet.buyZoo(
+            signers[i].address,
+            10 // Rate: 1:1000 (10 * 1000) = 10000
+        );
+    }
+
     return !useProxy // When live network, record the script as executed to prevent rexecution
+
+
 }
 
 export default func
