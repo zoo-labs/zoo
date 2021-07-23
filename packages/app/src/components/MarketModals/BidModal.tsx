0f7cf0107aa05c38e5322074419beb852b4c5881
bmalkmus <bmalkmus23@gmail.com>
2021-07-23 09:46:16 -0700

    updating marketplace

---
+++ packages/app/src/components/MarketModals/BidModal.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Modal as Existing, Text as Standard} from 'components'
+import BorderButton from 'components/Button/BorderButton'
+import { useModal } from "components/Modal";
+import Confirmation from "./ConfirmationModal"
+
+
+interface Props {
+    onDismiss?: () => void
+    item: any
+    Moralis: any
+}
+
+const Modal = styled(Existing)`
+    background-color: black;
+`
+
+const BidInput = styled.input.attrs({ 
+    type: 'number',
+    min: 1,
+    defaultValue: 1
+  })`
+    width: 90%;
+    margin: auto;
+  `
+
+const Text = styled(Standard)`
+
+`
+
+const confirmBuy = () => {
+    alert("buy")
+}
+const confirmBid = () => {
+    alert("bid")
+}
+
+const BidModal: React.FC<Props> = ({onDismiss = () => null, item, Moralis}) => {
+    const input = React.useRef(1)
+    const [onConfirmBuy] = useModal(
+        <Confirmation
+            confirmation = {confirmBuy}
+            onDismiss={()=>null}
+            action = "Buy"
+            name= {item.Name}
+            amount = {item.BuyNow}
+        />
+    )
+    const [onConfirmBid] = useModal(
+        <Confirmation
+            confirmation = {confirmBid}
+            onDismiss={()=>null}
+            action = "Bid"
+            name= {item.Name}
+            amount = {item.CurrentBid}
+        />
+    )
+
+
+    return (
+        <>
+        <Modal title = {`${item.Name}`} onDismiss={onDismiss}>
+            <Text>{`Current Bid: ${item.CurrentBid}`}</Text>
+            <Text>{`Buy Now: ${item.BuyNow}`}</Text>
+            <BidInput type="number" />
+            <BorderButton onClick={()=>onConfirmBuy()}>Buy Now</BorderButton>
+            <BorderButton onClick={()=>onConfirmBid()}>Bid</BorderButton>
+        </Modal>
+        </>
+    )
+}
+
+export default BidModal
