5fcaaf224d4e378e442725fda59c573fdf0b424c
jungsnn <60561405+jungsNN@users.noreply.github.com>
2021-07-23 16:44:55 -0400

    adding yield modal

--- packages/app/src/components/MarketModals/BidModal.tsx
+++ packages/app/src/components/MarketModals/BidModal.tsx
@@ -68,7 +68,7 @@ const BidModal: React.FC<Props> = ({onDismiss = () => null, item, Moralis}) => {
 
     return (
         <>
-        <Modal title = {`${item.Name}`} onDismiss={onDismiss}>
+        <Modal title = {`${item.Name}`} onDismiss={onDismiss} >
             <Text>{`Current Bid: ${item.CurrentBid}`}</Text>
             <Text>{`Buy Now: ${item.BuyNow}`}</Text>
             <BidInput type="number" onChange = {changed()} defaultValue={value}/>
