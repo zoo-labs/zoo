f9e159eefd7f31b3f8298aa09c03dc9d0e9e2d2a
bmalkmus <bmalkmus23@gmail.com>
2021-07-23 12:23:05 -0700

    live share saving

--- packages/app/src/views/MyZoo.tsx
+++ packages/app/src/views/MyZoo.tsx
@@ -64,22 +64,67 @@ const EggMarketplace: React.FC = () => {
   const chainIdSet = chainId === undefined ? "1" : String(chainId);
 
   // const allEggs = useSelector<AppState, AppState['cryptozoo']>((state) => state.cryptozoo.allEggs)
-  const allEggs = {
-    "3BCR3nl2wyq95D1xhciAndNT": {
-      currentBid: 500,
-      born: "08/06/2021",
-      rarity: "Legendary",
-      name: "Red Panda",
-      updatedAt: "23 July 2021 at 13:59:59 UTC",
-      supply: 300,
-      buyNow: 1000,
-      imageURL:
-        "https://i2.wp.com/bestlifeonline.com/wp-content/uploads/2018/10/red-panda-raising-fist.jpg?resize=640%2C360&ssl=1",
-      animalID: 4,
-      createdAt: "23 July 2021 at 13:56:24 UTC",
-      startBid: 500,
-    },
-  };
+
+  const allAnimals = {
+      1: {
+         tokenId: "1",
+         name: "Red Panda",
+         description: "Mystery",
+         yield: "543",
+         boost: "5678",
+         rarity: "Legendary",
+         dob: "1627064176",
+         imageUrl:
+            "https://i2.wp.com/bestlifeonline.com/wp-content/uploads/2018/10/red-panda-raising-fist.jpg?resize=640%2C360&ssl=1",
+      },
+      2: {
+         tokenId: "2",
+         name: "Suzanne",
+         description: "LOL",
+         yield: "4223",
+         boost: "2",
+         rarity: "Rare",
+         dob: "1627064176",
+         imageUrl: "https://ichef.bbci.co.uk/images/ic/1200x675/p02k8mcv.jpg",
+      },
+      3: {
+         tokenId: "3",
+         name: "Cool Doggo",
+         description: "WOOF wO0F",
+         yield: "321",
+         boost: "2",
+         rarity: "Uncommon",
+         dob: "1627064176",
+         imageUrl:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIuXjTOdhD--589Qjr310qX4IgAZrz_4-RAw&usqp=CAU",
+      },
+      4: {
+         tokenId: "4",
+         name: "Seal",
+         description: "BARK",
+         yield: "31",
+         boost: "22",
+         rarity: "Common",
+         dob: "1627064176",
+         imageUrl:
+            "https://sites.psu.edu/siowfa16/files/2016/09/baby-seal-29vsgyf.jpg",
+      },
+   }
+
+   const allEggs = {
+      5: {
+         tokenId: "5",
+         parent1: "1",
+         parent2: "2",
+         basic: true
+      },
+      6: {
+         tokenId: "6",
+         parent1: "3",
+         parent2: "4",
+         basic: false
+      },
+    }
 
   useEffect(() => {
     const showMoreData = (entries) => {
@@ -101,17 +146,18 @@ const EggMarketplace: React.FC = () => {
   const shownData = (data) => {
     return data.slice(0, numVisData);
   };
-  const renderContent = (): JSX.Element => {
-    const data = [];
+  const renderAnimals = (): JSX.Element => {
+    const animalData = [];
     // const updatedData = []
-    Object.values(allEggs).forEach((egg, index) => {
-      data.push({
+    Object.values(allAnimals).forEach((animal, index) => {
+      animalData.push({
         id: index,
-        ...egg,
-        name: egg.name.replace(/\u0000/g, ""),
+        ...animal,
+        name: animal.name.replace(/\u0000/g, ""),
       });
     });
-    empty = data.length === 0 && Object.keys(allEggs).length !== 0;
+    console.log(animalData)
+    empty = animalData.length === 0 && Object.keys(allAnimals).length !== 0;
     // Object.values(updatedTokens)
     //   .filter((tkn) => tkn.isToken)
     //   .forEach((token, ind) => {
@@ -120,31 +166,68 @@ const EggMarketplace: React.FC = () => {
     //     }
     //     updatedData.push({ id: ind, ...token })
     //   })
-    debugger; // eslint-disable-line no-debugger
+
 
     return (
       <FlexLayout>
         <Route exact path={`${path}`}>
-          {shownData(data).map((egg) => (
+          {shownData(animalData).map((animal) => (
             <Card>
               {/* <CardHeader>
                 <Heading color="cardLabel">
-                  {egg.name}
+                  {animal.name}
                 </Heading>
               </CardHeader> */}
-              <CardBody style={{backgroundImage: `url("${egg.imageURL}")`, backgroundSize: 'cover', backgroundPosition: 'center', height: 500}}>
-                <Heading mb="8px" style={{textShadow: '0px 2px rgba(0, 0, 0, 0.2)'}}>{egg.name}</Heading>
+              <CardBody style={{backgroundImage: `url("${animal.imageURL}")`, backgroundSize: 'cover', backgroundPosition: 'center', height: 500}}>
+                <Heading mb="8px" style={{textShadow: '0px 2px rgba(0, 0, 0, 0.2)'}}>{animal.name}</Heading>
               </CardBody>
-              <CardFooter>
                 <InfoBlock style={{textAlign: 'center'}}>
                   <Heading mb="8px" style={{textShadow: '0px 2px rgba(0, 0, 0, 0.2)'}}>{`BREED`}</Heading>
                 </InfoBlock>
-              </CardFooter>
             </Card>
           ))}
         </Route>
         <Route exact path={`${path}/history`}>
-          {shownData(data).map((egg) => (
+          {shownData(animalData).map((animal) => (
+            <Card
+            // key={JSON.stringify(token)}
+            />
+          ))}
+        </Route>
+      </FlexLayout>
+    );
+  };
+
+  const renderEggs = (): JSX.Element => {
+    const eggData = [];
+    // const updatedData = []
+    Object.values(allEggs).forEach((egg, index) => {
+      eggData.push({
+        id: index,
+        ...egg,
+        name: egg.basic ? "BASIC" : "HYBRID"
+      });
+    });
+    empty = eggData.length === 0 && Object.keys(allEggs).length !== 0;
+    const basicEggURL = 'static/images/basic.png'
+    const hybridEggURL = 'static/images/hybrid.png'
+
+    return (
+      <FlexLayout>
+        <Route exact path={`${path}`}>
+          {shownData(eggData).map((egg) => (
+            <Card style={{backgroundColor: '#000000'}}>
+              <CardBody style={{backgroundImage: `url("${egg.basic ? basicEggURL : hybridEggURL}")`, backgroundSize: 'cover', backgroundPosition: 'center', height: 250}}>
+                <Heading mb="8px" style={{textShadow: '0px 2px rgba(0, 0, 0, 0.2)'}}>{egg.name}</Heading>
+              </CardBody>
+              <InfoBlock style={{textAlign: 'center'}}>
+                <Heading mb="8px" style={{textShadow: '0px 2px rgba(0, 0, 0, 0.2)'}}>{`HATCH`}</Heading>
+              </InfoBlock>
+            </Card>
+          ))}
+        </Route>
+        <Route exact path={`${path}/history`}>
+          {shownData(eggData).map((egg) => (
             <Card
             // key={JSON.stringify(token)}
             />
@@ -157,7 +240,8 @@ const EggMarketplace: React.FC = () => {
   return (
     <div>
       <Page>
-        {renderContent()}
+        {renderEggs()}
+        {renderAnimals()}
         <IconCont ref={bottomRef}>
           {" "}
           {numVisData < Object.keys(allEggs).length ? (
