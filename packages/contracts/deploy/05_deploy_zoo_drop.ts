562dfca5725c39c109425b79ed17d18e986c4646
Jonathan Eid <jonathaneid98@gmail.com>
2021-07-24 02:25:59 -0400

    Zoodrop tests

--- packages/contracts/deploy/05_deploy_zoo_drop.ts
+++ packages/contracts/deploy/05_deploy_zoo_drop.ts
@@ -11,12 +11,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const useProxy = !hre.network.live
 
+  const mediaAddress = (await deployments.get('ZooMedia')).address
+
+  const tokenAddress = (await deployments.get('ZooToken')).address
   // Proxy only in non-live network (localhost and hardhat network) enabling
   // HCR (Hot Contract Replacement) in live network, proxy is disabled and
   // constructor is invoked
   await deploy('ZooDrop', {
     from: OWNER_ADDRESS,
-    args: [],
+    args: [tokenAddress, mediaAddress],
     log: true,
     // proxy: useProxy && 'postUpgrade',
   })
