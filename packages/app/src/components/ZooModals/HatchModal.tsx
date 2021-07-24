9fbfb89ddb92a0507610b642ceb088778c1a0a79
Michael Shen <michael.pocheng.shen@gmail.com>
2021-07-23 20:35:21 -0400

    Add dismiss to confirm button and change sidebar hamburger icon color

--- packages/app/src/components/ZooModals/HatchModal.tsx
+++ packages/app/src/components/ZooModals/HatchModal.tsx
@@ -14,17 +14,21 @@ const Modal = styled(Existing)`
 `
 
 
-const HatchModal: React.FC<Props> = ({onDismiss = () => null, confirmation}) => {
-    return (
-        <Modal title="Are you Sure?" onDismiss={onDismiss}>
-            <Standard>{`You want to hatch this egg?`}</Standard>
+const HatchModal: React.FC<Props> = ({ onDismiss = () => null, confirmation }) => {
+  function onConfirm() {
+    confirmation()
+    onDismiss()
+  }
+  return (
+      <Modal title="Are you Sure?" onDismiss={onDismiss}>
+          <Standard>{`You want to hatch this egg?`}</Standard>
 
-        <Flex justifyContent="space-evenly" flexDirection='row'>
-            <BorderButton onClick={()=>confirmation()}>Yes</BorderButton>
-            <BorderButton onClick={()=>onDismiss()}>No</BorderButton>
-        </Flex>
-        </Modal>
-    )
+      <Flex justifyContent="space-evenly" flexDirection='row'>
+          <BorderButton onClick={()=>onConfirm()}>Yes</BorderButton>
+          <BorderButton onClick={()=>onDismiss()}>No</BorderButton>
+      </Flex>
+      </Modal>
+  )
 }
 
 export default HatchModal
