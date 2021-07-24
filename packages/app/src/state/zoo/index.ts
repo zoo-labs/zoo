9aa9fba0a3c3b173328dddeef70f170d47da88e9
CoryFlynn <coryjamesflynn@hotmail.com>
2021-07-23 19:49:35 -0700

    remove egg on hatch

--- packages/app/src/state/zoo/index.ts
+++ packages/app/src/state/zoo/index.ts
@@ -40,11 +40,16 @@ export const ZooSlice = createSlice({
       },
       burnEgg: (state: ZooState, action) => {
          const toRemove: Egg = action.payload;
-         state.eggs[toRemove.tokenId] = null;
+         console.log(toRemove)
+         const newState = state;
+         delete newState.eggs[toRemove.tokenId]
+         state = newState
       },
       burnAnimal: (state: ZooState, action) => {
          const toRemove: Animal = action.payload;
-         state.animals[toRemove.tokenId] = null;
+         const newState = state
+         delete newState.animals[toRemove.tokenId]
+         state = newState
       },
       clearZoo: (state: ZooState, action) => {
          state = initialState;
