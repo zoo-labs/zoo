bb582e56451d16df7eeaa28b49764c73938682fc
yuki <uearngold@outlook.com>
2021-07-24 02:20:51 -0400

    cleanup MP16 font loading in global

--- packages/app/src/components/style/Global.tsx
+++ packages/app/src/components/style/Global.tsx
@@ -1,21 +1,25 @@
 import { createGlobalStyle } from 'styled-components'
 
 import { Theme } from 'theme'
+import MyMP16OSFFont from '../../fonts/MP16OSF.ttf'
 
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme { }
 }
 
 const GlobalStyle = createGlobalStyle`
+  @font-face{
+      font-family:'MyMP16OSFFont';
+      src:url('${MyMP16OSFFont}') format('TrueType');   
+  }
   * {
-    font-family: MyMP16OSFFont;
+    font-family: 'MyMP16OSFFont';
     color: #FFFFF;
   }
   body {
     background-color: #040404;
-  overflow-x: hidden;
-
-  position: relative
+    overflow-x: hidden;
+    position: relative;
     img {
       height: auto;
       max-width: 100%;
