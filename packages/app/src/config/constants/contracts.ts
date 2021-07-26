16bf8e4c55856676a02937873e8ed10828f74389
Brian Balayon <brianbalayon@gmail.com>
2021-07-26 13:25:27 -0400

    Added hardhart addresses

--- packages/app/src/config/constants/contracts.ts
+++ packages/app/src/config/constants/contracts.ts
@@ -1,9 +1,23 @@
 export default {
-
     zooToken: {
         97: '0x8175c10383511b3a1C68f9dB222dc14A19CC950e',
         56: '0x5e74094Cd416f55179DBd0E45b1a8ED030e396A1',
+        31337: '0x117FbB2f404e2F1e9FbaC0DdB372C25dD979bc60'
     },
-
+    zooMarket: {
+        31337: '0x86D9d34b581c3f5C954d7ee272A0eB46c8E1A803'
+    },
+    zooMedia: {
+        31337: '0xaF16f94DEAe27a46F6A157CBd1b3C85F14CF0Cb8'
+    },
+    zooAuction: {
+        31337: '0xb3C26aAFa12DeBE5EcFd5dA4D71E9910586dDAF7'
+    },
+    zooDrop: {
+        31337: '0x351DcE5B0972f4E9b369e5Fc726295E37360ce19'
+    },
+    zooFaucet: {
+        31337: '0x1D8A3bF24Fe919c90Af60fd1c722BB69B85a562e'
+    },
   }
   
