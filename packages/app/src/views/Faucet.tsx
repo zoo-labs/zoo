76b32d6ddbd6799ff15c33f4045cd22d6008a506
Ari Lerner <me@ari.io>
2021-07-26 15:32:41 -0700

    Added useContract (needs work) and a Faucet page

--- packages/app/src/views/Faucet.tsx
+++ packages/app/src/views/Faucet.tsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react'
 import Page from "components/layout/Page";
 import styled from 'styled-components'
 import { useWeb3React } from "@web3-react/core";
-import {useWeb3} from 'hooks';
+import {useWeb3, useContract} from 'hooks';
+
 import {ethers} from 'ethers';
+
 import CONTRACTS_JSON from '../contracts.json'
 import BorderButton from "components/Button/BorderButton";
 import ZooFaucet from '../artifacts/src/ZooFaucet.sol/ZooFaucet.json';
@@ -19,37 +21,37 @@ color: white;
 `
 
 export const Faucet: React.FC<FaucetProps> = () => {
-	const web3 = useWeb3();
-	   const { account } = useWeb3React();
+	const { account } = useWeb3React();
+	const [balance, setBalance] = useState(0);
 
-	const [faucetContract, setFaucetContract] = useState(null);
-	const [faucetContractSend, setFaucetContractSend] = useState(null);
-	const [provider, setProvider] = useState(null);
-
-	useEffect(() => {
-		const provider = new ethers.providers.Web3Provider((window as any).ethereum);
-		setProvider(provider);
-		const faucet = new ethers.Contract(CONTRACTS.ZooFaucet.address, ZooFaucet.abi, provider);
-		setFaucetContract(faucet);
-		const signer = provider.getSigner();
-		const senderFaucet = new ethers.Contract(CONTRACTS.ZooFaucet.address, ZooFaucet.abi, signer);
-		setFaucetContractSend(senderFaucet);
-	
-	}, [])
-
+	const ctx = useContract();
 
 	const onBuy = async () => {
-		const txn = await faucetContractSend.buyZoo(account, 1000);
+		const {senderContract} = ctx.getContract('ZooFaucet')
+		const txn = await senderContract.buyZoo(account, 10000);
 		await txn.wait();
+		setBalance(0);
 	}
+
+	useEffect(() => {
+		const getBalance = async () => {
+
+		const {contract} = ctx.getContract('ZooToken')
+		const b = await contract.balanceOf(account);
+		setBalance(b.toString());
+		}
+
+		getBalance();
+	}, [balance]);
 
 	return (
       <>
          <Page>
-		<FaucetWrapper>
+	<FaucetWrapper>
             <BorderButton scale="md" onClick={onBuy}>
                Grant Zoo
             </BorderButton>
+		<h1>Current balance {balance}</h1>
 		</FaucetWrapper>
 	 </Page>
       </>
