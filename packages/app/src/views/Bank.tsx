74a2e2c87c817675256e3258f8b11cb767d863e5
Brian Balayon <brianbalayon@gmail.com>
2021-07-26 13:44:02 -0400

    Added balance loading

--- packages/app/src/views/Bank.tsx
+++ packages/app/src/views/Bank.tsx
@@ -1,9 +1,10 @@
 import BorderButton from "components/Button/BorderButton";
 import Page from "components/layout/Page";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AppState } from "state";
 import { useSelector } from "react-redux";
 import { useWeb3React } from "@web3-react/core";
+import useWeb3 from "hooks/useWeb3";
 import { useHistory } from "react-router-dom";
 import styles from "styled-components";
 import { Label, Text } from "components/Text";
@@ -11,6 +12,7 @@ import { Heading } from "components";
 import Body from "components/layout/Body";
 import { useModal } from "components/Modal";
 import BuyEggs from "components/BuyEggs";
+import { getZooToken } from "util/contractHelpers";
 
 const HeadingContainer = styles.div`
     width: 100%;
@@ -47,59 +49,66 @@ const RowWrapper = styles.div`
     margin: 16px;
 `;
 
-
 const Bank: React.FC = () => {
-  const [zooBalance, setBalance] = useState(0)
-  const { account } = useWeb3React()
-  const history = useHistory()
-  
-  const handleClick = () => {
-    history.push('/account')
-  }
+   const [zooBalance, setBalance] = useState(0.0);
+   const { account, chainId } = useWeb3React();
+   const web3 = useWeb3();
+   const history = useHistory();
 
-  const getBalance = () => {
-    
-  }
+   const zooToken = getZooToken(web3, chainId);
 
-  const pageHeading = (
-    <HeadingContainer>
-      <Heading>My Bank</Heading>
-      <StyledButton
-        style={{
-          background: "transparent",
-          border: "none",
-          color: "white",
-          marginLeft: "8px",
-        }}
-        onClick={()=>handleClick()}
-      >
-        View Account
-      </StyledButton>
-    </HeadingContainer>
-  );
+   const handleClick = () => {
+      history.push("/account");
+   };
 
-  return (
-    <>
-      <Page>
-        {pageHeading}
-        <Body>
-            <LabelWrapper>
-              <Label>Wallet Balance</Label>
-              <BorderButton>Add Funds</BorderButton>
-            </LabelWrapper>
-            <ValueWrapper>
-              {zooBalance} ZOOTOKENS
-            </ValueWrapper>
-            <ValueWrapper>
-              ~100 USD
-            </ValueWrapper>
-            <ValueWrapper>
-              Total Daily Yield: 200 ZOOTOKENS
-            </ValueWrapper>
-          </Body>
-      </Page>
-    </>
-  );
+   const getBalance = async () => {
+      const decimals = await zooToken.methods.decimals().call();
+      const rawBalance = await zooToken.methods.balanceOf(account).call();
+      const divisor = parseFloat(Math.pow(10, decimals).toString());
+      const balance = rawBalance / divisor;
+      setBalance(balance);
+   };
+
+   useEffect(() => {
+      getBalance();
+   }, [account, chainId]);
+
+   useEffect(() => {
+      getBalance();
+   }, []);
+
+   const pageHeading = (
+      <HeadingContainer>
+         <Heading>My Bank</Heading>
+         <StyledButton
+            style={{
+               background: "transparent",
+               border: "none",
+               color: "white",
+               marginLeft: "8px",
+            }}
+            onClick={() => handleClick()}>
+            View Account
+         </StyledButton>
+      </HeadingContainer>
+   );
+
+   return (
+      <>
+         <Page>
+            {pageHeading}
+            <Body>
+               <LabelWrapper>
+                  <Label>Wallet Balance</Label>
+                  <BorderButton>Add Funds</BorderButton>
+               </LabelWrapper>
+               <ValueWrapper>{zooBalance} ZOOTOKENS</ValueWrapper>
+               <ValueWrapper>~100 USD</ValueWrapper>
+               <ValueWrapper>Total Daily Yield: 200 ZOOTOKENS</ValueWrapper>
+            </Body>
+         </Page>
+      </>
+   );
 };
 
 export default Bank;
