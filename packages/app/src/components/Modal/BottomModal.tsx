7f01b279872947ae83728c9024e663591103786f
TechInnovation-Blockchain <newcaesar628@gmail.com>
2021-07-26 16:35:47 -0500

    change modal to swipe slide

--- packages/app/src/components/Modal/BottomModal.tsx
+++ packages/app/src/components/Modal/BottomModal.tsx
@@ -6,17 +6,18 @@ import Sheet from 'react-modal-sheet';
 interface Props {
     onDismiss?: () => null
     header?: string
+    height?: string
 }
 
 
-const BottomModal: React.FC<Props> = ({ children, header,  onDismiss }) => {
+const BottomModal: React.FC<Props> = ({ children, header,  onDismiss, height }) => {
     const [isOpen, setOpen] = React.useState(true);
     const theme = useTheme();
 
         return (
             <>
                 <Sheet isOpen={isOpen} onClose={onDismiss} >
-                    <Sheet.Container style={{height: "300px", background: "#B1DCFF"}}>
+                    <Sheet.Container style={{height: height ? height : "300px", background: "#B1DCFF"}}>
                         <Sheet.Header />
                            <Sheet.Header>
                              <Label  mt="8px" mb="4px" textTransform="uppercase" textAlign="center" fontSize="22px" fontWeight="600" style={{color: "#C82064"}}>
