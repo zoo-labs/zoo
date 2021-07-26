29384e157b6824ceaca5d7e73b91e75737a44c94
Jonathan Eid <jonathaneid98@gmail.com>
2021-07-26 13:26:18 -0400

    Moved ZooDrop to ZooMedia

--- packages/contracts/deploy/02_deploy_zoo_media.ts
+++ packages/contracts/deploy/02_deploy_zoo_media.ts
@@ -13,12 +13,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const marketAddress = (await deployments.get('ZooMarket')).address
 
+  const tokenAddress = (await deployments.get("ZooToken")).address
+
   // Proxy only in non-live network (localhost and hardhat network) enabling
   // HCR (Hot Contract Replacement) in live network, proxy is disabled and
   // constructor is invoked
   await deploy('ZooMedia', {
     from: OWNER_ADDRESS,
-    args: ['CryptoZoo', 'ANML', marketAddress],
+    args: ['CryptoZoo', 'ANML', marketAddress, tokenAddress, 16000],
     log: true,
     // proxy: useProxy && 'postUpgrade',
   })
