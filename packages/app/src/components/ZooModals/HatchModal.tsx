a3cc055c8ebccacbb298bffd073e7514547187ce
Brian Balayon <brianbalayon@gmail.com>
2021-07-26 14:42:07 -0400

    Adjust the font sizes on modals and text (#60)

--- packages/app/src/components/ZooModals/HatchModal.tsx
+++ packages/app/src/components/ZooModals/HatchModal.tsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
-import {Modal, Text as Standard} from 'components'
-import BorderButton from 'components/Button/BorderButton'
-import { Flex } from 'components/Box'
+import React from "react";
+import styled from "styled-components";
+import { Modal, Text as Standard } from "components";
+import BorderButton from "components/Button/BorderButton";
+import { Flex } from "components/Box";
 
 interface Props {
-    onDismiss?: () => void
-    confirmation: any
+   onDismiss?: () => void;
+   confirmation: any;
 }
 
+const HatchModal: React.FC<Props> = ({
+   onDismiss = () => null,
+   confirmation,
+}) => {
+   function onConfirm() {
+      confirmation();
+      onDismiss();
+   }
+   return (
+      <Modal title="Are you Sure?" onDismiss={onDismiss}>
+         <Standard>{`You want to hatch this egg?`}</Standard>
 
-const HatchModal: React.FC<Props> = ({ onDismiss = () => null, confirmation }) => {
-  function onConfirm() {
-    confirmation()
-    onDismiss()
-  }
-  return (
-      <Modal title="Confirm Hatch" onDismiss={onDismiss}>
-          <Standard>{`Do you want to hatch this egg?`}</Standard>
-
-      <Flex justifyContent="space-evenly" flexDirection='row' mt="32px" mb="16px">
-          <BorderButton onClick={()=>onConfirm()}>Yes</BorderButton>
-          <BorderButton onClick={()=>onDismiss()}>No</BorderButton>
-      </Flex>
+         <Flex justifyContent="space-evenly" flexDirection="row">
+            <BorderButton scale="md" onClick={() => onConfirm()}>
+               Yes
+            </BorderButton>
+            <BorderButton scale="md" onClick={() => onDismiss()}>
+               No
+            </BorderButton>
+         </Flex>
       </Modal>
-  )
-}
+   );
+};
 
-export default HatchModal
+export default HatchModal;
