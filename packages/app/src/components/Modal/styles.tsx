a9a9e1db2ba2f231252262388ed6b0a552b65b96
jungsnn <60561405+jungsNN@users.noreply.github.com>
2021-07-27 11:44:54 -0400

    compelete dark version

--- packages/app/src/components/Modal/styles.tsx
+++ packages/app/src/components/Modal/styles.tsx
@@ -33,7 +33,7 @@ export const ModalCloseButton: React.FC<{ onDismiss: ModalProps["onDismiss"] }> 
 
 export const ModalBackButton: React.FC<{ onBack: ModalProps["onBack"] }> = ({ onBack }) => {
   return (
-    <IconButton variant="text" onClick={onBack} area-label="go back" mr="8px">
+    <IconButton variant="text" onClick={onBack} area-label="go back" mr="8px" >
       <ArrowBackIcon color="secondary" />
     </IconButton>
   );
@@ -42,7 +42,7 @@ export const ModalBackButton: React.FC<{ onBack: ModalProps["onBack"] }> = ({ on
 export const ModalContainer = styled(Box)<{ minWidth: string, borderRadius: string }>`
   background: ${({ theme }) => theme.colors.background}; 
   box-shadow: 0px 20px 36px -8px rgba(14, 14, 44, 0.1), 0px 1px 1px rgba(0, 0, 0, 0.05);
-  border: 2px solid ${({ theme }) => theme.colors.borderColor};
+  // border: 2px solid ${({ theme }) => theme.colors.borderColor};
   border-radius: 8px;
   width: 100%;
   z-index: ${({ theme }) => theme.zIndices.modal};
