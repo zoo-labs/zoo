70d418bd895584cb5e5994b2fad5a46f745b19ce
Zach Kelling <z@zeekay.im>
2021-07-27 03:33:15 -0400

    Update ZooDrop

--- packages/contracts/test/ZooDrop.test.ts
+++ packages/contracts/test/ZooDrop.test.ts
@@ -10,6 +10,9 @@ let owner;
 let auctionAddress: string;
 let tokenAddress: string;
 
+const TOKEN_URI = "idx.zoolabs.io/token/"
+const META_URI = "idx.zoolabs.io/meta/"
+
 describe.only("ZooDrop", () => {
     beforeEach(async () => {
         signers = await ethers.getSigners();
@@ -25,29 +28,29 @@ describe.only("ZooDrop", () => {
     });
 
     it("Should add Animal", async () => {
-        await zooDrop.addAnimal("Pug", 100, "Common", 5500, 1, "TOKENURI");
+        await zooDrop.addAnimal("Pug", 100, "Common", 5500, TOKEN_URI, META_URI);
 
         const Animal = await zooDrop.animals("Pug");
         const tokenURI = await zooDrop.tokenURI(Animal.name);
 
         expect(Animal.name).to.equal("Pug");
-        expect(tokenURI).to.equal("URI");
+        expect(tokenURI).to.equal(TOKEN_URI);
     });
 
     it("Should add an Hybrid", async () => {
-        await zooDrop.addHybrid("Puggy", "Pug","Pug", 120 ,"URI");
+        await zooDrop.addHybrid("Puggy", "Pug","Pug", 120, TOKEN_URI, META_URI);
 
         const Hybrid = await zooDrop.hybrids("PugPug");
         const tokenURI = await zooDrop.tokenURI("Puggy");
 
         expect(Hybrid.name).to.equal("Puggy");
-        expect(tokenURI).to.equal("URI");
+        expect(tokenURI).to.equal(TOKEN_URI);
     });
 
     it("Should revert when adding a animal not as owner", async() => {
         zooDrop = zooDrop.connect(signers[1]);
         try {
-            const tx = await zooDrop.addAnimal("Pug", 100, "Common", 5500, 1, "URI");
+            const tx = await zooDrop.addAnimal("Pug", 100, "Common", 5500, TOKEN_URI, META_URI);
         } catch (e) {
             expect(e.message.includes('Ownable: caller is not the owner')).to.be.true;
         }
@@ -57,7 +60,7 @@ describe.only("ZooDrop", () => {
     it("Should revert when adding a hybrid animal not as owner", async() => {
         zooDrop = zooDrop.connect(signers[1]);
         try {
-            const tx = await zooDrop.addHybrid("Puggy", "Pug","Pug", 120 ,"URI");
+            const tx = await zooDrop.addHybrid("Puggy", "Pug","Pug", 120, TOKEN_URI, META_URI);
         } catch (e) {
             expect(e.message.includes('Ownable: caller is not the owner')).to.be.true;
         }
@@ -87,31 +90,32 @@ describe.only("ZooDrop", () => {
 
     it("Should set tokenURI for Animal", async() => {
         zooDrop = zooDrop.connect(signers[0]);
-        await zooDrop.setTokenURI("pug", "URI");
+        await zooDrop.setTokenURI("pug", "pug.com");
         let tokenURI = await zooDrop.tokenURI("pug");
-        expect(tokenURI).to.equal("URI");
+        expect(tokenURI).to.equal("pug.com");
     });
 
     it("Should revert when setting tokenURI as non owner", async() => {
         zooDrop = zooDrop.connect(signers[1]);
         try {
-            const tx = await zooDrop.setTokenURI("pug", "URI");
+            const tx = await zooDrop.setTokenURI("pug", "pug.com");
         } catch (e) {
             expect(e.message.includes('Ownable: caller is not the owner')).to.be.true;
         }
     });
 
-    it("Should set metaDataURI for a pug", async() => {
+    it("Should set metadataURI for a pug", async() => {
         zooDrop = zooDrop.connect(signers[0]);
-        await zooDrop.setMetadataURI("pug", "URI");
-        let tokenURI = await zooDrop.metaDataURI("pug");
-        expect(tokenURI).to.equal("URI");
+        await zooDrop.setMetadataURI("pug", "pug.com/meta");
+        let metadataURI = await zooDrop.metadataURI("pug.com/meta");
+        console.log('metadataURI', metadataURI);
+        expect(metadataURI).to.equal("pug.com/meta");
     });
 
     it("Should revert when setting tokenURI as non owner", async() => {
       zooDrop = zooDrop.connect(signers[1]);
       try {
-          const tx = await zooDrop.setMetadataURI("pug", "URI");
+          const tx = await zooDrop.setMetadataURI("pug", "pug.com");
       } catch (e) {
           expect(e.message.includes('Ownable: caller is not the owner')).to.be.true;
       }
