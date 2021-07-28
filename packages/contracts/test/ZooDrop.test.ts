6488753e75779b78132b1c193b3e1faae4cd5f62
Zach Kelling <z@zeekay.io>
2021-07-27 23:27:35 -0400

    Fix ZooKeeper tests

--- packages/contracts/test/ZooDrop.test.ts
+++ packages/contracts/test/ZooDrop.test.ts
@@ -11,7 +11,7 @@ let owner;
 const TOKEN_URI = "idx.zoolabs.io/token/"
 const META_URI = "idx.zoolabs.io/meta/"
 
-describe.only("ZooDrop", () => {
+describe("ZooDrop", () => {
     beforeEach(async () => {
         signers = await ethers.getSigners();
         owner = signers[0]
