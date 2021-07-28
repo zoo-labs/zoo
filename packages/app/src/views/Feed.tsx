2eae50ba121d487589b612e848d5e44394b9b20b
Brian Balayon <brianbalayon@gmail.com>
2021-07-28 13:50:39 -0400

    Added the reroute for default and for going to the feed

--- packages/app/src/views/Feed.tsx
+++ packages/app/src/views/Feed.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styled from "styled-components";
 import { Text } from "components";
 import { ButtonMenu, ButtonMenuItem } from "components/ButtonMenu";
@@ -12,67 +12,65 @@ import Moralis from "moralis";
 import { AppState } from "state/index";
 import FeedCard from "./FeedCard";
 import BorderButton from "components/Button/BorderButton";
-import { ChevronLeftIcon } from 'components/Svg'
+import { ChevronLeftIcon } from "components/Svg";
 
 const Container = styled.div<{ isMobile?: boolean }>`
-  height: ${({ isMobile }) => (isMobile ? `100vh` : null)};
-  display: ${({ isMobile }) => (isMobile ? null : "flex")};
-  flex-direction: ${({ isMobile }) => (isMobile ? `column` : "row")};
-  flex-wrap: wrap;
-  & .swiper-container {
-    height: 100vh;
-  }
+   height: ${({ isMobile }) => (isMobile ? `100vh` : null)};
+   display: ${({ isMobile }) => (isMobile ? null : "flex")};
+   flex-direction: ${({ isMobile }) => (isMobile ? `column` : "row")};
+   flex-wrap: wrap;
+   & .swiper-container {
+      height: 100vh;
+   }
 `;
 
 const StyledChevron = styled(ChevronLeftIcon)`
-  height: 40px;
-  width: 40px;
-  z-index: 101;
-`
+   height: 40px;
+   width: 40px;
+   z-index: 101;
+`;
 
 const StyledMenuButton = styled.button`
-position: relative;
-top: -8px;
-left: -12px;
-  border: none;
-  background: transparent;
-  box-shadow: none;
-  color: transparant;
-
-`
-
+   position: relative;
+   top: -8px;
+   left: -12px;
+   border: none;
+   background: transparent;
+   box-shadow: none;
+   color: transparant;
+`;
 
 const ToggleContainer = styled.div`
-  div {
-    width: 100%;
-    border-radius: 0;
-    justify-content: center;
-    z-index: 1000;
-    position: absolute;
-    padding-top: 15px;
-    background: linear-gradient(#3d3d3d, transparent);
-  }
-  a {
-    border: none;
+   div {
+      width: 100%;
+      border-radius: 0;
+      justify-content: center;
+      z-index: 1000;
+      position: absolute;
+      padding-top: 15px;
+      background: linear-gradient(#3d3d3d, transparent);
+   }
+   a {
+      border: none;
 
-    font-size: 20px;
-    box-shadow: none;
-    cursor: pointer;
-  }
+      font-size: 20px;
+      box-shadow: none;
+      cursor: pointer;
+   }
 `;
 
 const EmptyZoo = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: center;
-  padding: 80px 0;
-  width: 100%;
-  height: 100%;
+   display: flex;
+   align-items: center;
+   flex-direction: column;
+   justify-content: center;
+   padding: 80px 0;
+   width: 100%;
+   height: 100%;
 
-  button {
-    margin-top: 24px;
-  }
+   button {
+      margin-top: 24px;
+   }
 `;
 
 Moralis.initialize("16weSJXK4RD3aYAuwiP46Cgzjm4Bng1Torxz5qiy");
@@ -80,117 +78,116 @@ Moralis.initialize("16weSJXK4RD3aYAuwiP46Cgzjm4Bng1Torxz5qiy");
 Moralis.serverURL = "https://dblpeaqbqk32.usemoralis.com:2053/server";
 
 export default function Feed() {
-  const animalsState = useSelector<AppState, AppState["zoo"]["animals"]>(
-    (state) => state.zoo.animals
-  );
-  const { isXl } = useMatchBreakpoints();
-  const isMobile = !isXl;
-  const [animals, setAnimals] = React.useState([]);
-  const [subUrl, setSubUrl] = React.useState("");
-  let filter = "";
+   const animalsState = useSelector<AppState, AppState["zoo"]["animals"]>(
+      (state) => state.zoo.animals
+   );
+   const { isXl } = useMatchBreakpoints();
+   const isMobile = !isXl;
+   const [animals, setAnimals] = React.useState([]);
+   const [subUrl, setSubUrl] = React.useState("");
+   let filter = "";
 
-  const history = useHistory();
-  const { account } = useWeb3React();
-  const { url } = useRouteMatch();
-  const { pathname } = useLocation();
+   const history = useHistory();
+   const { account } = useWeb3React();
+   const { url } = useRouteMatch();
+   const { pathname } = useLocation();
 
-  React.useEffect(() => {
-    getAnimals();
-  }, [animalsState]);
-  React.useEffect(() => {
-    return history.listen((location) => {
-      setSubUrl(location.pathname);
-    });
-  }, [history]);
+   useEffect(() => {
+      getAnimals();
+   }, [animalsState]);
 
-  const HomeClick = () => {
-    history.push("/account");
-  };
+   useEffect(() => {
+      return history.listen((location) => {
+         setSubUrl(location.pathname);
+      });
+   }, [history]);
 
-  const getAnimals = () => {
-    setAnimals(Object.values(animalsState));
-  };
+   const HomeClick = () => {
+      history.push("/account");
+   };
 
-  let activeIndex = 0;
-  switch (true) {
-    case pathname.includes("myzoo"):
-      activeIndex = 0;
-      break;
-    case pathname.includes("marketplace"):
-      activeIndex = 1;
-      break;
-    default:
-      activeIndex = 1;
-      break;
-  }
-  switch (true) {
-    case subUrl.includes("myzoo"):
-      filter = "myZoo";
-      break;
-    case subUrl.includes("marketplace"):
-      filter = "marketplace";
-      break;
-    default:
-      filter = "";
-      break;
-  }
-  const isZoo = filter === "myZoo";
-  const animalsFiltered = animals.filter((animal) => {
-    return animal.owner
-      ? isZoo
-        ? animal.owner.toLowerCase() === account.toLowerCase()
-        : animal.owner.toLowerCase() !== account.toLowerCase()
-      : !isZoo;
-  });
+   const getAnimals = () => {
+      setAnimals(Object.values(animalsState));
+   };
 
+   let activeIndex = 0;
+   switch (true) {
+      case pathname.includes("myzoo"):
+         activeIndex = 0;
+         break;
+      case pathname.includes("marketplace"):
+         activeIndex = 1;
+         break;
+      default:
+         activeIndex = 1;
+         break;
+   }
+   switch (true) {
+      case subUrl.includes("myzoo"):
+         filter = "myZoo";
+         break;
+      case subUrl.includes("marketplace"):
+         filter = "marketplace";
+         break;
+      default:
+         filter = "";
+         break;
+   }
+   const isZoo = filter === "myZoo";
+   const animalsFiltered = animals.filter((animal) => {
+      return animal.owner
+         ? isZoo
+            ? animal.owner.toLowerCase() === account.toLowerCase()
+            : animal.owner.toLowerCase() !== account.toLowerCase()
+         : !isZoo;
+   });
 
-  return (
-    <Container isMobile={isMobile}>
-      <ToggleContainer>
-        <ButtonMenu activeIndex={activeIndex + 1} scale="sm">
-          <StyledMenuButton onClick={()=> console.log('asda')}>
-            <StyledChevron onClick={HomeClick}/>
-          </StyledMenuButton>
-          <ButtonMenuItem
-            as={Link}
-            to={`${url}/myzoo`}
-            onClick={() => getAnimals()}
-          >
-            My Zoo
-          </ButtonMenuItem>
-          <ButtonMenuItem
-            as={Link}
-            to={`${url}/marketplace`}
-            onClick={() => getAnimals()}
-          >
-            Marketplace
-          </ButtonMenuItem>
-        </ButtonMenu>
-      </ToggleContainer>
-      {!isZoo || animalsFiltered.length ? (
-        <Swiper
-          spaceBetween={30}
-          slidesPerView={isMobile ? 1 : 3}
-          direction={isMobile ? "vertical" : "horizontal"}
-        >
-          {animalsFiltered.map((data) => {
-            return data.listed ? (
-              <SwiperSlide key={data.tokenId + "slide"}>
-                <FeedCard item={data} key={data.tokenId + "card"}/>
-              </SwiperSlide>
-            ) : (
-              <></>
-            );
-          })}
-        </Swiper>
-      ) : (
-        <EmptyZoo>
-          <Text textAlign="center">You do not currently own any animals</Text>
-          <BorderButton scale="md" onClick={() => HomeClick}>
-            Home
-          </BorderButton>
-        </EmptyZoo>
-      )}
-    </Container>
-  );
+   return (
+      <Container isMobile={isMobile}>
+         <ToggleContainer>
+            <ButtonMenu activeIndex={activeIndex + 1} scale="sm">
+               <StyledMenuButton onClick={() => console.log("asda")}>
+                  <StyledChevron onClick={HomeClick} />
+               </StyledMenuButton>
+               <ButtonMenuItem
+                  as={Link}
+                  to={`${url}/myzoo`}
+                  onClick={() => getAnimals()}>
+                  My Zoo
+               </ButtonMenuItem>
+               <ButtonMenuItem
+                  as={Link}
+                  to={`${url}/marketplace`}
+                  onClick={() => getAnimals()}>
+                  Marketplace
+               </ButtonMenuItem>
+            </ButtonMenu>
+         </ToggleContainer>
+         {!isZoo || animalsFiltered.length ? (
+            <Swiper
+               spaceBetween={30}
+               slidesPerView={isMobile ? 1 : 3}
+               direction={isMobile ? "vertical" : "horizontal"}>
+               {animalsFiltered.map((data) => {
+                  return data.listed ? (
+                     <SwiperSlide key={data.tokenId + "slide"}>
+                        <FeedCard item={data} key={data.tokenId + "card"} />
+                     </SwiperSlide>
+                  ) : (
+                     <></>
+                  );
+               })}
+            </Swiper>
+         ) : (
+            <EmptyZoo>
+               <Text textAlign="center">
+                  You do not currently own any animals
+               </Text>
+               <BorderButton scale="md" onClick={() => HomeClick}>
+                  Home
+               </BorderButton>
+            </EmptyZoo>
+         )}
+      </Container>
+   );
 }
