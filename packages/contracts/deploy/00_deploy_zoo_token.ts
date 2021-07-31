6a0cae190aa26fceb86c06513bbff68344795e17
Zach Kelling <z@zeekay.io>
2021-07-31 14:03:54 -0400

    Remove old deploy helper

--- packages/contracts/deploy/00_deploy_zoo_token.ts
+++ packages/contracts/deploy/00_deploy_zoo_token.ts
@@ -4,9 +4,9 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {deployments, getNamedAccounts} = hre
-  const {deploy} = deployments
-  const {deployer} = await getNamedAccounts()
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
 
   const useProxy = !hre.network.live
 
