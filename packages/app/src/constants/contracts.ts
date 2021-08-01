e50163b4af31870d039f916a71657d4a85e0ac26
Zach Kelling <z@zeekay.io>
2021-08-01 04:18:50 -0400

    Update constants

--- packages/app/src/constants/contracts.ts
+++ packages/app/src/constants/contracts.ts
@@ -1,43 +1,45 @@
 import contracts from "contracts.json";
 
 const hardhat = contracts[31337]['localhost']['contracts'];
-const testnet = contracts[97]['testnet']['contracts'];
+const testnet = hardhat;
+const mainnet = hardhat;
+// const testnet = contracts[97]['testnet']['contracts'];
 // const mainnet = contracts[56]['mainnet']['contracts'];
 
 export default {
    zooToken: {
-      31337: hardhat['ZooToken']['address'],  // local
-      97: testnet['ZooToken']['address'],     // testnet
-      // 56: mainnet['ZooToken']['address'],     // mainnet
+      31337: hardhat['ZooToken']['address'],            // local
+      97: testnet['ZooToken']['address'],               // testnet
+      56: '0xB09FE1613fE03E7361319d2a43eDc17422f36B09', // mainnet
    },
    zooMarket: {
       31337: hardhat['ZooMarket']['address'],
       97: testnet['ZooMarket']['address'],
-      // 56: mainnet['ZooMarket']['address'],
+      56: mainnet['ZooMarket']['address'],
    },
    zooMedia: {
       31337: hardhat['ZooMedia']['address'],
       97: testnet['ZooMedia']['address'],
-      // 56: mainnet['ZooMedia']['address'],
+      56: mainnet['ZooMedia']['address'],
    },
    zooAuction: {
       31337: hardhat['ZooAuction']['address'],
       97: testnet['ZooAuction']['address'],
-      // 56: mainnet['ZooAuction']['address'],
+      56: mainnet['ZooAuction']['address'],
    },
    zooDrop: {
       31337: hardhat['ZooDrop']['address'],
       97: testnet['ZooDrop']['address'],
-      // 56: mainnet['ZooDrop']['address'],
+      56: mainnet['ZooDrop']['address'],
    },
    zooFaucet: {
       31337: hardhat['ZooFaucet']['address'],
       97: testnet['ZooFaucet']['address'],
-      // 56: mainnet['ZooFaucet']['address'],
+      56: mainnet['ZooFaucet']['address'],
    },
    zooKeeper : {
       31337: hardhat['ZooKeeper']['address'],
       97: testnet['ZooKeeper']['address'],
-      // 56: mainnet['ZooKeeper']['address'],
+      56: mainnet['ZooKeeper']['address'],
    }
 };
