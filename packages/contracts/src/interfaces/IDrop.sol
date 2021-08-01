f4e43aba4b6ffa373a37e2d97d5892cca36c9d85
Zach Kelling <z@zeekay.io>
2021-07-31 21:58:19 -0400

    Make eggs configurable

--- packages/contracts/src/interfaces/IDrop.sol
+++ packages/contracts/src/interfaces/IDrop.sol
@@ -5,8 +5,9 @@ pragma solidity >=0.8.4;
 import { IZoo } from "./IZoo.sol";
 
 interface IDrop {
+    function title() external view returns (string memory);
     function eggPrice() external view returns (uint256);
-    function currentSupply() external view returns (uint256);
+    function eggSupply() external view returns (uint256);
     function newEgg() external view returns (IZoo.Token memory);
     function newHybridEgg(IZoo.Parents memory) external view returns (IZoo.Token memory);
     function getRandomAnimal(uint256) external view returns (IZoo.Token memory);
