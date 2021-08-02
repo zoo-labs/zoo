f6c59c17187800eba6670bba46dea7770552da2d
Zach Kelling <z@zeekay.io>
2021-08-01 20:51:12 -0400

    Update deploy scripts

--- packages/contracts/scripts/deploy.js
+++ packages/contracts/scripts/deploy.js
@@ -1,13 +1,60 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
 const ethers = hre.ethers
+
+const rarities = require('../utils/animals.json');
+const animals  = require('../utils/animals.json');
+const hybrids  = require('../utils/hybrids.json');
 
 async function main() {
   const [signer] = await ethers.getSigners()
 
+  const dropAddress = '0x0994282110F389553D5f7bCD09a5fdf89AA7B5f4';
+  const keeperAddress = '0x42adad34a80FBC488CF963A61eCCaa932736AeDb';
+  const keeper = await ethers.getContractAt('ZooKeeper', keeperAddress);
+  const drop = await ethers.getContractAt('ZooDrop', dropAddress);
+
+  // // Add Drop to ZooKeeper
+  // await keeper.setDrop(dropAddress)
+
+  // // Configure drop
+  // const eggs = [
+  //   {
+  //     name: "Base Egg",
+  //     price: 210,
+  //     supply: 16000,
+  //     tokenURI: "https://db.zoolabs/egg.jpg",
+  //     metadataURI: "https://db.zoolabs.org/egg.json"
+  //   },
+  //   {
+  //     name: "Hybrid Egg",
+  //     price: 0,
+  //     supply: 0,
+  //     tokenURI: "https://db.zoolabs/hybrid.jpg",
+  //     metadataURI: "https://db.zoolabs.org/hybrid.json"
+  //   }
+  // ]
+
+  // await Promise.all(eggs.map(async (v) => {
+  //   console.log('Add Egg:', v.name)
+  //   await drop.setEgg(v.name, v.price, v.supply, v.tokenURI, v.metadataURI)
+  // }))
+
+  // await drop.configureEggs("Base Egg", "Hybrid Egg")
+
+  // await Promise.all(animals.map(async (v) => {
+  //   console.log('Add Animal:', v.name)
+  //   await drop.setAnimal(v.name, v.rarity, v.tokenURI, v.metadataURI)
+  // }))
+
+  await Promise.all(hybrids.map(async (v) => {
+    console.log('Add Hybrid:', v.name)
+    await drop.setHybrid(v.name, v.rarity, v.yield, v.parentA, v.parentB, v.tokenURI, v.metadataURI)
+  }))
+
   // Deploy the token contract
-  const ZooToken = await ethers.getContractFactory('ZooToken', signer)
-  const token = await ZooToken.deploy()
-  console.log('ZooToken', token.address)
+  // const ZooToken = await ethers.getContractFactory('ZooToken', signer)
+  // const token = await ZooToken.deploy()
+  // console.log('ZooToken', token.address)
 
   // // Transfer token ownership to farm contract
   // console.log('Transferring Token ownership')
