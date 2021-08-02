655c29c24ea0b19d19983beab1cd92a5492917b3
Brian Balayon <brianbalayon@gmail.com>
2021-08-02 15:09:10 -0400

    Using owner info t odetermine if an animal can be freed

--- packages/app/src/components/MarketModals/YieldModal.tsx
+++ packages/app/src/components/MarketModals/YieldModal.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { useWeb3React } from "@web3-react/core";
 import { Label, Text, Flex } from "components";
 import { BottomModal } from "components/Modal";
 
 interface Props {
    onDismiss?: () => null;
-  item?: any;
-  animalGroup?: { [key: string]: number };
+   item?: any;
+   animalGroup?: { [key: string]: number };
 }
 // rgb(254 29 135) Darker PINK
 const YieldModal: React.FC<Props> = ({ item, onDismiss, animalGroup }) => {
-  const multiplier = animalGroup[item.animalId]
+   const { account, chainId } = useWeb3React();
+   const multiplier = animalGroup[item.animalId];
+   console.log(item);
+   const canFree = item.owner === account;
+   console.log("IS OWNER", canFree);
    return (
       <>
          <BottomModal header="Yield Info" height="300px" onDismiss={onDismiss}>
@@ -22,13 +27,29 @@ const YieldModal: React.FC<Props> = ({ item, onDismiss, animalGroup }) => {
                <Label fontSize="22px" color="text" fontWeight="550">
                   Daily Yield
                </Label>
-               <Text  bold ml="16px" mt="4px" mb="16px" fontSize="22px" color="text">
-                  {multiplier ? `${multiplier} x ${Number(item.yield)} = ${multiplier * Number(item.yield)}` : `${Number(item.yield)}`}
+               <Text
+                  bold
+                  ml="16px"
+                  mt="4px"
+                  mb="16px"
+                  fontSize="22px"
+                  color="text">
+                  {multiplier
+                     ? `${multiplier} x ${Number(item.yield)} = ${
+                          multiplier * Number(item.yield)
+                       }`
+                     : `${Number(item.yield)}`}
                </Text>
                <Label fontSize="22px" color="text" fontWeight="550">
                   Accumulated Yield
                </Label>
-               <Text bold ml="16px" mt="4px" mb="16px" fontSize="22px" color="text">
+               <Text
+                  bold
+                  ml="16px"
+                  mt="4px"
+                  mb="16px"
+                  fontSize="22px"
+                  color="text">
                   {Math.floor(item.yield * (Math.random() * (12 - 1) + 1))}
                </Text>
             </Flex>
