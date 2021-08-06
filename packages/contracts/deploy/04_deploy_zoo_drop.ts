66cd6c73e32f1efd961c93ab02c73a911be23c45
Zach Kelling <z@zeekay.im>
2021-08-05 20:02:12 -0400

    Rework Faucet, deploy scripts, randomization

--- packages/contracts/deploy/04_deploy_zoo_drop.ts
+++ packages/contracts/deploy/04_deploy_zoo_drop.ts
@@ -16,16 +16,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     log: true,
   })
 
-  const dropAddress = deployResult.address;
   const keeperAddress = (await deployments.get('ZooKeeper')).address
-  const keeper = await hre.ethers.getContractAt('ZooKeeper', keeperAddress);
-  const drop = await hre.ethers.getContractAt('ZooDrop', dropAddress);
+  const keeper = await ethers.getContractAt('ZooKeeper', keeperAddress);
+  const drop = await ethers.getContractAt('ZooDrop', deployResult.address);
 
   // Configure game executes a very long series of transactions which set the
   // initial state for our Gen 0 drop. Do not expect this to work during
   // Testnet or Mainnet deployment -- use the standalone `yarn deploy:drop` to
   // update Testnet or Mainnet contracts.
-  configureGame(keeper, drop);
+  await configureGame(keeper, drop);
 
   return hre.network.live;
 }
