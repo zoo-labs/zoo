8b9553e20af465d4561c23936aee298695e5eee6
Zach Kelling <z@zeekay.io>
2021-08-10 04:52:28 -0400

    Added moralis:watch script

--- packages/contracts/scripts/moralisFunctions.js
+++ packages/contracts/scripts/moralisFunctions.js
@@ -15,20 +15,17 @@ const SUBDOMAIN = {
   mainnet:   'j0ixlvmwc1kz.usemoralis.com',
 }[NETWORK]
 
-console.log(`Updating Cloud Functions for ${NETWORK}`)
-
 const moralisJS = fs.readFileSync(__dirname + '/../functions/moralis.js')
 const zkJSON    = fs.readFileSync(__dirname + `/../deployments/${NETWORK}/ZooKeeper.json`)
 const cloudFunctions = String(moralisJS).replace('CHAIN_ID', CHAIN_ID).replace('ZOOKEEPER', zkJSON)
 fs.writeFileSync(__dirname + '/../cache/moralis.js', cloudFunctions)
 
-console.log('Built Cloud Functions')
-
 const child = spawn('node', ['node_modules/.bin/moralis-admin-cli', 'watch-cloud-file', '--moralisSubdomain', SUBDOMAIN, '--moralisCloudFile', 'cache/moralis.js'], { shell: true })
 
 child.stdout.on('data', (data) => {
   if (String(data).match(/File Uploaded Correctly/)) {
-    console.log('Updated Cloud Functions')
+    console.log(`Updated Cloud Functions for ${NETWORK}`)
+
     child.kill('SIGHUP')
     process.exit(0)
   }
