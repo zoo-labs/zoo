52e7223fe33b1ad50dbdd55127558b87f071890c
Zach Kelling <z@zeekay.io>
2021-08-10 04:09:55 -0400

    Update Moralis integration
    Now with super powers...
    
    Try:
    
    `yarn moralis:functions`
    `yarn moralis:plugins`
    `yarn moralis:proxy`
    `yarn moralis:logs`

--- packages/contracts/scripts/moralisPlugins.js
+++ packages/contracts/scripts/moralisPlugins.js
@@ -1,84 +1,113 @@
-const Sentry = require("@sentry/node");
-const axios = require('axios');
-const fs = require('fs');
+const axios = require('axios')
+const fs    = require('fs')
+
 require('dotenv').config()
 const utils = require('moralis-admin-cli/utils')
-const { BASE_URI } = require('moralis-admin-cli/config');
-const { restartServer } = require('moralis-admin-cli/commands/common');
-const prompt = require('prompt-sync')({sigint: true});
+const { BASE_URI } = require('moralis-admin-cli/config')
+const { restartServer } = require('moralis-admin-cli/commands/common')
+
+const NETWORK = process.env.NETWORK ? process.env.NETWORK : 'localhost'
+const CHAIN_IDS = {
+  localhost: '0x539',
+  testnet:   '0x61',
+  mainnet:   '0x38',
+}
+const CHAIN_ID = CHAIN_IDS[NETWORK]
+const SERVERS = {
+  localhost: 'zoo Hardhat',
+  testnet:   'Zoo Testnet',
+  mainnet:   'Zoo Mainnet',
+}
+const SERVER_NAME = SERVERS[NETWORK]
+const ABI_PATH = `deployments/${NETWORK}/ZooKeeper.json`
 
 /**
  * Configures the events to subscribe to on a smart contract based on the abi file
  */
+;(async() => {
+    console.log(`Updating Plugins for ${NETWORK}`)
 
-// Get the path to the abi json file
-const abiPath = 'deployments/localhost/ZooKeeper.json';
-const serverName = 'zoo Hardhat';
-// const abiPath = 'deployments/testnet/ZooKeeper.json';
-// const serverName = 'Zoo Testnet';
-
-(async() => {
     try {
         // Get credentials
-        const apiKey = utils.getApiKey();
-        const apiSecret = utils.getApiSecret();
+        const apiKey = utils.getApiKey()
+        const apiSecret = utils.getApiSecret()
 
         // Load servers
-        const servers = await utils.getUserServers();
+        const servers = await utils.getUserServers()
         if (servers.length == 0) {
-            console.log("No servers found!");
-            return;
+            console.log('No servers found!')
+            return
         }
 
         // Get the server to apply the event syncs to
-        const server = servers.filter(item => item.updateCloudError === 0 && item.update === 0 && item.enabledEvms && item.name == serverName)[0]
-
-        console.log('Using server:', server.name)
+        const server = servers.filter(item => item.updateCloudError === 0 && item.update === 0 && item.enabledEvms && item.name == SERVER_NAME)[0]
 
         // read the abi
-        fs.readFile(abiPath, "utf8", async function read(err, data) {
+        fs.readFile(ABI_PATH, 'utf8', async function read(err, data) {
 
             // Check for errors
             if (err) {
-                console.log(`Error reading file from disk: ${err}`);
-                return;
+                console.log(`Error reading file from disk: ${err}`)
+                return
             }
 
             // get the events from the abi
             const contract = JSON.parse(data)
-            const abiEvents = contract.abi.filter(item => item.type == "event");
+            const abiEvents = contract.abi.filter(item => item.type == 'event')
             if (!abiEvents){
-                console.log("Could not read ABI");
+                console.log('Could not read ABI')
             }
 
             // Function for fixing datatypes
-            const fixType = (type) => type == "uint" ? "uint256" : type;
+            const fixType = (type) => type == 'uint' ? 'uint256' : type
 
             // function for getting the topic
-            const getTopic = (item) =>  `${item.name}(${item.inputs.reduce((a,o) => (a.push(fixType(o.type)),a), []).join()})`;
+            const getTopic = (item) =>  `${item.name}(${item.inputs.reduce((a,o) => (a.push(fixType(o.type)),a), []).join()})`
 
             // Get a list of all the available events to choose from
-            const eventNames = abiEvents.reduce((a,o,i) => (a.push(`\n${o.name}`),a), []).join('');
+            const eventNames = abiEvents.reduce((a,o,i) => (a.push(`\n${o.name}`),a), []).join('')
 
-            // Get the current plugins of the server
-            const plugins = [];
+            // Ensure the default plugins are installed
+            const plugins = [
+              {
+                  'id': 1,
+                  'path': './evm/consumer',
+                  'options': []
+              },
+              {
+                  'id': 2,
+                  'path': './evm/historical/transactions',
+                  'options': []
+              },
+              {
+                  'id': 3,
+                  'path': './evm/blocks',
+                  'options': []
+              },
+              {
+                  'id': 4,
+                  'path': './evm/balances',
+                  'options': []
+              },
+              {
+                  'id': 5,
+                  'path': './convenience/index',
+                  'options': []
+              }
+            ]
 
-            // Loop through the selected events
+            // Loop through events in ZooKeeper ABI
             for (let i = 0; i < abiEvents.length; i++) {
-                // Get the current event
-                const event = abiEvents[i];
-
+                const event = abiEvents[i]
                 const description = event.name
                 const contractAddress = contract.address
                 const tableName = event.name
-                const provider = utils.getSelectedServerProvider(JSON.parse(server.enabledEvms))
-                // Calculate the topic
-                const topic = getTopic(event);
+                const topic = getTopic(event)
 
                 // Define the new plugin
                 const plugin = {
-                    id: 1,
-                    path: "./evm/events",
+                    id: 6,
+                    path: './evm/events',
                     order: 5,
                     options: {
                         description: description,
@@ -86,35 +115,34 @@ const serverName = 'zoo Hardhat';
                         topic: topic,
                         address: contractAddress,
                         tableName: tableName,
-                        chainId: provider
+                        chainId: CHAIN_ID
                     }
-                };
-                console.log('Adding plugin for event: ', event.name, contractAddress)
+                }
+
+                console.log('Adding plugin for: ', event.name, contractAddress)
 
                 // Push the plugin to the list
-                plugins.push(plugin);
+                plugins.push(plugin)
             }
 
             // Post updated plugins to the api
-            console.log("\nPushing contract events to moralis server...")
+            console.log('\nPushing contract events...')
             await axios.post(`${BASE_URI}/api/cli/updateServerPlugins`, {
                 apiKey,
                 apiSecret,
-                parameters :
-                {
+                parameters: {
                     serverId: server.id,
                     plugins: JSON.stringify(plugins)
                 }
-            });
-            console.log("Successfully saved the contract events!");
+            })
+            console.log('Successfully saved the contract events!')
 
             // Restart server to apply sync
-            await restartServer(apiKey,apiSecret, server);
-            console.log("Events are now subscribed to!");
-        });
+            await restartServer(apiKey,apiSecret, server)
+            console.log('Events are now subscribed to!')
+        })
     } catch (e) {
-        console.log("Unexpected error")
+        console.log('Unexpected error')
         console.log(e)
-        Sentry.captureException(e);
     }
 })()
