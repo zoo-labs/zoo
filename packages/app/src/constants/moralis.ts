902256efbf3183bfeab05be29abd2e7b8758ec74
Zach Kelling <z@zeekay.im>
2021-08-10 19:28:26 -0400

    Update constants

--- packages/app/src/constants/moralis.ts
+++ packages/app/src/constants/moralis.ts
@@ -5,7 +5,7 @@ const config = {
     56:    'cIGUkzL7pyhM8aC8gIcDiH46QGpsEutO5SAQzTgy',
   },
   serverURL: {
-    1337: 'https://qjydxwdegh7e.usemoralis.com:2053/server',
+    1337:  'https://qjydxwdegh7e.usemoralis.com:2053/server',
     97:    'https://dblpeaqbqk32.usemoralis.com:2053/server',
     56:    'https://j0ixlvmwc1kz.usemoralis.com:2053/server',
   }
