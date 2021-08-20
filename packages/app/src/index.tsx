e5cd4dc9b518d6be5377bc86ee26e87dfedb8c50
johneyo <johneyo57@yahoo.com>
2021-08-20 23:03:05 +0100

    navbar

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import Providers from './Providers'
-
+import './index.css'
 ReactDOM.render(
   <React.StrictMode>
     <Providers>
--- packages/app/src/views/Login.tsx
+++ packages/app/src/views/Login.tsx
@@ -92,17 +92,34 @@ export default function LoginMain() {
   }
 
   return (
-    <Container>
-      <img
+    <div
+      className='relative pt-16 pb-32 flex content-center items-end justify-center'
+      style={{
+        minHeight: '100vh',
+        background: 'radial-gradient(circle, rgba(173, 61, 255, 1) 19%, rgba(0, 212, 255, 1) 100%)',
+      }}>
+      {/* <img
         alt='CryptoZoo Logo'
         src='bearshark-halftone.png'
         style={{ opacity: '0.8', top: '50%', transform: 'translateY(-50%)', left: '1%', position: 'absolute', width: '98%', margin: 'auto' }}
-      />
-      <img alt='CryptoZoo Logo' src='ZooLogoWhite.png' style={{ top: '50%', transform: 'translateY(-50%)', left: '29vw', position: 'absolute', width: '42vw', margin: 'auto' }} />
-      <Button onClick={metamaskLogin} style={{ position: 'absolute', left: '33%', bottom: '10%', width: '33%' }}>
-        <p>LOGIN WITH</p>
-        <p>METAMASK</p>
-      </Button>
+      /> */}
+
+      <div
+        className='absolute top-0 w-full h-full bg-center bg-cover'
+        style={{
+          backgroundImage: 'url(' + require('../assets/img/bearshark-halftone.png').default + ')',
+          backgroundSize: '80%',
+          backgroundRepeat: 'no-repeat',
+        }}>
+        <img alt='CryptoZoo Logo' src='ZooLogoWhite.png' style={{ top: '50%', transform: 'translateY(-50%)', left: '29vw', position: 'absolute', width: '42vw', margin: 'auto' }} />
+        {/* <span id='blackOverlay' className='w-full h-full absolute opacity-50 bg-black'></span> */}
+      </div>
+      <button
+        onClick={metamaskLogin}
+        className='z-10 whitespace-normal px-4 cursor-pointer lg:px-12 py-4 text-white font-bold rounded'
+        style={{ background: 'linear-gradient(360deg, #8c4ff8 20%, rgb(173, 61, 255) 100%)' }}>
+        LOGIN WITH METAMASK
+      </button>
       {/* <StyledFlex flexDirection="row" justifyContent="center" >
 
             {isMobile &&
@@ -118,6 +135,6 @@ export default function LoginMain() {
                 </>
                 }
                 </StyledFlex>  */}
-    </Container>
+    </div>
   )
 }
