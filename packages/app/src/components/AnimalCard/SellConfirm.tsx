e06a1dbbd3d289308e3336527c10e82e6d331df8
Zach Kelling <z@zeekay.io>
2021-08-20 22:00:58 -0400

    Update modals, redirects, menus

--- packages/app/src/components/AnimalCard/SellConfirm.tsx
+++ packages/app/src/components/AnimalCard/SellConfirm.tsx
@@ -18,16 +18,16 @@ export const SellConfirm: React.FC<SellConfirmProps> = ({ onDismiss = () => null
     <Modal title='Confirm Listing' onDismiss={onDismiss}>
       <Text style={{ textAlign: 'center' }}>{`Do you want to list ${sellAnimal.name}?`}</Text>
       <Flex width='100%' alignItems='center' justifyContent='space-evenly' flexDirection='row' mt='16px'>
-        <Text fontSize='20px' style={{ whiteSpace: 'nowrap', marginTop: '5px' }}>
-          BID PRICE
+        <Text fontSize='16px' style={{ whiteSpace: 'nowrap', marginTop: '5px' }}>
+          Price
         </Text>
         <BidPriceInput type='number' />
       </Flex>
       <Flex width='100%' alignItems='center' justifyContent='space-evenly' flexDirection='row' mt='16px'>
-        <BorderButton scale='md' onClick={() => breed(onDismiss)}>
+        <BorderButton style={{ fontSize: 14 }}scale='md' onClick={() => breed(onDismiss)}>
           Confirm
         </BorderButton>
-        <BorderButton scale='md' onClick={() => onDismiss()}>
+        <BorderButton style={{ fontSize: 14 }} scale='md' onClick={() => onDismiss()}>
           Cancel
         </BorderButton>
       </Flex>
