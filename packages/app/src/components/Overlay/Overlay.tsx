1fb8842232cd91207a7505d99aaa17adf32e98b0
johneyo <johneyo57@yahoo.com>
2021-08-23 11:33:19 +0100

    BLUR

--- packages/app/src/components/Overlay/Overlay.tsx
+++ packages/app/src/components/Overlay/Overlay.tsx
@@ -7,7 +7,7 @@ const Overlay = styled.div.attrs({ role: 'presentation' })<OverlayProps>`
   left: 0px;
   width: 100%;
   height: 100%;
-  background-color: ${({ theme }) => theme.colors.tertiary};
+  background-color: rgba(0, 0, 0, 0.424);
   transition: opacity 0.4s;
   opacity: ${({ show }) => (show ? 0.4 : 0)};
   z-index: ${({ zIndex }) => zIndex};
