f2377391d60568fe11c475cd4fe2c86402cbb133
Ari Lerner <me@ari.io>
2021-08-26 19:28:57 -0400

    Fixed ZooToken tests

--- packages/contracts/test/ZooToken.test.ts
+++ packages/contracts/test/ZooToken.test.ts
@@ -10,6 +10,8 @@ import { solidity } from 'ethereum-waffle'
 chai.use(asPromised)
 chai.use(solidity)
 
+const BigNumber = ethers.BigNumber;
+
 const setupTest = deployments.createFixture(async ({ deployments, getNamedAccounts, ethers }, options) => {
   const contracts = await deployments.fixture() // ensure you start from a fresh deployments
   const signers = await ethers.getSigners()
@@ -47,29 +49,55 @@ describe.only('ZooToken', function () {
     expect(await token.isBlacklisted(address2)).to.be.false
   })
 
-  it.only('should not allow transfer when blacklisted', async function() {
+  it('allows transfer for eligable accounts', async function() {
     const {signers, token} = await setupTest()
 
     const address = signers[1].address
     const address2 = signers[2].address
     const address3 = signers[3].address
 
-
     await token.mint(address, 1000)
     await token.mint(address2, 1000)
     await token.mint(address3, 1000)
 
-    await (token.connect(signers[1])).approve(address2, 1000)
-    // await token.connect(signers[2]).approve(address3, 1000)
+    await token.connect(signers[1]).approve(address2, 1000)
+    await token.connect(signers[2]).approve(address, 1000)
 
-    console.log(await token.allowance(address, address2))
-    await token.connect(signers[1]).transferFrom(address, address2, 100)
-    // await expect(token.transferFrom(address2, address3, 100)).not.to.be.reverted
+    const initialBalance = await token.balanceOf(address);
+    await expect(token.connect(signers[2]).transferFrom(address, address2, 100)).not.to.be.reverted;
+  });
+
+  it('should not allow transferFrom when blacklisted', async function() {
+    const {signers, token} = await setupTest()
+
+    const address = signers[1].address
+    const address2 = signers[2].address
+
+    await token.mint(address, 1000)
+    await token.mint(address2, 1000)
+
+    await token.connect(signers[1]).approve(address2, 1000)
+    //await token.connect(signers[2]).approve(address, 1000)
 
     // Add user to blacklist
     await token.blacklistAddress(address)
-    await expect(token.transferFrom(address, address2, 100)).to.be.revertedWith('ss is on blacklist')
-    // await token.transferFrom(address, address1, 100)
-
+    await expect(token.connect(signers[1]).transferFrom(address, address2, 100)).to.be.revertedWith('Address is on blacklist')
   })
+
+  it('does not allow transfer from a blacklisted address', async function() {
+    const {signers, token} = await setupTest()
+
+    const address = signers[1].address
+    const address2 = signers[2].address
+    const address3 = signers[3].address
+
+    await token.mint(address, 1000)
+    await token.mint(address2, 1000)
+
+    await token.connect(signers[1]).approve(address2, 1000)
+
+    // Add user to blacklist
+    await token.blacklistAddress(address)
+    await expect(token.connect(signers[1]).transfer(address2, 100)).to.be.revertedWith('Address is on blacklist')
+  });
 })
