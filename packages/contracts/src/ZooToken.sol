33f8f36f029a0ab2c6cc925c961d9fb8c9f2bf8e
Zach Kelling <z@zeekay.im>
2021-08-26 17:08:33 -0400

    Implement blacklist

--- packages/contracts/src/ZooToken.sol
+++ packages/contracts/src/ZooToken.sol
@@ -7,11 +7,27 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract ZooToken is ERC20, ERC20Burnable, Ownable {
+
+contract ZooToken is ERC20, ERC20Burnable, Ownable, AccessControl {
     using SafeERC20 for IERC20;
 
+    bytes32 public constant BLACKLISTED = keccak256("BLACKLISTED");
+
     constructor () ERC20("Zoo", "ZOO") {  }
+
+    function transfer(address _to, uint256 _value) public override returns (bool) {
+        require(hasRole(BLACKLISTED, msg.sender) == false);
+        require(hasRole(BLACKLISTED, _to) == false);
+        return super.transfer(_to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool) {
+        require(hasRole(BLACKLISTED, _to) == false);
+        require(hasRole(BLACKLISTED, _from) == false);
+        return super.transferFrom(_from, _to, _value);
+    }
 
     function mint(address to, uint256 value) public onlyOwner {
         super._mint(to, value);
