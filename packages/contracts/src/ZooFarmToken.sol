30f3873a048f12afbe2535b966db7f8809d0d2ae
Ari Lerner <me@ari.io>
2021-08-27 02:06:12 -0400

    Fixed setupTestFactory helper

--- packages/contracts/src/ZooFarmToken.sol
+++ packages/contracts/src/ZooFarmToken.sol
@@ -4,8 +4,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
-contract MemeFarmToken is ERC20("MemeFarmToken", "MFRM"), Ownable {
-    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MemeFarm).
+contract ZooFarmToken is ERC20("ZooFarmToken", "ZFRM"), Ownable {
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (ZooFarmToken).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
     }
