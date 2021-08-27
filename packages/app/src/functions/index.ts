34faa671223244d3d2c19697dae0f66c334bc9c5
johneyo <johneyo57@yahoo.com>
2021-08-27 01:38:50 +0100

    sawp / pool

---
+++ packages/app/src/functions/index.ts
@@ -0,0 +1,6 @@
+
+export * from './currency'
+export * from './format'
+export * from './parse'
+export * from './validate'
+export * from './contract'
---
+++ packages/app/src/views/Pool.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface PoolProps {}
+
+const Pool: React.FC<PoolProps> = ({}) => {
+  return <div>POOL</div>
+}
+
+export default Pool
