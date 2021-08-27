1e22fa7491a86c225e9cc5b0a9c7a13e859650c6
Zach Kelling <z@zeekay.im>
2021-08-27 17:46:56 -0400

    sawp / pool

--- packages/app/src/routes.tsx
+++ packages/app/src/routes.tsx
@@ -6,7 +6,7 @@ import Pool from './views/Pool'
 let indexRoutes = [
   { path: '/account', name: 'Account', component: Account },
   { path: '/home', name: 'Home', component: Account },
-  // { path: '/bank', name: 'Bank', component: Bank },
+  { path: '/bank', name: 'Bank', component: Bank },
   { path: '/feed', name: 'Feed', component: Feed },
   { path: '/swap', name: 'Swap', component: Swap },
   { path: '/limit-order', name: 'Limit', component: Swap },
