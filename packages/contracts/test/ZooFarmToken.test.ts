3cf4f9ce330f8d65975f30da95a49baa6a00d427
Ari Lerner <me@ari.io>
2021-08-27 02:15:50 -0400

    Updated ZooFarmToken tests

--- packages/contracts/test/ZooFarmToken.test.ts
+++ packages/contracts/test/ZooFarmToken.test.ts
@@ -5,8 +5,24 @@ const setupTest = setupTestFactory(['ZooFarmToken'])
 
 describe('ZooFarmToken', async () => {
     it('exists', async () => {
-        const {tokens} = await setupTest()
-        const token = tokens['ZooFarmToken']
+        const {tokens: {'ZooFarmToken': token}} = await setupTest()
         expect(token).not.to.be.null;
+    });
+
+    it('mints tokens by the owner', async () => {
+        const {tokens: {'ZooFarmToken': token}, signers} = await setupTest()
+        const toAddr = signers[0].address;
+        expect(parseInt(await token.totalSupply())).to.equal(0);
+        await token.mint(toAddr, 100);
+        expect(parseInt(await token.totalSupply())).to.equal(100);
+        expect(await token.balanceOf(toAddr)).to.equal(100);
+    })
+
+    it('mints tokens to any given address', async () => {
+        const {tokens: {'ZooFarmToken': token}, signers} = await setupTest()
+        const toAddr = signers[1].address;
+        expect(await token.balanceOf(toAddr)).to.equal(0)
+        await token.mint(toAddr, 100);
+        expect(await token.balanceOf(toAddr)).to.equal(100)
     })
 })
