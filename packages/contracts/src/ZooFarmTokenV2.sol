0861de4bc84dfb4d16f10dadffbce4842f39858a
Ari Lerner <me@ari.io>
2021-08-27 02:43:56 -0400

    ZooFarm started

---
+++ packages/contracts/src/ZooFarmTokenV2.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <=0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+// MFRM Token is the MemeFarm goverance token which is issued by the MemeFarm
+// contract as a reward for staking LP tokens
+contract ZooFarmTokenV2 is ERC20Upgradeable, OwnableUpgradeable {
+    function initialize() public initializer  {
+        __ERC20_init("ZooFarmTokenV2", "ZFRM");
+        __Ownable_init();
+    }
+
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (memefarm).
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
