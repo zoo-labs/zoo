28751b039e1164976a0d1ab01649649cb3678fac
Zach Kelling <z@zeekay.io>
2021-08-27 19:46:39 -0400

    Update app packages, imports

--- packages/app/src/components/CurrencyInputPanel/FiatValue.tsx
+++ packages/app/src/components/CurrencyInputPanel/FiatValue.tsx
@@ -1,4 +1,4 @@
-import { Currency, CurrencyAmount, Percent } from '@uniswap/sdk'
+import { Currency, CurrencyAmount, Percent } from '@sushiswap/sdk'
 import React, { useMemo } from 'react'
 
 // import { warningSeverity } from '../../functions/prices'
