3f23607b10e99c1b993998172e0e30179bbff7dc
Ari Lerner <me@ari.io>
2021-08-27 02:43:56 -0400

    ZooFarm started

---
+++ packages/contracts/test/ZooFarmTokenV2.test.ts
@@ -0,0 +1,11 @@
+import {setupTestFactory, requireDependencies} from './utils'
+
+const {expect} = requireDependencies();
+const setupTest = setupTestFactory(['ZooFarmTokenV2'])
+
+describe('ZooFarmTokenV2', async () => {
+    it('exists', async () => {
+        const {tokens: {'ZooFarmTokenV2': token}} = await setupTest()
+        expect(token).not.to.be.null;
+    });
+});
