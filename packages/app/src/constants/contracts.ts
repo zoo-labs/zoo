e87d64760808e7e33d686c13941a5cc7572a8b38
Zach Kelling <z@zeekay.io>
2021-08-29 17:54:39 -0400

    Recompile contracts

--- packages/app/src/constants/contracts.ts
+++ packages/app/src/constants/contracts.ts
@@ -1,43 +1,55 @@
-import contractsJSON from '../contracts.json'
+import contractsJSON from 'contracts.json'
 
 const hardhat = contractsJSON['1337']['hardhat']['contracts']
-const testnet = contractsJSON['97']['testnet']['contracts']
-const mainnet = testnet // does not work yet
+
+// BSC
+const testnet = hardhat // contracts['97']['testnet']['contracts']
+const mainnet = testnet // contracts['56']['mainnet']['contracts']
 
 export const contracts = hardhat
 
 export const addresses = {
-  ZooToken: {
-    1337: hardhat['ZooToken']['address'], // local
-    97: testnet['ZooToken']['address'], // testnet
-    56: '0xB09FE1613fE03E7361319d2a43eDc17422f36B09', // mainnet
+  token: {
+    1337: hardhat['ZooV2']['address'], // local
+    97: testnet['ZooV2']['address'],   // testnet
+    56: mainnet['ZooV2']['address'],   // mainnet
   },
-  ZooMarket: {
-    1337: hardhat['ZooMarket']['address'],
-    97: testnet['ZooMarket']['address'],
-    56: mainnet['ZooMarket']['address'],
+  bridge: {
+    1337: hardhat['Bridge']['address'], // local
+    97: testnet['Bridge']['address'],   // testnet
+    56: mainnet['Bridge']['address'],   // mainnet
   },
-  ZooMedia: {
-    1337: hardhat['ZooMedia']['address'],
-    97: testnet['ZooMedia']['address'],
-    56: mainnet['ZooMedia']['address'],
+  dao: {
+    1337: hardhat['Dao']['address'], // local
+    97: testnet['Dao']['address'],   // testnet
+    56: mainnet['Dao']['address'],   // mainnet
   },
-  ZooAuction: {
-    1337: hardhat['ZooAuction']['address'],
-    97: testnet['ZooAuction']['address'],
-    56: mainnet['ZooAuction']['address'],
+  market: {
+    1337: hardhat['Market']['address'],
+    97: testnet['Market']['address'],
+    56: mainnet['Market']['address'],
   },
-  ZooDrop: {
-    1337: hardhat['ZooDrop']['address'],
-    97: testnet['ZooDrop']['address'],
-    56: mainnet['ZooDrop']['address'],
+  media: {
+    1337: hardhat['Media']['address'],
+    97: testnet['Media']['address'],
+    56: mainnet['Media']['address'],
   },
-  ZooFaucet: {
-    1337: hardhat['ZooFaucet']['address'],
-    97: testnet['ZooFaucet']['address'],
-    56: mainnet['ZooFaucet']['address'],
+  auction: {
+    1337: hardhat['Auction']['address'],
+    97: testnet['Auction']['address'],
+    56: mainnet['Auction']['address'],
   },
-  ZooKeeper: {
+  drop: {
+    1337: hardhat['Drop']['address'],
+    97: testnet['Drop']['address'],
+    56: mainnet['Drop']['address'],
+  },
+  faucet: {
+    1337: hardhat['Faucet']['address'],
+    97: testnet['Faucet']['address'],
+    56: mainnet['Faucet']['address'],
+  },
+  zooKeeper: {
     1337: hardhat['ZooKeeper']['address'],
     97: testnet['ZooKeeper']['address'],
     56: mainnet['ZooKeeper']['address'],
