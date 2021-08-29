fe99c15bd1dfa5385f69ba21507355c7d8101bf8
Zach Kelling <z@zeekay.im>
2021-08-29 18:16:46 -0400

    Prepare for ETH deployments

--- packages/contracts/hardhat.network.ts
+++ packages/contracts/hardhat.network.ts
@@ -2,8 +2,6 @@ import { HardhatUserConfig } from 'hardhat/config'
 
 import fs from 'fs'
 
-const alchemyKey = 'EuD-FVgI2gMBGf0aypDghsPHYWHB9nhn'
-
 function mnemonic() {
   try {
     return fs.readFileSync(`./mnemonic.txt`).toString().trim()
@@ -35,36 +33,36 @@ const networks: HardhatUserConfig['networks'] = {
     blockGasLimit: 200000000,
     allowUnlimitedContractSize: true,
   },
-  mainnet: {
-    url: 'https://bsc-dataseed.binance.org/',
+  bsc: {
+    url: 'https://speedy-nodes-nyc.moralis.io/2d2690514d99c11ad72d31fd/bsc/mainnet',
     chainId: 56,
     accounts: {
       mnemonic: mnemonic(),
     },
   },
   testnet: {
-    url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+    url: 'https://speedy-nodes-nyc.moralis.io/2d2690514d99c11ad72d31fd/bsc/testnet',
     chainId: 97,
-    gasPrice: 12e9,
-    gas: 20e6,
+    accounts: {
+      mnemonic: mnemonic(),
+    },
+    // gasPrice: 12e9,
+    // gas: 20e6,
+  },
+  eth: {
+    url: 'https://speedy-nodes-nyc.moralis.io/2d2690514d99c11ad72d31fd/eth/mainnet',
+    chainId: 1,
+    accounts: {
+      mnemonic: mnemonic(),
+    },
+  },
+  ropsten: {
+    url: 'https://speedy-nodes-nyc.moralis.io/2d2690514d99c11ad72d31fd/eth/ropsten',
+    chainId: 3,
     accounts: {
       mnemonic: mnemonic(),
     },
   },
 }
-
-// if (process.env.FORK_ENABLED == "true") {
-//   networks.hardhat = {
-//     chainId: 1,
-//     forking: {
-//       url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyKey}`,
-//       // blockNumber: 12226812
-//     },
-//     accounts: {
-//       mnemonic,
-//     },
-//   }
-// }  else {
-// }
 
 export default networks
