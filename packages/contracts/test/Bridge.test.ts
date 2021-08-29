242f6ab457de6074f56a435ec054601430f09573
Ari Lerner <me@ari.io>
2021-08-29 16:03:45 -0400

    Updated bridge test

--- packages/contracts/test/Bridge.test.ts
+++ packages/contracts/test/Bridge.test.ts
@@ -39,26 +39,6 @@ const generateTokens = (token: any, count: number = 2, custom: TokenCustomizatio
 }
 
 describe.only('Bridge', function () {
-
-    const bridge = tokens.Bridge
-    const token = tokens.ZooV2
-    token.configure(bridge.address)
-
-    const [user1, user2] = signers
-    const address1 = user1.address
-    const address2 = user2.address
-
-    // Mint tokens to user1
-    token.mint(address1, 10000)
-    await token.approve(bridge.address, 200)
-
-    const [tokenA, tokenB] = generateTokens(token, 2)
-    await bridge.setToken(tokenA)
-    await bridge.setToken(tokenB)
-
-    await expect(bridge.swap(tokenA, tokenB, address2, 100, 1)).to.not.be.rejected
-  })
-
   it('explodes if not approved', async () => {
     const {
       signers,
