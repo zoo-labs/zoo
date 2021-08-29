706d36c1037c6d355972c24d478c7a5eaf033f16
Zach Kelling <z@zeekay.im>
2021-08-29 15:52:47 -0400

    Add 0.4.24 compiler

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -29,6 +29,15 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        version: '0.4.24',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
         version: '0.6.12',
         settings: {
           optimizer: {
