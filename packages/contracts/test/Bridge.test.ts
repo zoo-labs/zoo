0b42f0c5d040f980e7ae2b436c4942f070755826
Ari Lerner <me@ari.io>
2021-08-29 00:59:19 -0400

    Updated Bridge test with two working tests

--- packages/contracts/test/Bridge.test.ts
+++ packages/contracts/test/Bridge.test.ts
@@ -7,6 +7,21 @@ const { expect } = requireDependencies()
 
 const setupTest = setupTestFactory(['Bridge', 'ZooTokenV2'])
 
+const generateTokens = (token: any, count: number = 2) => {
+  // Lame version
+  let tokens = []
+  for (let i = 0; i < count; i++) {
+    tokens.push({
+      kind: 0,
+      id: i,
+      chainID: 1337 + i,
+      tokenAddress: token.address,
+      enabled: true,
+    })
+  }
+  return tokens
+}
+
 describe.only('Bridge', function () {
   it('it should swap tokens', async function () {
     const { signers, tokens } = await setupTest()
@@ -20,26 +35,28 @@ describe.only('Bridge', function () {
 
     // Mint tokens to user1
     token.mint(address1, 10000)
-    await token.approve(bridge.address, 200);
+    await token.approve(bridge.address, 200)
 
-    const tokenA = {
-      kind: 0,
-      id: 0,
-      chainID: 1337,
-      tokenAddress: token.address,
-      enabled: true,
-    }
-    const tokenB = {
-      kind: 0,
-      id: 1,
-      chainID: 1338,
-      tokenAddress: token.address,
-      enabled: true,
-    }
-
+    const [tokenA, tokenB] = generateTokens(token, 2)
     await bridge.setToken(tokenA)
     await bridge.setToken(tokenB)
 
-    await bridge.swap(tokenA, tokenB, address2, 100, 1)
+    await expect(bridge.swap(tokenA, tokenB, address2, 100, 1)).to.not.be.rejected
+  })
+
+  it('explodes if not approved', async () => {
+    const {
+      signers,
+      tokens: { Bridge, ZooTokenV2 },
+    } = await setupTest()
+    const [user1, user2] = signers
+    ZooTokenV2.mint(user1.address, 100)
+
+    const [tokenA, tokenB] = generateTokens(ZooTokenV2, 2)
+
+    await Bridge.setToken(tokenA)
+    await Bridge.setToken(tokenB)
+    await expect(Bridge.swap(tokenA, tokenB, user2.address, 100, 1))
+    .to.rejectedWith('ERC20: burn amount exceeds allowance')
   })
 })
