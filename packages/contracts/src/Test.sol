cbeebad7ea09ed43ab6679825fc2609d6611a532
Zach Kelling <z@zeekay.io>
2021-08-28 21:41:17 -0400

    Initial support for NFT swapping

--- packages/contracts/src/Test.sol
+++ packages/contracts/src/Test.sol
@@ -2,12 +2,12 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "./console.log";
+import "./console.sol";
 
 contract Test {
     constructor() {
-        bytes4 burn = Hash(bytes4(keccak256('burn(address, uint256)')));
-        bytes4 mint = Hash(bytes4(keccak256('mint(address, uint256)')));
-        console.log(burn ^ mint);
+        bytes4 burn = bytes4(keccak256('burn(address, uint256)'));
+        bytes4 mint = bytes4(keccak256('mint(address, uint256)'));
+        console.logBytes4(burn ^ mint);
     }
 }
