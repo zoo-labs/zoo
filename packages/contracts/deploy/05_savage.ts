b2882b8d3f612074cf52a4e0f97f98454d07aee1
Zach Kelling <z@zeekay.io>
2021-08-30 03:50:57 -0400

    Ensure mainnet addresses right on deploy

--- packages/contracts/deploy/05_savage.ts
+++ packages/contracts/deploy/05_savage.ts
@@ -2,15 +2,17 @@
 
 import { Deploy } from '@zoolabs/contracts/utils/deploy'
 
-export default Deploy('Savage', [], async({ hre, deploy, deployments, deps }) => {
-  const token = deps.Savage
+export default Deploy('Savage', ['B', 'Z', 'UniswapV2Factory', 'UniswapV2Router02'], async({ hre, deploy, deployments, deps }) => {
+  const { B, Z, UniswapV2Factory, UniswapV2Router02 } = deps
 
-  const B  = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
-  const Z  = '0x8e7788ee2b1d3e5451e182035d6b2b566c2fe997'
-  const PF = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
-  const PR = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
-  const A  =  100000000000
-  const M  =  90000000000
+  const isMainnet = hre.network == 'mainnet'
 
-  await deploy([B, Z, PF, PR, A, M])
+  const b  = isMainnet ? '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c' : B.address
+  const z  = isMainnet ? '0x8e7788ee2b1d3e5451e182035d6b2b566c2fe997' : Z.address
+  const f  = isMainnet ? '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73' : UniswapV2Factory.address
+  const r  = isMainnet ? '0x10ED43C718714eb63d5aA57B78B54704E256024E' : UniswapV2Router02.address
+  const a  =  100000000000
+  const m  =  90000000000
+
+  await deploy([b, z, f, r, a, m])
 })
