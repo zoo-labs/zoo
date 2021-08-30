39bba640250b9d7f2dc2e6806e70f71b83fdca11
Zach Kelling <z@zeekay.im>
2021-08-29 21:12:06 -0400

    Sushi inject (wip)

---
+++ packages/legacy/src/App.tsx
@@ -0,0 +1,212 @@
+import React, { Suspense, useEffect, lazy } from 'react'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
+import styled from 'styled-components'
+import BigNumber from 'bignumber.js'
+import useEagerConnect from 'hooks/useEagerConnect'
+import ResetCSS from './components/ResetCSS'
+import GlobalStyle from './components/style/Global'
+import Menu from './components/Menu'
+import SuspenseWithChunkError from './components/SuspenseWithChunkError'
+import ToastListener from './components/ToastListener'
+import PageLoader from './components/Svg/Icons/LoadingLogo'
+import history from './routerHistory'
+import { PrivateRoute } from 'components/PrivateRoute'
+import { useWeb3React } from '@web3-react/core'
+import { useDispatch } from 'react-redux'
+import { clearZoo } from 'state/zoo'
+import { addEggs, addAnimals, addEgg, addAnimal, burnEgg, burnAnimal } from 'state/actions'
+import Moralis from 'moralis'
+import { useMoralisSubscription } from 'react-moralis'
+import { Egg, Animal } from 'types/zoo'
+import { getZooKeeper } from 'util/contracts'
+import useWeb3 from 'hooks/useWeb3'
+import AppBar from './components/AppBar/AppBar'
+
+import { mapEgg, mapAnimal, queryEggs, queryAnimals } from 'util/moralis'
+
+const Account = lazy(() => import('./views/Account'))
+const Login = lazy(() => import('./views/Login'))
+const Bank = lazy(() => import('./views/Bank'))
+const Feed = lazy(() => import('./views/Feed'))
+
+// This config is required for number formating
+BigNumber.config({
+  EXPONENTIAL_AT: 1000,
+  DECIMAL_PLACES: 80,
+})
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+  overflow-x: hidden;
+`
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  flex: 1;
+  overflow-y: auto;
+  overflow-x: hidden;
+  z-index: 1;
+  justify-content: center;
+  background-repeat: no-repeat;
+  background-position: bottom 24px center;
+  background-size: 90%;
+  ${({ theme }) => theme.mediaQueries.xs} {
+    background-size: auto;
+  }
+  ${({ theme }) => theme.mediaQueries.lg} {
+    background-repeat: no-repeat;
+    background-position: center 420px, 10% 230px, 90% 230px;
+    background-size: contain, 266px, 266px;
+    min-height: 90vh;
+  }
+`
+
+const Marginer = styled.div`
+  margin-top: 5rem;
+`
+
+const App: React.FC = () => {
+  useEagerConnect()
+  const web3 = useWeb3()
+  const { chainID } = web3
+  const dispatch = useDispatch()
+  const signedIn = chainID && window.localStorage.getItem('connectorId')
+
+  const getEggs = async () => {
+    console.log('GETTING EGGS')
+    try {
+      const eggs = []
+      for (const egg of await queryEggs()) {
+        eggs.push(mapEgg(egg))
+      }
+      dispatch(addEggs(eggs))
+    } catch (e) {
+      console.error('ISSUE GETTING EGGS \n', e)
+    }
+  }
+
+  const getAnimals = async () => {
+    console.log('GETTING ANIMALS')
+
+    try {
+      const animals = []
+      for (const animal of await queryAnimals()) {
+        animals.push(mapAnimal(animal))
+      }
+      dispatch(addAnimals(animals))
+    } catch (e) {
+      console.error('ISSUE GETTING ANIMAL \n', e)
+    }
+  }
+
+  const createEgg = async (data) => {
+    console.log('CREATING EGG', mapEgg(data))
+    try {
+      dispatch(addEgg(mapEgg(data)))
+    } catch (e) {
+      console.error('ISSUE CREATING EGG:', e)
+    }
+  }
+
+  const updateEgg = async (data) => {
+    console.log('UPDATING EGG', mapEgg(data))
+    try {
+      dispatch(addEgg(mapEgg(data)))
+    } catch (e) {
+      console.error('ISSUE UPDATING EGG:', e)
+    }
+  }
+
+  const createAnimal = async (data) => {
+    console.log('CREATING ANIMAL', mapAnimal(data))
+    try {
+      dispatch(addAnimal(mapAnimal(data)))
+    } catch (e) {
+      console.error('ISSUE CREATING ANIMAL:', e)
+    }
+  }
+
+  const updateAnimal = async (data) => {
+    console.log('UPDATING ANIMAL', mapAnimal(data))
+    try {
+      dispatch(addAnimal(mapAnimal(data)))
+    } catch (e) {
+      console.error('ISSUE UPDATING ANIMAL:', e)
+    }
+  }
+
+  const deleteAnimal = async (data) => {
+    console.log('DELETING ANIMAL', mapAnimal(data))
+    try {
+      dispatch(burnAnimal(mapAnimal(data)))
+    } catch (e) {
+      console.error('ISSUE DELETING ANIMAL:', e)
+    }
+  }
+
+  const deleteEgg = async (data) => {
+    console.log('DELETING EGG', mapEgg(data))
+    try {
+      dispatch(burnEgg(mapEgg(data)))
+    } catch (e) {
+      console.error('ISSUE DELETING EGG:', e)
+    }
+  }
+
+  useMoralisSubscription('Eggs', (q) => q, [], {
+    onCreate: (data) => createEgg(data),
+    onUpdate: (data) => updateEgg(data),
+    onDelete: (data) => deleteEgg(data),
+  })
+
+  useMoralisSubscription('Animals', (q) => q, [], {
+    onCreate: (data) => createAnimal(data),
+    onUpdate: (data) => updateAnimal(data),
+    onDelete: (data) => deleteAnimal(data),
+  })
+
+  useEffect(() => {
+    dispatch(clearZoo())
+    console.warn = () => null  // TODO: fix MaxListenersExceededWarning
+    getEggs()
+    getAnimals()
+  }, [chainID])
+
+  function requireLogin(component) { return (signedIn ? component : <Redirect to='/login'/>) }
+
+  return (
+    <Suspense fallback={null}>
+      <Router history={history}>
+        <ResetCSS/>
+        <GlobalStyle/>
+<<<<<<< HEAD:packages/legacy/src/App.tsx
+        <AppBar/>
+=======
+        <Heading/>
+>>>>>>> f4f637d (Sushi inject (wip)):packages/app/src/App.tsx
+        <Switch>
+          <Route exact path='/login'>{signedIn ? <Redirect to='/account' /> : <Login />}</Route>
+          <SuspenseWithChunkError fallback={<></>}>
+            <Route path='/account'>{requireLogin(<Account/>)}</Route>
+            <Route path='/feed'>{requireLogin(<Feed/>)}</Route>
+            <Route path='/bank'>{requireLogin(<Bank/>)}</Route>
+            <Route exact path='/'>{requireLogin(<Redirect to='/account'/>)}</Route>
+          </SuspenseWithChunkError>
+        </Switch>
+        <ToastListener/>
+<<<<<<< HEAD:packages/legacy/src/App.tsx
+        {/* <Footer> */}
+=======
+        <Footer>
+>>>>>>> f4f637d (Sushi inject (wip)):packages/app/src/App.tsx
+      </Router>
+    </Suspense>
+  )
+}
+
+export default React.memo(App)
---
+++ packages/legacy/src/views/Bank.tsx
@@ -0,0 +1,365 @@
+import BorderButton from 'components/Button/BorderButton'
+import styled from 'styled-components'
+import StickyBottomMenu from 'components/Button/StickyBottomMenu'
+import Page from 'components/layout/Page'
+import React, { useState, useEffect } from 'react'
+import { AppState } from 'state'
+import { useSelector } from 'react-redux'
+import { useWeb3 } from 'hooks/useWeb3'
+import { useHistory } from 'react-router-dom'
+import styles from 'styled-components'
+import { Label, Text } from 'components/Text'
+import { Flex, Heading } from 'components'
+import Body from 'components/layout/Body'
+import { useModal } from 'components/Modal'
+import BuyEggs from 'components/BuyEggs'
+import { getZooToken, getZooFaucet } from 'util/contracts'
+import { useMatchBreakpoints } from 'components'
+import { FaHome } from 'react-icons/fa'
+import Moralis from 'moralis'
+import { resourceLimits } from 'worker_threads'
+
+const HeadingContainer = styles.div`
+    width: 100%;
+    display: flex;
+    justify-content: start;
+    margin: 0px 8px;
+`
+
+const StyledButton = styles.button`
+    cursor: pointer;
+    text-decoration: underline;
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.colors.text};
+    background: transparent;
+    border: none;
+    margin-left: 8px;
+`
+
+const LabelWrapper = styles.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    align-items: center;
+    color: ${({ theme }) => theme.colors.text};
+`
+
+const ValueWrapper = styles(Text)`
+    color: ${({ theme }) => theme.colors.text};
+    width: 100%;
+    display: flex;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding: 16px;
+    font-size: 18px;
+`
+
+const EarnerValueWrapper = styles(Text)`
+   color: ${({ theme }) => theme.colors.text};
+   width: 100%;
+   flex-direction: column;
+   overflow: hidden;
+   text-overflow: ellipsis;
+   padding: 12px 16px;
+`
+
+const EarnerValue = styles(Text)`
+   color: ${({ theme }) => theme.colors.text};
+   font-size: 18px;
+   line-height: 1.8;
+`
+
+const Container = styled.div`
+  filter: ${({ theme }) => theme.card.dropShadow};
+  width: 100%;
+  background: ${({ theme }) => theme.card.background};
+  border-radius: 16px;
+  min-width: 280px;
+`
+
+const TableWrapper = styled.div`
+  flex-direction: row;
+  flex: 1 1 auto;
+  -webkit-box-pack: start;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+const StyledTable = styled.table`
+  display: flex;
+  border-collapse: collapse;
+  font-size: 14px;
+  border-radius: 16px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  position: relative;
+  overflow-x: scroll;
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px ${({ theme }) => theme.colors.primaryPop};
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.primaryPop};
+  }
+  &::-webkit-scrollbar {
+    height: 7px;
+  }
+`
+
+const TableBody = styled.tbody`
+  width: fit-content;
+  & tr {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    td {
+      font-size: 16px;
+      vertical-align: left;
+    }
+    th {
+      vertical-align: left;
+    }
+  }
+`
+
+const TableRow = styled.tr`
+  color: white;
+`
+
+const TableContainer = styled.div`
+  position: relative;
+`
+
+const TableData = styled.td`
+  width: 100px;
+  margin: 5px 10px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
+const TableHeader = styled.th`
+  width: 100px;
+  margin: 15px 0px;
+  font-weight: 700;
+  text-align: left;
+  font-size: 18px;
+`
+
+const TableText = styled(Text)`
+  width: 100%;
+  text-align: center;
+  margin: 20px 0px;
+  font-size: 16px;
+`
+
+const StyledHeading = styled(Heading)`
+  color: ${({ theme }) => theme.colors.text};
+`
+
+const StyledText = styled(Text)`
+  color: ${({ theme }) => theme.colors.text};
+`
+
+function numberWithCommas(num) {
+  const values = num.toString().split('.')
+  return values[0].replace(/.(?=(?:.{3})+$)/g, '$&,') + (values.length == 2 ? '.' + values[1] : '')
+}
+
+const Bank: React.FC = () => {
+  const web3 = useWeb3()
+  const { account, chainID } = web3
+  const history = useHistory()
+  const { isXl } = useMatchBreakpoints()
+
+  const animalsState = useSelector<AppState, AppState['zoo']['animals']>((state) => state.zoo.animals)
+
+  const [zooBalance, setBalance] = useState(0.0)
+  const [wait, setWait] = useState(false)
+  const [transactions, setTransactions] = useState([])
+  const [waitTx, setWaitTx] = useState(true)
+
+  const zooToken = getZooToken(web3)
+  const faucet = getZooFaucet(web3)
+
+  const accountAnimals = Object.values(animalsState).filter((animal) => {
+    return animal.owner && animal.owner.toLowerCase() === account.toLowerCase()
+  })
+
+  const getBalance = async () => {
+    try {
+      const decimals = await zooToken.methods.decimals().call()
+      const rawBalance = await zooToken.methods.balanceOf(account).call()
+      const divisor = parseFloat(Math.pow(10, decimals).toString())
+      const balance = rawBalance / divisor
+      setBalance(balance)
+    } catch (e) {
+      console.error('ISSUE LOADING ZOO BALANCE \n', e)
+    }
+  }
+
+  useEffect(() => {
+    getBalance()
+    getTransactions()
+  }, [account])
+
+  useEffect(() => {
+    getBalance()
+  }, [])
+
+  const handleFaucet = () => {
+    try {
+      setWait(true)
+      faucet.methods
+        .fund(account)
+        .send({ gas: 21000, from: account })
+        .then(() => {
+          setWait(false)
+          getBalance()
+        })
+        .catch((e) => {
+          console.error('ISSUE USING FAUCET \n', e)
+          setWait(false)
+        })
+      getBalance() // update balance
+    } catch (e) {
+      console.error('ISSUE USING FAUCET \n', e)
+    }
+  }
+
+  const handleFunds = () => {
+    switch (chainID) {
+      case 1337:
+        handleFaucet()
+        break
+      case 97:
+        handleFaucet()
+        break
+      default:
+        location.href = 'https://pancakeswap.info/token/0x8e7788ee2b1d3e5451e182035d6b2b566c2fe997'
+    }
+  }
+
+  const getTransactions = async () => {
+    console.log('GETTING TRANSACTIONS for account', account)
+    try {
+      const Transactions = Moralis.Object.extend('Transactions')
+      const query = new Moralis.Query(Transactions)
+      query.limit(1000)
+      query.descending('createdAt')
+      query.equalTo('from', account.toLowerCase())
+      const results = await query.find()
+      let transactions = []
+      for (const tx of results) {
+        const action = tx.get('action')
+        const txHash = tx.get('transactionHash')
+        const URL = `https://testnet.bscscan.com/tx/${txHash}`
+
+        // Filter out Burned Tokens
+        if (action == 'Burned Token') continue
+
+        console.log('tx', tx)
+        transactions.push({
+          id: tx.get('objectId'),
+          from: tx.get('from'),
+          action: action,
+          hash: txHash,
+          URL: URL,
+          createdAt: tx.get('createdAt').toLocaleDateString(),
+          blockNumber: tx.get('blockNumber'),
+          timestamp: tx.get('timestamp'),
+          tokenID: tx.get('tokenID'),
+        })
+      }
+      console.log('transactions', transactions)
+      setTransactions(transactions)
+      setWaitTx(false)
+    } catch (e) {
+      console.error('ISSUE GETTING TRANSACTIONS \n', e)
+    }
+  }
+
+  // Calculate yield
+  const dailyYield = accountAnimals.reduce((acc, x) => acc + Number(x.yield), 0)
+
+  // Get top ten animals
+  const topTenAnimals = accountAnimals.sort((a, b) => Number(b.yield) - Number(a.yield)).slice(0, 10)
+
+  return (
+    <>
+      <Page>
+        <Body>
+          <LabelWrapper>
+            <Label small>Wallet Balance</Label>
+
+            <BorderButton scale='sm' minWidth={!isXl ? '120px' : '140px'} style={{ fontSize: `${!isXl ? '14px' : '16px'}` }} onClick={handleFunds}>
+              {chainID !== 97 ? 'Buy ZOO' : wait ? 'Processing' : 'Get ZOO'}
+            </BorderButton>
+          </LabelWrapper>
+          <Flex width='100%' alignItems='center' justifyContent='space-around'>
+            <ValueWrapper>{numberWithCommas(zooBalance)} ZOO </ValueWrapper>
+            {/* Commented out since there is to ZOO to USD conversion yet */}
+            {/* <ValueWrapper style={{ fontSize: "16px",  color: "rgb(221 224 26)" }}>0 USD</ValueWrapper> */}
+          </Flex>
+          <Label small>Total Daily Yield</Label>
+          <ValueWrapper> {dailyYield} ZOO </ValueWrapper>
+          <Label small>Top Earners</Label>
+          {topTenAnimals.length === 0 ? (
+            <ValueWrapper style={{ justifyContent: 'center' }}> No animals </ValueWrapper>
+          ) : (
+            <EarnerValueWrapper>
+              {topTenAnimals.map((animal) => {
+                return (
+                  <EarnerValue key={animal.tokenID + '_earner_'}>
+                    {animal.name} - {animal.yield}/day
+                  </EarnerValue>
+                )
+              })}
+            </EarnerValueWrapper>
+          )}
+          <Label small>Recent Tansactions</Label>
+          {waitTx ? (
+            <TableText> Loading Transactions... </TableText>
+          ) : transactions.length === 0 ? (
+            <TableText> No Transaction Data </TableText>
+          ) : (
+            <Container>
+              <TableContainer>
+                <TableWrapper>
+                  <StyledTable>
+                    <TableBody>
+                      <TableRow>
+                        <TableHeader>Tx Hash</TableHeader>
+                        <TableHeader>Action</TableHeader>
+                        <TableHeader>Block Number</TableHeader>
+                        <TableHeader>Token ID</TableHeader>
+                      </TableRow>
+                      {transactions.map((tx) => {
+                        return (
+                          <TableRow key={tx.id}>
+                            <TableData>
+                              <a href={tx.url}>{tx.hash}</a>
+                            </TableData>
+                            <TableData>{tx.action}</TableData>
+                            <TableData>{tx.blockNumber}</TableData>
+                            <TableData>
+                              <a href={`/feed/myzoo/${tx.tokenID}`}>{tx.tokenID}</a>
+                            </TableData>
+                          </TableRow>
+                        )
+                      })}
+                    </TableBody>
+                  </StyledTable>
+                </TableWrapper>
+              </TableContainer>
+            </Container>
+          )}
+        </Body>
+      </Page>
+    </>
+  )
+}
+
+export default Bank
