4ed8a0b17c50bca94e8386f5e0ecd4bc4a35444c
Zach Kelling <z@zeekay.im>
2021-08-30 02:10:59 -0400

    Update deployment contracts

---
+++ packages/contracts/deploy/04_v2router02.js
@@ -0,0 +1,32 @@
+// deploy/04_v2router02.ts
+const { WNATIVE } = require("@sushiswap/sdk");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  const chainId = await getChainId();
+
+  let wethAddress;
+
+  if (chainId === "31337") {
+    wethAddress = (await deployments.get("WETH9Mock")).address;
+  } else if (chainId in WNATIVE) {
+    wethAddress = WNATIVE[chainId].address;
+  } else {
+    throw Error("No WNATIVE!");
+  }
+
+  const factoryAddress = (await deployments.get("UniswapV2Factory")).address;
+
+  await deploy("UniswapV2Router02", {
+    from: deployer,
+    args: [factoryAddress, wethAddress],
+    log: true,
+    deterministicDeployment: false,
+  });
+};
+
+module.exports.tags = ["UniswapV2Router02", "AMM"];
+module.exports.dependencies = ["UniswapV2Factory", "Mocks"];
