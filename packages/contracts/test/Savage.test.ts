52816c04c708de34b4e9c44263db645a2fb98f65
Zach Kelling <z@zeekay.im>
2021-08-30 00:09:11 -0400

    Updates for Sava test

--- packages/contracts/test/Savage.test.ts
+++ packages/contracts/test/Savage.test.ts
@@ -1,20 +1,24 @@
 import { setupTestFactory, requireDependencies } from './utils'
 import { Signer } from '@ethersproject/abstract-signer'
-import { Savage } from '../types'
+import { Savage, Token } from '../types'
 
 import { BigNumber } from 'ethers';
 const { expect } = requireDependencies()
 const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
 
-const setupTest = setupTestFactory(['Savage'])
+const setupTest = setupTestFactory(['Savage', 'Token'])
 
 describe.only('Savage', function () {
   let savage: Savage
+  let Z: Token
+  let B: Token
   let signers: Signer[]
 
   beforeEach(async () => {
     const test = await setupTest()
     savage = test.tokens.Savage as Savage
+    Z = test.tokens.Token as Token
+    B = test.tokens.Token as Token
     signers = test.signers
   })
 
