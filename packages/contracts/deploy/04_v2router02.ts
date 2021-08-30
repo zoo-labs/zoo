7a574ce9d5747080a054154659ae07e7e42c87ec
Zach Kelling <z@zeekay.im>
2021-08-30 04:37:53 -0400

    Update deploy

--- packages/contracts/deploy/04_v2router02.ts
+++ packages/contracts/deploy/04_v2router02.ts
@@ -1,32 +1,37 @@
 // deploy/04_v2router02.ts
-import { WNATIVE } from "@sushiswap/sdk"
+// import { WNATIVE } from "@sushiswap/sdk"
 
-export default async function ({ getChainId, getNamedAccounts, deployments }) {
-  const { deploy } = deployments;
+// export default async function ({ getChainId, getNamedAccounts, deployments }) {
+//   const { deploy } = deployments;
 
-  const { deployer } = await getNamedAccounts();
+//   const { deployer } = await getNamedAccounts();
 
-  const chainId = await getChainId();
 
-  let wethAddress;
+//   await deploy("UniswapV2Router02", {
+//     from: deployer,
+//     args: [factoryAddress, wethAddress],
+//     log: true,
+//     deterministicDeployment: false,
+//   });
+// };
 
-  if (chainId === "1337") {
-    wethAddress = (await deployments.get("WETH")).address;
-  } else if (chainId in WNATIVE) {
-    wethAddress = WNATIVE[chainId].address;
-  } else {
-    throw Error("No WNATIVE!");
-  }
+// module.exports.tags = ["UniswapV2Router02", "AMM"];
+// module.exports.dependencies = ["UniswapV2Factory", "WETH"];
 
-  const factoryAddress = (await deployments.get("UniswapV2Factory")).address;
+import { Deploy } from '@zoolabs/contracts/utils/deploy'
 
-  await deploy("UniswapV2Router02", {
-    from: deployer,
-    args: [factoryAddress, wethAddress],
-    log: true,
-    deterministicDeployment: false,
-  });
-};
+export default Deploy('UniswapV2Router02', ['WETH', 'UniswapV2Factory'], async({ getChainId, hre, deploy, deployments, deps }) => {
+  const { WETH, UniswapV2Factory } = deps
+  const chainId = await getChainId()
 
-module.exports.tags = ["UniswapV2Router02", "AMM"];
-module.exports.dependencies = ["UniswapV2Factory", "WETH"];
+  // let wethAddress
+
+  // if (chainId === "1337") {
+  //   wethAddress = (await deployments.get("WETH")).address;
+  // } else if (chainId in WNATIVE) {
+  //   wethAddress = WNATIVE[chainId].address
+  // } else {
+  //   throw Error("No WNATIVE!")
+  // }
+  await deploy([UniswapV2Factory.address, WETH.address])
+})
