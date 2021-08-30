5c228a1ed2589975d315b28564cff121597ef8ac
Zach Kelling <z@zeekay.io>
2021-08-30 12:59:35 -0400

    Update tests

--- packages/contracts/deploy/06_savage.ts
+++ packages/contracts/deploy/06_savage.ts
@@ -11,8 +11,8 @@ export default Deploy('Savage', ['B', 'Z', 'UniswapV2Factory', 'UniswapV2Router0
   const z  = isMainnet ? '0x8e7788ee2b1d3e5451e182035d6b2b566c2fe997' : Z.address
   const f  = isMainnet ? '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73' : UniswapV2Factory.address
   const r  = isMainnet ? '0x10ED43C718714eb63d5aA57B78B54704E256024E' : UniswapV2Router02.address
-  const a  =  100000000000
-  const m  =  90000000000
+  const a  =  100
+  const m  =  90
 
   await deploy([b, z, f, r, a, m])
 })
