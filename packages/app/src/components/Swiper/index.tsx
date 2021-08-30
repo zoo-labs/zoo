87f0db323826159e69ffd9bf30bcc6ea8182fb3d
Zach Kelling <z@zeekay.io>
2021-08-30 05:05:13 -0400

    Add eth-scaffold

--- packages/app/src/components/Swiper/index.tsx
+++
@@ -1,7 +0,0 @@
-// import Swiper core and required modules
-// import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
-
-import { Swiper as _Swiper, SwiperSlide as _Slide } from 'swiper/react/swiper-react.js'
-
-export const Swiper = _Swiper
-export const Slide = _Slide
--- packages/app/src/functions/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './currency'
-export * from './format'
-export * from './parse'
-export * from './validate'
-export * from './contract'
