b2411aa412e25c12524baff5b9c8048c16fbb629
Zach Kelling <z@zeekay.io>
2021-08-30 20:18:33 -0400

    Add pool launch method

--- packages/contracts/src/Savage.sol
+++ packages/contracts/src/Savage.sol
@@ -2,6 +2,7 @@
 pragma solidity =0.6.12;
 
 import { UniswapV2Pair } from "./uniswapv2/UniswapV2Pair.sol";
+import { IUniswapV2Factory } from "./uniswapv2/interfaces/IUniswapV2Factory.sol";
 import { IUniswapV2Router01 } from "./uniswapv2/interfaces/IUniswapV2Router01.sol";
 import { TransferHelper } from "./uniswapv2/libraries/TransferHelper.sol";
 import { IERC20 } from "./uniswapv2/interfaces/IERC20.sol";
@@ -13,27 +14,50 @@ contract Savage {
     using SafeMath  for uint;
     using SafeMath  for uint8;
 
-    IERC20  A;                 // from A
-    IERC20  B;                 // to B
+    IERC20 A;                 // from A
+    IERC20 B;                 // to B
+    IERC20 C;                 // to C
+
     IUniswapV2Router01 Router; // router
+    IUniswapV2Factory Factory; // factory
 
     address a;
     address b;
+    address c;
     address router;
+    address factory;
+    address owner;
 
     event SwapTokens(uint256 amountIn, uint256 amountOutMin);
+    event Pair(address pair, address b, address c);
+    event Liquidity(address b, uint256 bAmount, address c, uint256 cAmount);
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Savage: Only owner has access");
+        _;
+    }
 
     // Setup swap
-    constructor(address _a, address _b, address _router) public {
+    constructor(address _a, address _b, address _factory, address _router) public {
+        owner = msg.sender;
         A = IERC20(_a);
         B = IERC20(_b);
-        Router = IUniswapV2Router01(_router);
+        Factory = IUniswapV2Factory(_factory);
+        Router  = IUniswapV2Router01(_router);
+
         a = _a;
         b = _b;
+        factory = _factory;
         router = _router;
     }
 
-    // get path A -> B
+    // Configure new token
+    function configure(address _c) public onlyOwner {
+        c = _c;
+        C = IERC20(_c);
+    }
+
+    // Get path A -> B
     function getPath() private view returns (address[] memory) {
         address[] memory path = new address[](2);
         path[0] = a;
@@ -41,19 +65,13 @@ contract Savage {
         return path;
     }
 
-
-    function approve() public {
-        console.log('approve');
-        TransferHelper.safeApprove(a, router, A.balanceOf(msg.sender));
-    }
-
-    // Maybe works
+    // Swap token A for token B
     function swapTokens(uint amountIn, uint amountOutMin) public {
-        console.log('swapTokens', amountIn, amountOutMin);
 
         // Calculate deadline
         uint deadline = block.timestamp + 15;
 
+        console.log('swapTokens', amountIn, amountOutMin);
         console.log('balanceOf msg.sender', A.balanceOf(msg.sender));
         console.log('balanceOf savage', A.balanceOf(address(this)));
         console.log('balanceOf router', A.balanceOf(router));
@@ -84,6 +102,49 @@ contract Savage {
         console.log('B balanceOf router', B.balanceOf(router));
 
         emit SwapTokens(amountIn, amountOutMin);
+    }
+
+    // Show current balances
+    function balanceZOO() public view returns (uint256) {
+        return A.balanceOf(address(this));
+    }
+
+    function balanceBNB() public view returns (uint256) {
+        return B.balanceOf(address(this));
+    }
+
+    function balanceZOOV2() public view returns (uint256) {
+        return C.balanceOf(address(this));
+    }
+
+    // Launch new pair and add liquidity
+    function launchPool() public onlyOwner returns (address) {
+        console.log('launchPool');
+
+        Factory.createPair(b, c);
+        console.log('Factory.createPair', b, c);
+
+        address pair = Factory.getPair(b, c);
+
+        console.log('Factory.getPair', pair);
+        emit Pair(pair, b, c);
+
+        uint amountB = B.balanceOf(address(this));
+        uint amountC = C.balanceOf(address(this));
+        uint deadline = block.timestamp + 15;
+
+        Router.addLiquidity(
+            b,
+            c,
+            amountB, amountC,
+            100, 100,
+            msg.sender,
+            deadline
+        );
+
+        console.log('Router.addLiquidity', amountB, amountC);
+        emit Liquidity(b, amountB, c, amountC);
+        return pair;
     }
 
     // Helper to show the init code for the UniswapV2Pair
