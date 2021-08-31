a5bc6c6c51faacf04c48e84075812072cbe5b9bb
Zach Kelling <z@zeekay.io>
2021-08-31 19:40:33 -0400

    Update contracts, lab

--- packages/contracts/src/test/BNB.sol
+++ packages/contracts/src/test/BNB.sol
@@ -4,8 +4,8 @@ pragma solidity >=0.8.4;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Z is ERC20 {
-    constructor () ERC20("Z", "Z") {}
+contract BNB is ERC20 {
+    constructor () ERC20("BNB", "BNB") {}
 
     function mint(address to, uint256 value) public {
         super._mint(to, value);
