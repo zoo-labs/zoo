949509ef9212c2be35aff7182c3da8a93adc2f6e
Julian <innovate.createJM@gmail.com>
2021-08-31 23:05:36 -0400

    cleaning ugly code

--- packages/app/src/views/Swap/Header.tsx
+++ packages/app/src/views/Swap/Header.tsx
@@ -29,21 +29,7 @@ interface ExchangeHeaderProps {
 }
 
 const ExchangeHeader: FC<ExchangeHeaderProps> = ({ input, output, allowedSlippage }) => {
-
-
-
-  // const getBalance = async () => {
-  //   try {
-  //     const decimals = await zooToken.methods.decimals().call()
-  //     const rawBalance = await zooToken.methods.balanceOf(account).call()
-  //     const divisor = parseFloat(Math.pow(10, decimals).toString())
-  //     const balance = rawBalance / divisor
-  //     setBalance(balance)
-  //   } catch (e) {
-  //     console.error('ISSUE LOADING ZOO BALANCE \n', e)
-  //   }
-  // }
-
+  
   const [balance, setBalance] = useState(0)
   const { chainId } = useWeb3React()
   
@@ -52,31 +38,17 @@ const ExchangeHeader: FC<ExchangeHeaderProps> = ({ input, output, allowedSlippag
 
   const zooToken = getToken(web3)
 
-  // const zooKeeper = getZooKeeper(web3)
-  // const zooDrop = getDrop(web3)
-  // const keeperAdd = zooKeeper.options.address
-  //   const router = useRouter()
-  
   const [animateWallet, setAnimateWallet] = useState(false)
   //   const isRemove = router.asPath.startsWith('/remove')
   //   const isLimitOrder = router.asPath.startsWith('/limit-order')
+
   const getBalance = async () => {
     try {
-      // const decimals = await zooToken.methods.decimals().call()
-      // setBalance(parseFloat(balance.toFixed(4)));
-
       const decimals = await zooToken.methods.decimals().call()
       const rawBalance = await zooToken.methods.balanceOf(account).call()
       const divisor = parseFloat(Math.pow(10, decimals).toString())
       const balance = rawBalance / divisor
       setBalance(balance)
-
-
-      // await eth.getBalance(account).then((val) => {
-      //   const divisor = parseFloat(Math.pow(10, 18).toString())
-      //   const balance = parseFloat(val) / divisor
-      //   setBalance(parseFloat(balance.toFixed(4)))
-      // })
     } catch (e) {
       console.error('ISSUE LOADING ZOO BALANCE \n', e)
     }
