afdc8e8740c80a54c79ebf1194335fbb2308da6d
Zach Kelling <z@zeekay.io>
2021-09-01 03:30:32 -0400

    Fix Savage test

--- packages/lab/src/App.jsx
+++ packages/lab/src/App.jsx
@@ -48,7 +48,7 @@ const { ethers } = require("ethers");
 */
 
 /// 📡 What chain are your contracts deployed to?
-const targetNetwork = NETWORKS.mainnet; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+const targetNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
 
 // 😬 Sorry for all the console logging
 const DEBUG = true;
