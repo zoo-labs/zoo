042aa8ef5caf90a570c601632912547c133fadd6
Zach Kelling <z@zeekay.im>
2021-08-31 20:49:28 -0400

    Update deployment scripts

--- packages/contracts/deploy/01_faucet.ts
+++ packages/contracts/deploy/01_faucet.ts
@@ -2,53 +2,26 @@
 
 import { Deploy } from '@zoolabs/contracts/utils/deploy'
 
-export default Deploy('Faucet', ['ZOO'], async({ hre, deploy, deployments, deps }) => {
-  const token = deps.ZOO
-  await deploy([token.address])
+export default Deploy('Faucet', ['ZOO'], async({ ethers, deploy, deployments, deps, hre }) => {
+  const token = await ethers.getContract('ZOO')
+
+  const tx = await deploy([token.address])
+
+  if (hre.network.name != 'hardhat') return
+
+  // 100B ZOO to faucet
+  const exp = ethers.BigNumber.from('10').pow(18)
+  const amount = ethers.BigNumber.from('100000000000').mul(exp)
+  await token.mint(tx.address, amount)
+
+  // Get signers to fund
+  const signers = await ethers.getSigners()
+
+  // 100M ZOO to each signer
+  const signerAmount = ethers.BigNumber.from('100000000').mul(exp)
+
+  // Mint new tokens
+  for (var i = 0; i < signers.length; i++) {
+    await token.mint(signers[i].address, signerAmount)
+  }
 })
-
-// const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-//   const { deployments, ethers, network, getNamedAccounts } = hre
-//   const { deploy } = deployments
-//   const { deployer } = await getNamedAccounts()
-
-//   const tokenAddress = (await deployments.get('ZOO')).address
-
-//   const deployResult = await deploy('Faucet', {
-//     from: deployer,
-//     args: [tokenAddress],
-//     log: true,
-//   })
-
-  // const { network } = hre
-  // if (network.name != 'hardhat') return
-
-//   // Get signers to fund
-//   const signers = await ethers.getSigners()
-
-//   // Get Token instance
-//   const token = await ethers.getContractAt('ZOO', tokenAddress)
-
-//   // Get Faucet instance
-//   const faucet = await ethers.getContractAt('Faucet', deployResult.address)
-
-//   // 100B ZOO to faucet
-//   const exp = ethers.BigNumber.from('10').pow(18)
-//   const faucetAmount = ethers.BigNumber.from('100000000000').mul(exp)
-
-//   // 100M ZOO to each signer
-//   const signerAmount = ethers.BigNumber.from('100000000').mul(exp)
-
-//   // Mint new tokens
-//   await token.mint(faucet.address, faucetAmount)
-//   for (var i = 0; i < signers.length; i++) {
-//     await token.mint(signers[i].address, signerAmount)
-//   }
-
-//   // return hre.network.live
-// }
-
-// export default func
-// func.id = 'faucet'
-// func.tags = ['Faucet']
-// func.dependencies = ['ZOO']
