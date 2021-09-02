3d7ec329b89a26fda201bdf6bb57151870e3b1d5
johneyo <johneyo57@yahoo.com>
2021-09-02 21:37:08 +0100

    hardhat

--- packages/app/src/components/Header/NetworkCard.tsx
+++ packages/app/src/components/Header/NetworkCard.tsx
@@ -4,6 +4,9 @@ import { NETWORK_ICON, NETWORK_LABEL } from '../../constants/networks'
 import React from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { useWalletModal } from 'components/WalletModal'
+// import { HardHatIcon } from 'components/SideMenu/icons'
+import HardHatIcon from '../../modals/icons/HardHat'
+
 // import { useActiveWeb3React } from '../../hooks/useActiveWeb3React'
 // import { useNetworkModalToggle } from '../../state/application/hooks'
 
@@ -20,7 +23,8 @@ function NetworkCard(): JSX.Element | null {
       className='flex items-center rounded-xl whitespace-nowrap text-sm font-medium cursor-pointer select-none pointer-events-auto bg-secondary mr-2 hover:bg-gray-800'
       onClick={() => onPresentNetworkModal()}>
       <div className='grid items-center grid-flow-col px-3 py-2 space-x-2 text-sm rounded-lg pointer-events-auto auto-cols-max bg-transparent text-secondary'>
-        <img src={NETWORK_ICON[chainId]} alt='Switch Network' className='rounded-md' width='22px' height='22px' />
+        {chainId === 1337 || 1338 ? <HardHatIcon /> : <img src={NETWORK_ICON[chainId]} alt='Switch Network' className='rounded-md' width='22px' height='22px' />}
+
         <div className='text-primary'>{NETWORK_LABEL[chainId]}</div>
       </div>
     </div>
