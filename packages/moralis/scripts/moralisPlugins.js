db35dac716a75f647249fb507fc38ad2c81a297e
Zach Kelling <z@zeekay.io>
2021-09-01 20:58:17 -0400

    Add new servers

--- packages/moralis/scripts/moralisPlugins.js
+++ packages/moralis/scripts/moralisPlugins.js
@@ -8,15 +8,19 @@ const { restartServer } = require('moralis-admin-cli/commands/common')
 
 const NETWORK = process.env.NETWORK ? process.env.NETWORK : 'localhost'
 const CHAIN_IDS = {
-  localhost: '0x539',
-  testnet: '0x61',
-  mainnet: '0x38',
+  hardhat:  '0x539',
+  testnet:  '0x61',
+  mainnet:  '0x38',
+  ethereum: '0x1',
+  ropsten:  '0x3',
 }
 const CHAIN_ID = CHAIN_IDS[NETWORK]
 const SERVER_NAME = {
-  localhost: 'zoo Hardhat',
-  testnet: 'Zoo Testnet',
-  mainnet: 'Zoo Mainnet',
+  hardhat:  'Zoo Hardhat',
+  testnet:  'Zoo Testnet',
+  mainnet:  'Zoo Mainnet',
+  ethereum: 'Zoo ETH Mainnet',
+  ropsten:  'Zoo ETH (Ropsten)',
 }[NETWORK]
 const ABI_PATH = `${__dirname}/../../contracts/deployments/${NETWORK}/ZooKeeper.json`
 
