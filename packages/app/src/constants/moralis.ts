fda6c14dec63e6097bca9f6cd8787cacf0b5537b
Zach Kelling <z@zeekay.im>
2021-09-03 03:51:37 -0400

    Update moralis config

--- packages/app/src/constants/moralis.ts
+++ packages/app/src/constants/moralis.ts
@@ -1,11 +1,11 @@
 const config = {
   applicationID: {
-    1337: 'UYWgmpnoFH5kSVeiekDaePaQUUTcHnM2Q10yb8N1',
+    1337: 'wBLjsQWUNSVJ61NHRimu6xy6slg2s6Zwd5pCQrnx',
     97: '16weSJXK4RD3aYAuwiP46Cgzjm4Bng1Torxz5qiy',
     56: 'cIGUkzL7pyhM8aC8gIcDiH46QGpsEutO5SAQzTgy',
   },
   serverURL: {
-    1337: 'https://qjydxwdegh7e.usemoralis.com:2053/server',
+    1337: 'https://zdkisefixfx6.bigmoralis.com:2053/server',
     97: 'https://dblpeaqbqk32.usemoralis.com:2053/server',
     56: 'https://j0ixlvmwc1kz.usemoralis.com:2053/server',
   },
