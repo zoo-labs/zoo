02d79209cd30155bf00954998f7ba719df30aa22
Ari Lerner <me@ari.io>
2021-09-03 00:08:35 -0400

    All tests pass

--- packages/contracts/test/Savage.test.ts
+++ packages/contracts/test/Savage.test.ts
@@ -94,7 +94,7 @@ describe('Savage', function () {
     await z1.approve(savage.address, amountIn)
     await savage.drainPool()
 
-    await zoo.unpause()
+    // await zoo.unpause()
 
     expect(await bnb.balanceOf(savage.address)).to.be.at.least(finalBNB)
 
