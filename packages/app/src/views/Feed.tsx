3a351baf83e9fcfe14cf586618e22861f4b37641
johneyo <johneyo57@yahoo.com>
2021-09-05 07:59:55 +0100

    Feed BACK

--- packages/app/src/views/Feed.tsx
+++ packages/app/src/views/Feed.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import { Flex, Text } from 'components'
 import { ButtonMenu, ButtonMenuItem, ButtonMenuItemProps } from 'components/ButtonMenu'
 
 import { Swiper, Slide } from 'components/Swiper'
 import { useRouteMatch, Link, useLocation, useHistory, RouteComponentProps, withRouter } from 'react-router-dom'
-
+import { Splide, SplideSlide } from '@splidejs/react-splide'
 import { useWeb3 } from 'hooks/useWeb3'
 import { useSelector } from 'react-redux'
 import { useMatchBreakpoints } from 'hooks'
@@ -14,7 +14,8 @@ import { AppState } from 'state/index'
 import FeedCard from './FeedCard'
 import BorderButton from 'components/Button/BorderButton'
 import { ChevronLeftIcon } from 'components/Svg'
-
+import '@splidejs/splide/dist/css/themes/splide-default.min.css'
+import EggFeedCard from './EggFeedCard'
 interface ButtonProp extends ButtonMenuItemProps {
   activeIndex: number
 }
@@ -96,14 +97,17 @@ export interface FeedPagePops extends RouteComponentProps<{ key?: string }> {}
 
 function Feed<FeedPagePops>({ match }) {
   const animalsState = useSelector<AppState, AppState['zoo']['animals']>((state) => state.zoo.animals)
+  const eggsState = useSelector<AppState, AppState['zoo']['eggs']>((state) => state.zoo.eggs)
+
   const { isXl } = useMatchBreakpoints()
   const isMobile = !isXl
   const history = useHistory()
   const { account } = useWeb3()
-  const [swiperRef, setSwiperRef] = useState(undefined)
   const [activeIndex, setActiveIndex] = useState(0)
-
+  const swiperRef = useRef(null)
   let animals = Object.values(animalsState)
+  let eggs = Object.values(eggsState).filter((egg) => !egg.burned)
+  const myEggs = eggs.filter((egg) => egg.owner === account)
   const { pathname, state } = useLocation()
   //  Reroute to homes
   const HomeClick = () => {
@@ -111,19 +115,8 @@ function Feed<FeedPagePops>({ match }) {
   }
 
   const onItemClick = (index) => {
-    // if (activeIndex == 0) {
-    //   index = 1
-    // } else {
-    //   index = 0
-    // }
-    swiperRef.slideTo(index, 200)
+    swiperRef.current.splide.go(index)
     setActiveIndex(index)
-
-    // if (index == 0) {
-    //   history.push('/myzoo')
-    // } else {
-    //   history.push('/market')
-    // }
   }
 
   const handleIndexChange = (obj) => {
@@ -160,18 +153,12 @@ function Feed<FeedPagePops>({ match }) {
       HomeClick()
     }
   }
-
   return (
     // <Container className='feed-container' onClick={handleContainerClick} isMobile={isMobile}>
 
     <>
       <main className='flex flex-col items-center justify-start flex-grow w-full h-full'>
-        {/* <ButtonMenu activeIndex={activeIndex} onItemClick={onItemClick} scale='sm'>
-        <ButtonMenu activeIndex={activeIndex} onItemClick={onItemClick} scale='sm'>
-          <ButtonMenuItem as='a'>My Zoo</ButtonMenuItem>
-          <ButtonMenuItem as='a'>Market</ButtonMenuItem>
-        </ButtonMenu> */}
-        <div className='w-full items-center justify-center absolute z-10 mt-4 flex'>
+        <div className='w-full items-center justify-center fixed z-10 mt-4 flex'>
           <div className={`grid rounded-md p-1 grid-cols-2 justify-self-start`} style={{ backgroundColor: 'rgb(44, 47, 54)', height: 46 }}>
             {['My Zoo', 'Market'].map((item: string, index: number) => {
               const active = activeIndex == index
@@ -188,57 +175,153 @@ function Feed<FeedPagePops>({ match }) {
             })}
           </div>
         </div>
-
-        <Swiper
-          className='swiper-wrapper'
-          onSwiper={setSwiperRef}
-          onActiveIndexChange={handleIndexChange}
-          centeredSlides={isMobile ? true : false}
-          spaceBetween={30}
-          slidesPerView={1}
-          direction='horizontal'>
-          <Slide key={0}>
-            {animals.length ? (
-              <Swiper mousewheel={{ releaseOnEdges: true }} speed={900} initialSlide={animalIndex} spaceBetween={30} slidesPerView={1} direction='vertical'>
-                {animals.map((data) => {
-                  return (
-                    <Slide className='swiper-wrapper' key={data.tokenID + 'slide'}>
-                      <FeedCard item={data} key={data.tokenID + 'card'} animalGroup={animalGroup} hideBid={activeIndex === 0} />
-                    </Slide>
-                  )
-                })}
-              </Swiper>
-            ) : (
-              <EmptyZoo>
-                <Text textAlign='center'>You do not currently own any animals</Text>
-                <BorderButton scale='md' onClick={HomeClick}>
-                  Buy Egg
-                </BorderButton>
-              </EmptyZoo>
-            )}
-          </Slide>
-          <Slide key={1}>
-            <Swiper speed={900} initialSlide={animalIndex} spaceBetween={30} slidesPerView={1} direction='vertical'>
-              {animals.map((data, index) => {
-                return data.listed ? (
-                  <Slide key={data.tokenID + 'slide'}>
-                    <FeedCard item={data} key={data.tokenID + 'card'} animalGroup={{}} />
-                  </Slide>
+        <Splide
+          ref={swiperRef}
+          options={{
+            direction: 'ltr',
+            arrows: false,
+            perPage: 1,
+            focus: 'center',
+            width: '100vw',
+            pagination: false,
+            drag: false,
+          }}>
+          <SplideSlide>
+            <div className='flex justify-center'>
+              <Splide
+                options={{
+                  direction: 'ttb',
+                  height: '100vh',
+                  autoWidth: true,
+                  arrows: false,
+                  pagination: false,
+                  perPage: 1,
+                  perMove: 1,
+                  gap: '5em',
+                  focus: 'center',
+                }}>
+                {myEggs.length ? (
+                  myEggs.map((data) => {
+                    return (
+                      <SplideSlide>
+                        <div className='w-full p-px  bg-gradient-to-b from-btn1  to-btn2'>
+                          <div className='flex flex-col p-1  bg-dark-900'>
+                            {/* <FeedCard item={data} key={data.tokenID + 'card'} animalGroup={animalGroup} hideBid={activeIndex === 0} /> */}
+                            <EggFeedCard item={data} key={data.tokenID + 'card'} />
+                          </div>
+                        </div>
+                      </SplideSlide>
+                    )
+                  })
                 ) : (
-                  <EmptyZoo>
-                    <Text textAlign='center'>There are currently no animals up for auction</Text>
-                    <BorderButton scale='md' onClick={HomeClick}>
-                      Home
-                    </BorderButton>
-                  </EmptyZoo>
-                )
-              })}
-            </Swiper>
-          </Slide>
-        </Swiper>
+                  <SplideSlide>
+                    <div className='w-full h-full p-px rounded bg-gradient-to-b from-btn1  to-btn2'>
+                      <div className='flex h-full flex-col p-1 rounded bg-dark-900 justify-center items-center px-8'>
+                        <Text textAlign='center'>You do not currently own any animals</Text>
+                        <BorderButton scale='md' onClick={HomeClick}>
+                          Buy Egg
+                        </BorderButton>
+                      </div>
+                    </div>
+                  </SplideSlide>
+                )}
+              </Splide>
+            </div>
+          </SplideSlide>
+
+          <SplideSlide tabindex='0'>
+            <div className='flex justify-center'>
+              <Splide
+                options={{
+                  direction: 'ttb',
+                  height: '100vh',
+                  autoWidth: true,
+                  arrows: false,
+                  pagination: false,
+                  perPage: 1,
+                  perMove: 1,
+                  gap: '5em',
+                  focus: 'center',
+                }}>
+                {animals.length ? (
+                  animals.map((data) => {
+                    return (
+                      <SplideSlide>
+                        <div className='w-full p-px  bg-gradient-to-b from-btn1  to-btn2'>
+                          <div className='flex flex-col p-1  bg-dark-900'>
+                            <FeedCard item={data} key={data.tokenID + 'card'} animalGroup={{}} />
+                          </div>
+                        </div>
+                      </SplideSlide>
+                    )
+                  })
+                ) : (
+                  <SplideSlide>
+                    <div className='w-full p-px rounded bg-gradient-to-b from-btn1  to-btn2'>
+                      <div className='flex flex-col p-1 rounded bg-dark-900'>
+                        <Text textAlign='center'>You do not currently own any animals</Text>
+                        <BorderButton scale='md' onClick={HomeClick}>
+                          Buy Egg
+                        </BorderButton>
+                      </div>
+                    </div>
+                  </SplideSlide>
+                )}
+              </Splide>
+            </div>
+          </SplideSlide>
+        </Splide>
       </main>
     </>
   )
 }
-
 export default withRouter(Feed)
+{
+  /* <Swiper
+className='swiper-wrapper'
+onSwiper={setSwiperRef}
+onActiveIndexChange={handleIndexChange}
+centeredSlides={isMobile ? true : false}
+spaceBetween={30}
+slidesPerView={1}
+direction='horizontal'>
+<Slide key={0}>
+  {animals.length ? (
+    <Swiper mousewheel={{ releaseOnEdges: true }} speed={900} initialSlide={animalIndex} spaceBetween={30} slidesPerView={1} direction='vertical'>
+      {animals.map((data) => {
+        return (
+          <Slide className='swiper-wrapper' key={data.tokenID + 'slide'}>
+            <FeedCard item={data} key={data.tokenID + 'card'} animalGroup={animalGroup} hideBid={activeIndex === 0} />
+          </Slide>
+        )
+      })}
+    </Swiper>
+  ) : (
+    <EmptyZoo>
+      <Text textAlign='center'>You do not currently own any animals</Text>
+      <BorderButton scale='md' onClick={HomeClick}>
+        Buy Egg
+      </BorderButton>
+    </EmptyZoo>
+  )}
+</Slide>
+<Slide key={1}>
+  <Swiper speed={900} initialSlide={animalIndex} spaceBetween={30} slidesPerView={1} direction='vertical'>
+    {animals.map((data, index) => {
+      return data.listed ? (
+        <Slide key={data.tokenID + 'slide'}>
+          <FeedCard item={data} key={data.tokenID + 'card'} animalGroup={{}} />
+        </Slide>
+      ) : (
+        <EmptyZoo>
+          <Text textAlign='center'>There are currently no animals up for auction</Text>
+          <BorderButton scale='md' onClick={HomeClick}>
+            Home
+          </BorderButton>
+        </EmptyZoo>
+      )
+    })}
+  </Swiper>
+</Slide>
+</Swiper> */
+}
