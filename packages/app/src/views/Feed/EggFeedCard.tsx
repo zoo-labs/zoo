f370260e4a73c5654b830256a34d07a345fdbde1
Ari Lerner <me@ari.io>
2021-09-06 13:06:38 -0400

    Removed controls from the egg card

--- packages/app/src/views/Feed/EggFeedCard.tsx
+++ packages/app/src/views/Feed/EggFeedCard.tsx
@@ -149,6 +149,7 @@ const EggFeedCard: React.FC<Props> = ({ item }) => {
           loop
           muted
           style={{
+            pointerEvents: 'none',
             height: '100%',
             alignSelf: 'center',
           }}>
