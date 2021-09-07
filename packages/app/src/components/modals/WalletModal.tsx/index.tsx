590f9004ee2fd3079203353a7affcc801cf819ee
Zach Kelling <z@zeekay.io>
2021-09-07 19:04:34 -0400

    Update tooltip and errors

--- packages/app/src/components/modals/WalletModal.tsx/index.tsx
+++ packages/app/src/components/modals/WalletModal.tsx/index.tsx
@@ -220,9 +220,7 @@ export default function WalletModal({ onDismiss = () => null }: { onDismiss?: ()
           </CloseButton>
           <HeaderRow style={{ paddingLeft: 0, paddingRight: 0 }}>{error instanceof UnsupportedChainIdError ? `Wrong Network` : `Error connecting`}</HeaderRow>
           <div>
-            {error instanceof UnsupportedChainIdError ? <h5>Please connect to Ethereum or Binance Smart Chain.</h5> : `Error connecting. Try refreshing the page.`}
-            <div style={{ marginTop: '1rem' }} />
-            <button onClick={deactivate}>Disconnect</button>
+            {error instanceof UnsupportedChainIdError ? <h5>Please connect to a supported network.</h5> : `Error connecting. Try refreshing the page.`}
           </div>
         </UpperSection>
       )
