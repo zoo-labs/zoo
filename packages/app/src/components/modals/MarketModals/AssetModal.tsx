199fbc33a4a9fe99e119a1792062560ddfe3fcaa
johneyo <johneyo57@yahoo.com>
2021-09-25 23:33:54 +0100

    Ask modal

--- packages/app/src/components/modals/MarketModals/AssetModal.tsx
+++ packages/app/src/components/modals/MarketModals/AssetModal.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { useWeb3React } from '@web3-react/core'
 import { connectorLocalStorageKey } from '../config'
 import useWeb3 from 'hooks/useWeb3'
-import { getFaucet, getToken } from 'util/contracts'
+import { getFaucet, getToken, getZooKeeper } from 'util/contracts'
 import AltModal from 'components/Modal/AltModal'
 import { numberWithCommas } from 'components/Functions'
 import HeaderModal from 'components/Modal/HeaderModal'
@@ -20,6 +20,10 @@ import { useIsAnimationMode } from 'state/user/hooks'
 import { ImArrowRight2, ImArrowUpRight2 } from 'react-icons/im'
 import { useMatchBreakpoints } from 'hooks'
 import { FaCompress, FaCompressAlt, FaExpand } from 'react-icons/fa'
+import { RiShareFill } from 'react-icons/ri'
+import Web3 from 'web3'
+import useToast from 'hooks/useToast'
+import { formatError } from 'functions'
 
 interface AssetModalProps {
   item: any
@@ -29,10 +33,13 @@ const AssetModal: React.FC<AssetModalProps> = ({ item }) => {
   const assetModal = useModalOpen(ApplicationModal.ASSET)
   const toggleAssetModal = useAssetModalToggle()
   const [fullView, setFullView] = useState(false)
+  const [amount, setAmount] = useState(0)
   const { logout } = useAuth()
   const { chainId, account } = useWeb3React()
   const history = useHistory()
   const zooBalance = useSelector<AppState, AppState['zoo']['zooBalance']>((state) => state.zoo.zooBalance)
+  const web3 = useWeb3()
+  const { chainID, gasPrice } = web3
   const getNetwork = (chainId: number) => {
     switch (chainId) {
       case 56:
@@ -78,11 +85,44 @@ const AssetModal: React.FC<AssetModalProps> = ({ item }) => {
   const ownerAccount = item.owner || ''
   const txHashEllipsis = `${txHash.substring(0, 10)}...${txHash.substring(txHash.length - 6)}`
   const accountEllipsis = `${ownerAccount.substring(0, 6)}...${ownerAccount.substring(txHash.length - 4)}`
-
+  const { toastSuccess, toastError, toastInfo, clear } = useToast()
+  const [disabled, setDisabled] = useState(false)
   const myTransactions = useSelector<AppState, AppState['zoo']['myTransactions']>((state) => state.zoo.myTransactions)
   console.log(item)
-  const { isXl, isXs, isSm, isMd, isLg } = useMatchBreakpoints()
-  console.log('assetModal is', assetModal)
+  const { isSm } = useMatchBreakpoints()
+  const zooKeeper = getZooKeeper(web3)
+  const zooToken = getToken(web3)
+  const setAsk = async () => {
+    setDisabled(true)
+    try {
+      await zooKeeper.methods
+        .setAsk(item.tokenID, { amount: 200, currency: ownerAccount }) //set Ask price for token
+        .send({ from: account, gasPrice: gasPrice })
+        .then((res) => {
+          clear()
+          toastInfo('Ask Price Set.')
+          console.log('Set Ask Price', res)
+
+          setDisabled(false)
+          toggleAssetModal()
+        })
+        .catch((err) => {
+          const message = formatError(err)
+          setDisabled(false)
+          clear()
+          toastError(message)
+          console.error(message)
+        })
+    } catch (err) {
+      console.error(err)
+      clear()
+      setDisabled(false)
+
+      toastError('Unable to purchase eggs. Try again later.')
+    }
+    // console.log(testEggs)
+    // dispatch(addEggs(testEggs))
+  }
   return (
     <Modal isOpen={assetModal} onDismiss={() => null} isMax scrollable={isSm}>
       <BidModalHeader onBack={() => toggleAssetModal()} className='absolute p-6 w-full z-10' />
@@ -136,28 +176,35 @@ const AssetModal: React.FC<AssetModalProps> = ({ item }) => {
                 <p className='text-sm text-justify text-gray-500 my-4 font-semibold' style={{ color: '#f2f2f2' }}>
                   Contains 1 of 16 Generation One Base Animals. To hatch or to hold…
                 </p>
-                {/*
-              <div className='w-full mb-4'>
-                <div className='rounded border-2 border-gray-400 border-solid p-4' style={{ borderWidth: 1 }}>
-                  <h2 className='text-sm font-bold mb-2'>Reserve Price</h2>
-                  <div className=''>
-                    <span className='mr-2 text-xl  font-semibold'>0.25 ETH</span>
-                    <span className='font-light'>$975.00 USD</span>
+
+                <div className='w-full mb-4'>
+                  <div className='rounded border-2 border-gray-400 border-solid p-4' style={{ borderWidth: 1 }}>
+                    <h2 className='text-sm font-bold mb-2'>Reserve Price</h2>
+                    <div className=''>
+                      <span className='mr-2 text-xl  font-semibold'>25K ZOO</span>
+                      <span className='font-light'>$975.00 USD</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className='w-full  grid  mb-8' style={{ gridTemplateColumns: '1fr 54px', gap: '10px' }}>
-                <a
-                  style={{ pointerEvents: 'unset', padding: '11px 0px', borderRadius: 4 }}
-                  className='rounded bg-white text-black flex items-center justify-center font-bold text-sm'
-                  href='#'>
-                  Place Bid
-                </a>
-                <button style={{ paddingLeft: 0, paddingRight: 0, padding: '11px 20px', backgroundColor: '#f2f2f2', borderRadius: 4 }} className=''>
-                  <RiShareFill fill='black' />
-                </button>
-              </div>
-              */}
+                <div className='w-full  grid  mb-8' style={{ gridTemplateColumns: '1fr 54px', gap: '10px' }}>
+                  <a
+                    style={{ pointerEvents: 'unset', padding: '11px 0px', borderRadius: 4 }}
+                    className='rounded bg-white text-black flex items-center justify-center font-bold text-sm transform active:scale-95'
+                    onClick={() => setAsk()}>
+                    {disabled ? (
+                      <span className='pr-2'>
+                        <i className={`${disabled && 'animate-spin'} fas fa-circle-notch `}></i>
+                      </span>
+                    ) : account.toLowerCase() == ownerAccount.toString() ? (
+                      'Set Ask'
+                    ) : (
+                      'Place Bid'
+                    )}
+                  </a>
+                  <button style={{ paddingLeft: 0, paddingRight: 0, padding: '11px 20px', backgroundColor: '#f2f2f2', borderRadius: 4 }} className=''>
+                    <RiShareFill fill='black' />
+                  </button>
+                </div>
 
                 <div className='w-full mb-4'>
                   <div className=' rounded border-2 border-gray-400 border-solid p-4' style={{ borderWidth: 1 }}>
