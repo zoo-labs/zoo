825489463513a69297af90431c827b355773b801
Zach Kelling <z@zeekay.io>
2021-10-01 21:04:19 +0200

    Add BNB pricing and buy Eggs helper

--- packages/contracts/src/ZooKeeper.sol
+++ packages/contracts/src/ZooKeeper.sol
@@ -14,6 +14,7 @@ import { UUPSUpgradeable } from '@openzeppelin/contracts-upgradeable/proxy/utils
 import { IDrop } from './interfaces/IDrop.sol';
 import { IMedia } from './interfaces/IMedia.sol';
 import { IZoo } from './interfaces/IZoo.sol';
+import { IUniswapV2Pair } from './uniswapv2/interfaces/IUniswapV2Pair.sol';
 
 import './console.sol';
 
@@ -32,7 +33,6 @@ contract ZooKeeper is UUPSUpgradeable, OwnableUpgradeable {
   event Hatch(address indexed from, uint256 eggID, uint256 indexed tokenID);
   event Mint(address indexed from, uint256 indexed tokenID);
   event Swap(address indexed owner, uint256 indexed tokenID, uint256 indexed chainID);
-  event Remint(address indexed owner, uint256 indexed tokenID, uint256 indexed chainID);
 
   // Mapping of Address to Drop ID
   mapping(uint256 => address) public drops;
@@ -49,44 +49,65 @@ contract ZooKeeper is UUPSUpgradeable, OwnableUpgradeable {
   // External contracts
   IMedia public media;
   IERC20 public zoo;
+  IUniswapV2Pair public pair;
   address public bridge;
   bool public unlocked;
-  // string public dev = "01001001 01010100 00100111 01010011 00100000 01000100 01000101 01000110 01001001 00100000 01000010 01010010 01001111 00100001 00001101 00001010 00101101 01001110 01001111 00100000 01001001 01000100 01000101 01000001 00101101 00001101 00001010 00001101 00001010 01101000 01110100 01110100 01110000 01110011 00111010 00101111 00101111 01111001 01101111 01110101 01110100 01110101 00101110 01100010 01100101 00101111 01100010 01011010 00110100 01010001 01010000 01000011 01100111 01011010 01100110 01101100 01101011";
 
+  // Only bridge can call method
   modifier onlyBridge() {
     require(msg.sender == bridge);
     _;
   }
 
+  // Ensure animals can breed
+  modifier canBreed(uint256 parentA, uint256 parentB) {
+    console.log('canBreed', parentA, parentB);
+
+    require(media.tokenExists(parentA) && media.tokenExists(parentB), 'Non-existent token');
+    require(keccak256(abi.encode(parentA)) != keccak256(abi.encode(parentB)), 'Not able to breed with self');
+    require(breedReady(parentA) && breedReady(parentB), 'Wait for cooldown to finish.');
+    require(isBaseAnimal(parentA) && isBaseAnimal(parentB), 'Only BASE_ANIMAL can breed.');
+    _;
+  }
+
+  // Ensure only owner can upgrade contract
   function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
+  // Initialize upgradeable contract
   function initialize() public initializer {
     __Ownable_init_unchained();
   }
 
+  // Configure ZooKeeper
   function configure(
     address _media,
     address _zoo,
+    address _pair,
     address _bridge,
     bool _unlocked
   ) public onlyOwner {
     media = IMedia(_media);
     zoo = IERC20(_zoo);
+    pair = IUniswapV2Pair(_pair);
     bridge = _bridge;
     unlocked = _unlocked;
   }
 
-  function setDrop(address dropAddress) public returns (uint256) {
+  // Add new drop
+  function addDrop(address dropAddress) public onlyOwner returns (uint256) {
     require(dropAddresses[dropAddress] == 0, 'Drop already added');
-
     IDrop drop = IDrop(dropAddress);
-
     dropIDs.increment();
     uint256 dropID = dropIDs.current();
     drops[dropID] = dropAddress;
     dropAddresses[dropAddress] = dropID;
     emit AddDrop(dropAddress, drop.title(), drop.eggSupply());
     return dropID;
+  }
+
+  // Set price for buying a name
+  function setNamePrice(uint256 price) public onlyOwner {
+    namePrice = price.mul(10**18);
   }
 
   // Issue a new token to owner
@@ -118,15 +139,42 @@ contract ZooKeeper is UUPSUpgradeable, OwnableUpgradeable {
     emit Swap(owner, tokenID, chainID);
   }
 
-  // Swap from new chain requested
+  // Remint token swapped from another chain
   function remint(
     address owner,
-    IZoo.Token memory token,
-    uint256 chainID
+    IZoo.Token memory token
   ) external onlyBridge {
-    console.log('remint', owner, token.id, chainID);
-    token = mint(owner, token);
-    emit Remint(owner, token.id, chainID);
+    mint(owner, token);
+  }
+
+  // Mint egg
+  function mintEgg(uint256 dropID, address owner) internal returns (IZoo.Token memory) {
+    require(media.balanceOf(owner) < 3, 'Only 3 eggs allowed');
+
+    // Get Egg for drop
+    IDrop drop = IDrop(drops[dropID]);
+    IZoo.Token memory egg = drop.newEgg();
+
+    // Mint Egg Token
+    egg = mint(owner, egg);
+    console.log('minted egg', egg.id);
+    emit BuyEgg(owner, egg.id);
+    return egg;
+  }
+
+  // Accept ZOO and return Egg NFT
+  function buyEgg(uint256 dropID) public returns (IZoo.Token memory) {
+    console.log('buyEgg', dropID);
+
+    // Check egg price
+    IDrop drop = IDrop(drops[dropID]);
+    require(zoo.balanceOf(msg.sender) >= drop.eggPrice(), 'Not enough ZOO');
+
+    // Transfer funds
+    console.log('Transfer ZOO', msg.sender, address(this), drop.eggPrice());
+    zoo.transferFrom(msg.sender, address(this), drop.eggPrice());
+
+    return mintEgg(dropID, msg.sender);
   }
 
   // Accept ZOO and return Egg NFT
@@ -138,19 +186,17 @@ contract ZooKeeper is UUPSUpgradeable, OwnableUpgradeable {
   }
 
   // Accept ZOO and return Egg NFT
-  function buyEgg(uint256 dropID) public returns (IZoo.Token memory) {
-    require(unlocked, 'Game is not unlocked yet');
-    require(media.balanceOf(msg.sender) < 3, 'Only 3 eggs allowed');
-
-    console.log('buyEgg', dropID);
+  function buyEggBNB(uint256 dropID) public payable returns (IZoo.Token memory) {
+    console.log('buyEggBNB', dropID);
 
     // Check egg price
     IDrop drop = IDrop(drops[dropID]);
-    require(zoo.balanceOf(msg.sender) >= drop.eggPrice(), 'ZK: Not Enough ZOO to purchase Egg');
+    uint256 bnbPrice = zooPriceBNB() * drop.eggPrice();
+    require(msg.sender.balance >= bnbPrice, 'Not enough BNB');
 
     // Transfer funds
-    console.log('zoo.transferFrom', msg.sender, address(this), drop.eggPrice());
-    zoo.transferFrom(msg.sender, address(this), drop.eggPrice());
+    console.log('Transfer BNB', msg.sender, address(this), bnbPrice);
+    payable(msg.sender).transfer(bnbPrice);
 
     // Get Egg from this drop
     IZoo.Token memory egg = drop.newEgg();
@@ -158,10 +204,15 @@ contract ZooKeeper is UUPSUpgradeable, OwnableUpgradeable {
     // Mint Egg Token
     egg = mint(msg.sender, egg);
     console.log('minted egg', egg.id);
-
     emit BuyEgg(msg.sender, egg.id);
-
     return egg;
+  }
+
+  function buyEggsBNB(uint256 dropID, uint256 quantity) public payable {
+    console.log('buyEggsBNB', dropID, quantity);
+    for (uint8 i = 0; i < quantity; i++) {
+      buyEggBNB(dropID);
+    }
   }
 
   // Burn egg and randomly return an animal NFT
@@ -233,11 +284,6 @@ contract ZooKeeper is UUPSUpgradeable, OwnableUpgradeable {
     return yield;
   }
 
-  // Set price for buying a name
-  function setNamePrice(uint256 price) public onlyOwner {
-    namePrice = price.mul(10**18);
-  }
-
   // Buy a custom name for your NFT
   function buyName(uint256 tokenID, string memory customName) public {
     require(zoo.balanceOf(msg.sender) < namePrice, 'ZK: Not enough ZOO to purchase Name');
@@ -258,17 +304,6 @@ contract ZooKeeper is UUPSUpgradeable, OwnableUpgradeable {
   // Ensure base animal
   function isBaseAnimal(uint256 tokenID) private view returns (bool) {
     return tokens[tokenID].kind == IZoo.Type.BASE_ANIMAL;
-  }
-
-  // Ensure animals can breed
-  modifier canBreed(uint256 parentA, uint256 parentB) {
-    console.log('canBreed', parentA, parentB);
-
-    require(media.tokenExists(parentA) && media.tokenExists(parentB), 'Non-existent token');
-    require(keccak256(abi.encode(parentA)) != keccak256(abi.encode(parentB)), 'Not able to breed with self');
-    require(breedReady(parentA) && breedReady(parentB), 'Wait for cooldown to finish.');
-    require(isBaseAnimal(parentA) && isBaseAnimal(parentB), 'Only BASE_ANIMAL can breed.');
-    _;
   }
 
   // Get a random base or hybrid animal based on a given egg
@@ -319,14 +354,33 @@ contract ZooKeeper is UUPSUpgradeable, OwnableUpgradeable {
     return false;
   }
 
+  // Calculate price of ZOO denominted in BNB based on pair reserves
+  function zooPriceBNB() public view returns(uint) {
+    (uint zooAmount, uint bnbAmount,) = pair.getReserves();
+    return zooAmount / bnbAmount;
+  }
+
   // Return total amount of ZOO in contract
-  function zooSupply() public view returns (uint256) {
+  function supplyBNB() public view returns (uint256) {
     return zoo.balanceOf(address(this));
   }
 
-  // // Enable owner to withdraw ZOO if necessary
-  // function zooWithdraw(address receiver, uint256 amount) public onlyOwner returns (bool) {
-  //   return zoo.transfer(receiver, amount);
-  // }
+  // Return total amount of ZOO in contract
+  function supplyZOO() public view returns (uint256) {
+    return zoo.balanceOf(address(this));
+  }
 
+  // Enable owner to withdraw ZOO if necessary
+  function withdrawBNB(address payable receiver, uint256 amount) public onlyOwner {
+    require(receiver.send(amount));
+  }
+
+  // Enable owner to withdraw ZOO if necessary
+  function withdrawZOO(address receiver, uint256 amount) public onlyOwner {
+    require(zoo.transfer(receiver, amount));
+  }
+
+  // Payable fallback functions
+  receive() external payable {}
+  fallback() external payable {}
 }
