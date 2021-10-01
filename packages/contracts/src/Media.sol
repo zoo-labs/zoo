7f2d0e0329c4d19f296ef68a7212fa3413793c74
Zach Kelling <z@zeekay.im>
2021-10-01 23:20:08 +0200

    Deploy pool for testing BNB purchases

--- packages/contracts/src/Media.sol
+++ packages/contracts/src/Media.sol
@@ -379,7 +379,7 @@ contract Media is IMedia, ERC721Burnable, ReentrancyGuard, Ownable {
     IMarket.BidShares memory bidShares
   ) internal onlyValidURI(data.tokenURI) onlyValidURI(data.metadataURI) {
     require(data.contentHash != 0, 'Media: content hash must be non-zero');
-    require(_contentHashes[data.contentHash] == false, 'Media: a token has already been created with this content hash');
+    // require(_contentHashes[data.contentHash] == false, 'Media: a token has already been created with this content hash');
     require(data.metadataHash != 0, 'Media: metadata hash must be non-zero');
 
     uint256 tokenId = _tokenIdTracker.current();
