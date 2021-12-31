68cc995473347d313e7c70325eb1f120016a47ec
johneyo <johneyo57@yahoo.com>
2021-12-31 18:40:40 +0000

    min

--- packages/app/src/pages/home/index.tsx
+++ packages/app/src/pages/home/index.tsx
@@ -2,10 +2,6 @@ import _ from "lodash";
 import Head from "next/head";
 import { useModal } from "react-morphing-modal";
 import Container from "../../components/Container";
-import AssetSaleModal from "../../lux/AssetSaleModal";
-import AssetList from "../../lux/AssetList";
-import { useTokenTypes } from "../../lux/state";
-import AssetSale from "../../lux/AssetSale";
 import { useRouter } from "next/router";
 // sections
 import HeroSection from "sections/Home/HeroSection";
@@ -26,12 +22,7 @@ export default function Mint() {
       className="py-4 md:py-8 lg:py-12 "
       maxWidth="6xl"
     >
-      <Head>
-        <title>Home | ZOO</title>
-        <meta name="description" content="ZOO" />
-      </Head>
-
-      <div className={`grid grid-cols-1 gap-5 md:grid-cols-4`}>
+      <div>
         <HeroSection />
         <PartnersSection />
         <OpportunitySection />
