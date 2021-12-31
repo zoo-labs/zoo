01531124e2f1eef5b76bbb4aece506695d038f0d
johneyo <johneyo57@yahoo.com>
2021-12-31 15:20:39 +0000

    lock ish

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#1EE9B6",
+        blue: "#007EFF",
+        black: "#1F2030",
+      },
+    },
   },
   plugins: [],
-}
+};
