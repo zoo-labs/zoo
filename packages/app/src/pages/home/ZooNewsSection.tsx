f4cba556ac42d6d9ceb3ec26f9fd8415400288cf
victorbruce <victorbruce82@gmail.com>
2022-01-03 22:59:58 +0000

    update: create nft page and improve stylings on some section of the page

--- packages/app/src/pages/home/ZooNewsSection.tsx
+++ packages/app/src/pages/home/ZooNewsSection.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import Image from "next/image";
-import { useActiveWeb3React } from "hooks";
-import Link from "next/link";
 
 const ZooNewsSection = () => {
-  const { account, chainId, library } = useActiveWeb3React();
-
   return (
     <section className="ZooNews">
       <div className="flex flex-col items-center px-6 pb-20 mx-auto max-w-7xl">
@@ -22,23 +18,19 @@ const ZooNewsSection = () => {
           <div className="overflow-hidden rounded-2xl lg:basis-1/2">
             <Image src="/img/story-image.png" width={565} height={512} alt="" />
           </div>
-          <div className="flex flex-col justify-center px-4 py-0 -mt-4 lg:py-0 bg-black100 rounded-b-2xl lg:rounded-2xl lg:-ml-1 lg:basis-1/2">
-            <h2 className="mb-3 text-3xl font-bold">
-              Stories From Our Community: Kohaku &amp; Moyo Shiro
-            </h2>
-            <p className="mb-4">
-              How the ZOO foundation helped save over 100,000 acres of elephant
-              habitat to date.{" "}
-            </p>
-            {!account ? (
-              <button className="w-1/2 px-5 py-3 text-sm bg-gradient-to-b from-purple to-blue font-bold text-white rounded-full ZooNews__btn md:text-base md:px-6 md:py-4 lg:px-10">
-                Connect Wallet
-              </button>
-            ) : (
-              <a className="" href="/blog">
+          <div className="flex flex-col justify-center px-4 py-4 -mt-4 lg:py-0 bg-black100 rounded-b-2xl lg:rounded-2xl lg:-ml-1 lg:basis-1/2">
+            <div className="max-w-sm mx-auto">
+              <h2 className="mb-3 text-3xl font-bold">
+                Stories From Our Community: Kohaku &amp; Moyo Shiro
+              </h2>
+              <p className="mb-6">
+                How the ZOO foundation helped save over 100,000 acres of
+                elephant habitat to date.{" "}
+              </p>
+              <a className="bg-gradient-to-b from-purple to-blue text-white text-sm md:text-base px-5 py-3 md:px-6 md:py-4 lg:px-10 rounded-full" href="/blog">
                 Blog
               </a>
-            )}
+            </div>
           </div>
         </div>
       </div>
