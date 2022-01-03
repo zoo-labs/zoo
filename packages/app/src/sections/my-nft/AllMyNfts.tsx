b6fdfdf02597f2940c98df6f32bd080135f50e8f
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2022-01-03 18:39:29 +0100

    AllMyNfts component and top-zoo page style convert

--- packages/app/src/sections/my-nft/AllMyNfts.tsx
+++ packages/app/src/sections/my-nft/AllMyNfts.tsx
@@ -4,63 +4,24 @@ import Image from "next/image";
 const AllMyNfts = () => {
   return (
     <div className="allMyNfts mb-20">
-      <h2 className="mb-8"
-        style={{
-          fontSize: "36px",
-          fontWeight: "500",
-          lineHeight: "54px",
-          letterSpacing: "1.2000000476837158px",
-        }}
-      >
+      <h2 className="mb-8 font-medium text-4xl leading-10 tracking-wider">
         All My Nfts
       </h2>
       <div className="allMyNfts__items flex">
         <div
-          className="allMyNfts__item grid place-content-center"
-          style={{
-            height: "265px",
-            width: "156px",
-            borderRadius: "13px",
-            border: "1px solid #A67CED",
-            marginRight: "25px",
-          }}
-        >
+          className="allMyNfts__item grid place-content-center rounded-xl mr-6 h-66 w-39 border border-nft-card-border">
           <Image src="/img/rhinovid.png" width={160} height={200} alt="" />
         </div>
         <div
-          className="allMyNfts__item grid place-content-center"
-          style={{
-            height: "265px",
-            width: "156px",
-            borderRadius: "13px",
-            border: "1px solid #A67CED",
-            marginRight: "25px",
-          }}
-        >
+          className="allMyNfts__item grid place-content-center rounded-xl mr-6 h-66 w-39 border border-nft-card-border">
           <Image src="/img/plasma.png" width={160} height={200} alt="" />
         </div>
         <div
-          className="allMyNfts__item  grid place-content-center"
-          style={{
-            height: "265px",
-            width: "156px",
-            borderRadius: "13px",
-            border: "1px solid #A67CED",
-            marginRight: "25px",
-          }}
-        >
+          className="allMyNfts__item  grid place-content-center rounded-xl mr-6 h-66 w-39 border border-nft-card-border">
           <Image src="/img/egg1.png" width={160} height={200} alt="" />
         </div>
         <div
-          className="allMyNfts__item  grid place-content-center"
-          style={{
-            height: "265px",
-            width: "156px",
-            borderRadius: "13px",
-            border: "1px solid #A67CED",
-            marginRight: "25px",
-          }}
-        >
+          className="allMyNfts__item  grid place-content-center rounded-xl mr-6 h-66 w-39 border border-nft-card-border">
           <Image src="/img/egg2.png" width={160} height={200} alt="" />
         </div>
       </div>
