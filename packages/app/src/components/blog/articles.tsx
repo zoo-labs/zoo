6d022a0d567f20ed96ac377a64e9c34b629038bf
Gifted <gkenny896@gmail.com>
2022-01-04 11:21:06 +0100

    Fix: Removed [size] classes

--- packages/app/src/components/blog/articles.tsx
+++ packages/app/src/components/blog/articles.tsx
@@ -8,8 +8,8 @@ interface Props {
 }
 const Article: FC<Props> = ({ article: { name, image } }) => {
   return (
-    <div className="max-w-[353px]">
-      <div className="h-[259px] bg-dark-blue flex items-center">
+    <div className="max-w-22">
+      <div className="h-64 bg-dark-blue flex items-center">
         <img src={image} alt="" />
       </div>
       <div className="my-4 flex justify-start">
@@ -17,7 +17,7 @@ const Article: FC<Props> = ({ article: { name, image } }) => {
           New
         </p>
       </div>
-      <p className="text-butter-white font-semibold text-2xl text-left mb-[25px]">
+      <p className="text-butter-white font-semibold text-2xl text-left mb-6">
         {name}
       </p>
       <div className="flex justify-between text-sm">
@@ -27,7 +27,7 @@ const Article: FC<Props> = ({ article: { name, image } }) => {
         </div>
         <p>Feb 03, 2021</p>
       </div>
-      <hr className="h-px border-gray-300 mb-2.5 mt-[60px]" />
+      <hr className="h-px border-gray-300 mb-2.5 mt-14" />
     </div>
   );
 };
