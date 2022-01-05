30ba39b0f1e4429ad30fbe1f2be6d9d69614b810
victorbruce <victorbruce82@gmail.com>
2022-01-05 18:31:24 +0000

    update: convert community into a dropdown menu

--- packages/app/src/components/Header/index.tsx
+++ packages/app/src/components/Header/index.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import LanguageSwitch from "../LanguageSwitch";
 import Link from "next/link";
 import More from "./More";
+import Community from "./Community";
 import NavLink from "../NavLink";
 import { Popover } from "@headlessui/react";
 import QuestionHelper from "../QuestionHelper";
@@ -79,14 +80,8 @@ function AppBar(): JSX.Element {
                         {i18n._(t`Chart`)}
                       </a>
 
-                      <NavLink href="/community">
-                        <a
-                          id={`mint-nav-link`}
-                          className="p-2 text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis md:p-3 whitespace-nowrap"
-                        >
-                          {i18n._(t`Community`)}
-                        </a>
-                      </NavLink>
+                      <Community />
+
                       <NavLink href="/press">
                         <a
                           id={`mint-nav-link`}
@@ -241,7 +236,6 @@ function AppBar(): JSX.Element {
                       <Web3Status
                         title="My Wallet"
                         className="border-green border text-green font-bold"
-
                       />
                     </div>
                     <div className="hidden md:block">
@@ -313,13 +307,15 @@ function AppBar(): JSX.Element {
                   {i18n._(t`Chart`)}
                 </a>
 
-                <a
+                <Community />
+
+                {/* <a
                   id={`community`}
                   className="p-2 text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis md:p-3 whitespace-nowrap"
                   href="/community"
                 >
                   {i18n._(t`Community`)}
-                </a>
+                </a> */}
 
                 <a
                   id={`press`}
