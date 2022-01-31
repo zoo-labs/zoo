00db4d8fa37d9f034f740c601f2b3cb01bb1d1f7
victorbruce <victorbruce82@gmail.com>
2022-01-31 10:10:45 +0000

    update: merge changes

--- packages/lab/src/components/Contract/index.jsx
+++ packages/lab/src/components/Contract/index.jsx
@@ -43,7 +43,7 @@ const noContractDisplay = (
 
 const isQueryable = fn => (fn.stateMutability === "view" || fn.stateMutability === "pure") && fn.inputs.length === 0;
 
-export default function cdContract({
+export default function Contract({
   customContract,
   account,
   gasPrice,
