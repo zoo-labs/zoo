11a88dc4e7f7ccdbc98f57bae7000001fa9a160d
Zach Kelling <z@zeekay.im>
2022-01-31 20:40:57 -0500

    Update Sentry config

--- packages/app/src/components/Header/index.tsx
+++ packages/app/src/components/Header/index.tsx
@@ -27,7 +27,7 @@ import Banner from "components/Banner";
 // import { ExternalLink, NavLink } from "./Link";
 // import { ReactComponent as Burger } from "../assets/images/burger.svg";
 
-function AppBar(props: { banner: boolean }): JSX.Element {
+function AppBar(props: { banner?: boolean }): JSX.Element {
   const { i18n } = useLingui();
   const { account, chainId, library } = useActiveWeb3React();
   // const getZooBalance = useZoobalance();
@@ -182,7 +182,7 @@ function AppBar(props: { banner: boolean }): JSX.Element {
                         <div className="hidden md:inline-flex">
                           <QuestionHelper
                             text={i18n._(
-                              t`Add ZOO to your MetaMask wallet 
+                              t`Add ZOO to your MetaMask wallet
                               ${process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}`
                             )}
                           >
@@ -387,7 +387,7 @@ function AppBar(props: { banner: boolean }): JSX.Element {
                       </a>
                     </Link>
                   </>
-                )} 
+                )}
 
                 {chainId && featureEnabled(Feature.STAKING, chainId) && (
                   <Link href={"/stake"}>
