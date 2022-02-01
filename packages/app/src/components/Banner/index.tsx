9e38e391376dbf76c2d75268f1d16d6f644ec3b3
johneyo <johneyo57@yahoo.com>
2022-02-01 00:01:39 +0000

    prod changes

--- packages/app/src/components/Banner/index.tsx
+++ packages/app/src/components/Banner/index.tsx
@@ -1,20 +1,38 @@
-import { FC } from 'react'
-import { XIcon } from '@heroicons/react/outline'
+import { FC } from "react";
+import { XIcon } from "@heroicons/react/outline";
+import Copy from "components/AccountDetails/Copy";
+import Typography from "components/Typography";
 
 const Banner: FC = () => (
   <div className="relative w-full bg-purple bg-opacity-20">
     <div className="px-3 py-3 mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div className="pr-16 sm:text-center sm:px-16">
-        <p className="font-medium text-white">
+        {/* <p className="font-medium text-white">
           <span className="md:hidden">We announced a new product!</span>
-          <span className="hidden md:inline">Big news! We&apos;re excited to announce a brand new product.</span>
+          <span className="hidden md:inline">
+            Big news! We&apos;re excited to announce a brand new product.
+          </span>
           <span className="block sm:ml-2 sm:inline-block">
             <a href="#" className="font-bold text-white underline">
-              {' '}
+              {" "}
               Learn more <span aria-hidden="true">&rarr;</span>
             </a>
           </span>
-        </p>
+        </p> */}
+        <span className="block sm:ml-2 sm:inline-block">
+          <Copy
+            className=""
+            toCopy={
+              process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ||
+              "0x09e2b83fe5485a7c8beaa5dffd1d324a2b2d5c13"
+            }
+          >
+            <Typography variant="sm" className="font-bold text-white underline">
+              {process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ||
+                "0x09e2b83fe5485a7c8beaa5dffd1d324a2b2d5c13"}
+            </Typography>
+          </Copy>
+        </span>
       </div>
       <div className="absolute inset-y-0 right-0 flex items-start pt-1 pr-1 sm:pt-1 sm:pr-2 sm:items-start">
         <button type="button" className="flex p-2 focus:outline-none">
@@ -24,6 +42,6 @@ const Banner: FC = () => (
       </div>
     </div>
   </div>
-)
+);
 
-export default Banner
+export default Banner;
