08ffe53ac520d71c10517fa10e8f2573c6a627ed
victorbruce <victorbruce82@gmail.com>
2022-02-04 23:06:11 +0000

    Fix: tabnav border and lifecycle order

--- packages/app/src/components/ComingSoon/index.tsx
+++ packages/app/src/components/ComingSoon/index.tsx
@@ -11,7 +11,7 @@ const ComingSoon = () => {
     fadeInOnScroll(comingSoonRef.current);
   }, []);
   return (
-    <div className="py-20">
+    <div className="">
       <div className="max-w-7xl mx-auto py-20 px-4">
         <div
           className="flex flex-col items-center justify-center text-center "
