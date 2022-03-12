99fd59f78fd335f51b7a043f2a473c20b64cec55
jimmy nagles <55357484+JimmyNagles@users.noreply.github.com>
2022-03-11 19:52:57 -0600

    moonpay widg

---
+++ packages/app/src/pages/api/moonpay.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function moonpay(req, res) {
+  const { address } = await req.body;
+
+  //   console.log(email);
+
+  //   console.log("MAKING REQUEST NOW");
+  //   console.log("___________-----------_______");
+
+  //if (req.method === "POST") {
+  // Process a POST request
+
+  // } else {
+  // Handle any other HTTP method
+  //}
+}
