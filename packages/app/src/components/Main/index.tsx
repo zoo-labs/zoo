5a9b420015034be10e28ba29244b9304a34739c3
johneyo <johneyo57@yahoo.com>
2022-04-07 19:18:24 +0000

    Banner fix

--- packages/app/src/components/Main/index.tsx
+++ packages/app/src/components/Main/index.tsx
@@ -1,6 +1,6 @@
 const Main = ({ children }) => (
   <main
-    className="flex flex-col items-center justify-start flex-grow w-full h-full bg-black"
+    className="flex flex-col items-center justify-start flex-grow w-full h-full mt-12 bg-black"
     style={{ height: "max-content" }}
   >
     <div className="w-full mt-12 lg:mt-20">{children}</div>
