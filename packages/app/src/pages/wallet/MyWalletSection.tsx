637af167c4f161f222d084424b822cf93f2fe95a
Gifted <gkenny896@gmail.com>
2022-07-15 16:45:11 +0100

    WALLET IMAGE

--- packages/app/src/pages/wallet/MyWalletSection.tsx
+++ packages/app/src/pages/wallet/MyWalletSection.tsx
@@ -127,7 +127,7 @@ const MyWalletSection = ({ myNfts, nftTransfers, fetchNfts }) => {
         </div>
 
         <div className="flex flex-wrap items-center justify-center gap-4 mb-4 w-full">
-          <div className="flex flex-col md:flex-row items-start justify-center w-full gap-x-2 gap-y-8 flex-wrap">
+          <div className="flex flex-col md:flex-row items-start justify-center w-full gap-x-2.5 gap-y-8 flex-wrap">
             {filteredNfts.map((nft: MyNFT, index) => {
               const { kind, name, id, dropId, stage, token_uri } = nft;
               console.log("nftttt", nft);
