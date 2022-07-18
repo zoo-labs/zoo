4efc15513b586eb99f3937103c282efedfe8cd09
johneyo <johneyo57@yahoo.com>
2022-07-18 10:54:28 +0000

    wewefwe

--- packages/app/src/layouts/Default/index.tsx
+++ packages/app/src/layouts/Default/index.tsx
@@ -15,7 +15,7 @@ const Layout = ({ children, banner = undefined }) => {
   return (
     <div className="z-0 flex flex-col items-center w-full h-screen pb-16 lg:pb-0">
       <Header banner={banner} />
-      <Main>{children}</Main>
+      <Main isModal={false}>{children}</Main>
       <Popups />
       <Footer />
     </div>
