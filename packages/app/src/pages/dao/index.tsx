319ea8b0222f14eda5962279481edbe1f27d7f9a
Gifted <gift@montech.io>
2022-08-04 15:54:55 +0100

    DAO implementation

--- packages/app/src/pages/dao/index.tsx
+++ packages/app/src/pages/dao/index.tsx
@@ -1,17 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
 import ProposalsTable from "components/Voting/ProposalsTable";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import Link from "next/link";
 import useActiveWeb3React from "hooks/useActiveWeb3React";
-import { ProposalState } from "hooks/useVote";
+import { ProposalState, useGetAllProposals } from "hooks/useVote";
 import Head from "next/head";
 import { ChainId } from "constants/chainIds";
 import DaoLayout from "layouts/Dao";
 import { AppProps } from "next/app";
 import { NextComponentType, NextPageContext } from "next";
 import { useRouter } from "next/router";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { padding } from "@mui/system";
 
 const Voting = ({}: AppProps & {
   Component: NextComponentType<NextPageContext>;
@@ -22,8 +24,13 @@ const Voting = ({}: AppProps & {
   const [_proposals, setProposals] = useState([]);
   const { proposals }: any = useSelector((state: any) => state.voting);
   const { chainId, account } = useActiveWeb3React();
+  const getAllProposals = useGetAllProposals();
 
   const { push } = useRouter();
+
+  useEffect(() => {
+    getAllProposals();
+  }, [getAllProposals]);
 
   useEffect(() => {
     if (proposals) {
@@ -67,28 +74,138 @@ const Voting = ({}: AppProps & {
           <div className="flex items-center justify-between mb-8">
             <p className="text-2xl">All Proposal</p>
             <div className="flex items-center">
-              <select className="bg-white-10 py-2.5 px-1 text-sm font-semibold mr-5 rounded-full">
-                <option value="">All</option>
-                <option value="">Community</option>
-                <option value="">Core</option>
-              </select>
-              <select className="bg-white-10 py-2.5 px-1 text-sm font-semibold rounded-full">
-                <option value="">Vote now</option>
-                <option value="">Soon</option>
-                <option value="">Closed</option>
-              </select>
+              {/* <select
+                onChange={(e) => setProposalType(Number(e.target.value))}
+                className="bg-white-10 py-2.5 px-1 text-sm font-semibold mr-5 rounded-full"
+              >
+                <option value={3}>All</option>
+                <option value={0}>Community</option>
+                <option value={1}>Core</option>
+              </select> */}
+              <FormControl fullWidth sx={{ borderBottom: 0 }}>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={proposalType}
+                  variant="filled"
+                  onChange={(e) => setProposalType(Number(e.target.value))}
+                  className="bg-white-10 py-2.5 px-1 text-sm font-semibold mr-5 rounded-full text-white"
+                  sx={{
+                    backgroundColor: "rgba(255, 255, 255, 0.1)",
+                    maxHeight: "46px",
+                    borderRadius: "100px",
+                    minWidth: "150px",
+                    "&:hover": {
+                      backgroundColor: "rgba(255, 255, 255, 0.1)",
+                      opacity: "1",
+                    },
+                    "&:focus": {
+                      backgroundColor: "rgba(255, 255, 255, 0.1)",
+                      opacity: "1",
+                    },
+                    "& 	.MuiSelect-filled": {
+                      color: "white",
+                      fontWeight: 600,
+                      fontSize: 14,
+                      paddingTop: "8px",
+                      "&:focus": {
+                        backgroundColor: "rgba(255, 255, 255, 0.1)",
+                        opacity: "1",
+                        borderBottom: "none",
+                      },
+                    },
+                    "& .MuiSelect-icon": {
+                      color: "white",
+                    },
+                    "& .MuiList-root": {
+                      backgroundColor: "rgba(255, 255, 255, 0.1)",
+                    },
+                    "& .MuiMenu-list": {
+                      backgroundColor: "rgba(255, 255, 255, 0.1)",
+                    },
+                  }}
+                >
+                  <MenuItem value={3}>All</MenuItem>
+                  <MenuItem value={1}>Community</MenuItem>
+                  <MenuItem value={0}>Core</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl fullWidth>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={proposalState}
+                  variant="filled"
+                  onChange={(e) =>
+                    setProposalState(e.target.value as ProposalState)
+                  }
+                  className="bg-white-10 py-2.5 px-1 text-sm font-semibold mr-5 rounded-full text-white"
+                  sx={{
+                    backgroundColor: "rgba(255, 255, 255, 0.1)",
+                    maxHeight: "46px",
+                    borderRadius: "100px",
+                    minWidth: "150px",
+                    "&:hover": {
+                      backgroundColor: "rgba(255, 255, 255, 0.1)",
+                      opacity: "1",
+                    },
+                    "&:focus": {
+                      backgroundColor: "rgba(255, 255, 255, 0.1)",
+                      opacity: "1",
+                    },
+                    "& 	.MuiSelect-filled": {
+                      color: "white",
+                      fontWeight: 600,
+                      fontSize: 14,
+                      paddingTop: "8px",
+                      "&:focus": {
+                        backgroundColor: "rgba(255, 255, 255, 0.1)",
+                        opacity: "1",
+                        borderBottom: "none",
+                      },
+                    },
+                    "& .MuiSelect-icon": {
+                      color: "white",
+                    },
+                  }}
+                >
+                  <MenuItem value={ProposalState.ONGOING}>Vote now</MenuItem>
+                  <MenuItem value={ProposalState.PENDING}>Soon</MenuItem>
+                  <MenuItem value={ProposalState.ENDED}>Closed</MenuItem>
+                </Select>
+              </FormControl>
+              {/* <select
+                onChange={(e) =>
+                  setProposalState(e.target.value as ProposalState)
+                }
+                className="bg-white-10 py-2.5 px-1 text-sm font-semibold rounded-full"
+              >
+                <option value={ProposalState.ONGOING}>Vote now</option>
+                <option value={ProposalState.PENDING}>Soon</option>
+                <option value={ProposalState.ENDED}>Closed</option>
+              </select> */}
             </div>
           </div>
-          {Array(8)
-            .fill(0)
-            .map((_, i) => (
+          {/* {_proposals.map((_, i) => {
+            const proposalType = _.proposalType === 0 ? "Community" : "Core";
+            // const proposalState = _.proposalState === 0 ? "Ongoing" : "Closed";
+            const proposalState = (() => {
+              if (_.proposalStatus === 0) {
+                return "Soon";
+              } else if (_.proposalStatus === 1) {
+                return "Vote Now";
+              } else {
+                return "Closed";
+              }
+            })();
+            return (
               <div
-                key={i}
+                key={_.id}
                 className="border border-white-10 rounded-2xl py-6 px-5 mb-10 cursor-pointer"
                 style={{
                   backdropFilter: "blur(100px)",
                 }}
-                onClick={() => push("/dao/1")}
+                onClick={() => push(`/dao/${_.id}`)}
               >
                 <div className="flex items-center justify-between mb-1">
                   <div className="flex items-center">
@@ -96,24 +213,23 @@ const Voting = ({}: AppProps & {
                       Ends April 20, 2022 | 10:00 AM
                     </p>
                     <div className="text-activeGreen text-[10px] font-light border border-activeGreen py-1 px-2 rounded-full">
-                      Core
+                      {proposalType}
                     </div>
                   </div>
                   <div className="text-xs bg-green py-2 px-5 text-white rounded-full">
-                    Vote Now
+                    {proposalState}
                   </div>
                 </div>
-                <p className="text-lg mb-3">
-                  Lorem ipsum dolor sit amet, consectetur
-                </p>
-                <p className="text-sm max-w-[85%]">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Varius tincidunt egestas porta tincidunt in. Eros, venenatis
-                  et ullamcorper quis diam velit eu. Senectus luctus enim turpis
-                  urna. Iaculis a sagittis tincidunt id ac.
-                </p>
+                <p className="text-lg mb-3">{_.title}</p>
+                <p className="text-sm max-w-[85%]">{_.description}</p>
               </div>
-            ))}
+            );
+          })} */}
+          <ProposalsTable
+            proposals={_proposals}
+            state={proposalState}
+            type={proposalType}
+          />
         </div>
       </div>
     </div>
