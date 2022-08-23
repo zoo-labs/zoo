06682e86575eee9503f9ad53d5cf44ead34ee5ef
johneyo <john@montech.io>
2022-08-23 08:43:29 +0000

    ok

--- packages/app/src/pages/market/index.tsx
+++ packages/app/src/pages/market/index.tsx
@@ -18,12 +18,13 @@ import { useRouter } from "next/router";
 import Wallet from "./wallet";
 import { useTokenTypes } from "zoo/state";
 import { useFetchMyNFTs, useGetAvailableEggs } from "state/zoo/hooks";
-import { useActiveWeb3React, useDrop } from "../../hooks";
+import { useActiveWeb3React, useDrop, useZooKeeper } from "../../hooks";
 import { useMoralis } from "react-moralis";
 import { abbreviateNumber } from "functions/abbreviateNumbers";
 import { accountEllipsis } from "functions/lux";
 import { FaMoneyBillWave } from "react-icons/fa";
 import { Auction, AvailableEgg } from "types";
+import Web3 from "web3";
 
 const PrettoSlider = styled(Slider)({
   color: "#15F195",
@@ -157,18 +158,18 @@ const MarketPlacePage = () => {
   });
 
   const router = useRouter();
-  const [zooBnbPrice, setZooBnbPrice] = useState(0);
-  const zooKeeper = useZooKeeper();
+  // const [zooBnbPrice, setZooBnbPrice] = useState(0);
+  // const zooKeeper = useZooKeeper();
 
-  const getZooBnbPrice = useCallback(async () => {
-    const price = await zooKeeper?.BNBPrice();
-    const value = Web3.utils.fromWei(price.toString(), "ether");
-    setZooBnbPrice(parseFloat(value));
-  }, [zooKeeper]);
+  // const getZooBnbPrice = useCallback(async () => {
+  //   const price = await zooKeeper?.BNBPrice();
+  //   const value = Web3.utils.fromWei(price.toString(), "ether");
+  //   setZooBnbPrice(parseFloat(value));
+  // }, [zooKeeper]);
 
-  useEffect(() => {
-    getZooBnbPrice();
-  }, [getZooBnbPrice]);
+  // useEffect(() => {
+  //   getZooBnbPrice();
+  // }, [getZooBnbPrice]);
 
   const onClickTokenType = (name: string) => {
     console.log("name", name);
