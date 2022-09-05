34abfa4ba25a02b6a1cb525bf2442d05990d96d9
johneyo <john@montech.io>
2022-09-05 20:21:55 +0000

    activeSwitch

--- packages/app/src/pages/home/HeroSection.tsx
+++ packages/app/src/pages/home/HeroSection.tsx
@@ -27,60 +27,91 @@ const HeroSection = () => {
   const slides = [
     { title: " Exotic Animals", video: "/videoes/videoplayback.mp4" },
     { title: "Siberian Tiger", video: "/videoes/videoplayback.mp4" },
-    { title: "Elephant", video: "/videoes/videoplayback.mp4" },
+    { title: "Sumatran Elephant", video: "/videoes/videoplayback.mp4" },
+    { title: "Nubian Giraffe", video: "/videoes/videoplayback.mp4" },
+    { title: "Leopards + More", video: "/videoes/videoplayback.mp4" },
+    { title: "Genesis Eggs", video: "/videoes/videoplayback.mp4" },
   ];
+  const videoRef = useRef(null);
+  const videoCurrent = videoRef?.current || null;
+  useEffect(() => {
+    if (videoCurrent) {
+      videoCurrent.addEventListener("timeupdate", handleVideoUpdate);
+      return () =>
+        videoCurrent.removeEventListener("timeupdate", handleVideoUpdate);
+    }
+  }, [videoCurrent]);
+  const handleVideoUpdate = (event) => {
+    const time = event.target.currentTime;
 
+    const activeSwitch = (time: number): number => {
+      switch (true) {
+        case time < 9:
+          return 0;
+        case time > 9 && time < 17:
+          return 1;
+        case time > 17 && time < 21:
+          return 2;
+        case time > 21 && time < 29:
+          return 3;
+        case time > 29 && time < 33:
+          return 2;
+        case time > 33 && time < 38:
+          return 3;
+        case time > 38 && time < 46:
+          return 5;
+        case time > 46 && time < 54:
+          return 4;
+        case time > 54:
+          return 5;
+        default:
+          return 0;
+      }
+    };
+    console.log("activeSwitch", activeSwitch(time), time);
+    activeSlideIndex !== activeSwitch(time) &&
+      setActiveSlideIndex(() => activeSwitch(time));
+  };
   return (
     <div className="flex items-center justify-center h-screen Hero bg-zoo">
-      <div className="w-full m-0 overflow-hidden">
+      <div className="relative m-0 -mt-5 overflow-hidden w-ful">
         <div
-          className={`transition duration-1000 ease-in whitespace-nowrap `}
+          className={`w-[100vw] absolute z-50 h-full transition duration-1000 ease-in whitespace-nowrap mt-4 `}
           style={{
             transform: `translate3d(${-activeSlideIndex * 100}%, 0, 0)`,
           }}
         >
           {slides.map((slide, index) => (
-            <div
-              className="inline-block w-full "
-              key={index}
-              style={{ backgroundColor: slide.title }}
-            >
-              <div className="relative flex items-center justify-center w-full h-full mx-auto">
-                <div className="absolute z-50 w-full px-4 py-4 mx-auto max-w-7xl">
-                  <div className="px-4 py-4 mx-auto mb-6 max-w-1/3 md:mb-12 md:flex md:flex-col md:items-center md:text-center lg:items-start lg:text-left lg:basis-1/2">
-                    <h1 className="mb-3 text-4xl font-bold break-all lg:text-9xl lg:mb-6 ">
-                      {slide.title}
-                    </h1>
+            <div className={`inline-block w-full h-full `}>
+              <div className="flex items-center justify-center w-full h-full">
+                <div className="w-[80vw] px-4 py-4">
+                  <h1 className="mb-3 text-4xl font-bold break-all lg:text-9xl lg:mb-6 ">
+                    {slide.title}
+                  </h1>
 
-                    <div className="px-5 py-3 text-sm font-semibold text-white bg-transparent border-2 border-white rounded-full w-max md:text-lg md:px-6 md:py-4 lg:px-10 hover:cursor-pointer">
-                      Explore the Zoo
-                    </div>
+                  <div className="px-5 py-3 text-sm font-semibold text-white bg-transparent border-2 border-white rounded-full w-max md:text-lg md:px-6 md:py-4 lg:px-10 hover:cursor-pointer">
+                    Explore the Zoo
                   </div>
                 </div>
-                <video
-                  autoPlay={index === activeSlideIndex}
-                  muted
-                  onEnded={() =>
-                    setActiveSlideIndex(() =>
-                      index === slides.length - 1 ? 0 : index + 1
-                    )
-                  }
-                  id="bgVideo"
-                  // className="invisible md:visible"
-                  style={{
-                    height: "100vh",
-                    minHeight: "100vh",
-                    width: "100vw",
-                    visibility:
-                      index === activeSlideIndex ? "visible" : "hidden",
-                  }}
-                >
-                  <source src={slide.video} type="video/mp4"></source>
-                </video>
               </div>
             </div>
           ))}
         </div>
+        <video
+          autoPlay
+          loop
+          ref={videoRef}
+          muted
+          id="bgVideo"
+          // className="invisible md:visible"
+          style={{
+            height: "100vh",
+            minHeight: "100vh",
+            width: "100vw",
+          }}
+        >
+          <source src="/videoes/videoplayback.mp4" type="video/mp4"></source>
+        </video>
       </div>
     </div>
   );
