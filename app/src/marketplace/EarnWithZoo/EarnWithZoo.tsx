3eed2cc85a13ba2509b320762be16829e6b22b12
Zach Kelling <z@zeekay.io>
2023-02-18 14:53:09 -0600

    Switch to new marketplace

--- app/src/marketplace/EarnWithZoo/EarnWithZoo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import EarnCard from './EarnCard'
-
-const EarnWithZoo = ({ children }) => {
-  return <div className="grid md:grid-cols-3 gap-6 min-h-[300px] w-full bg-transparent ">{children}</div>
-}
-
-export default EarnWithZoo
