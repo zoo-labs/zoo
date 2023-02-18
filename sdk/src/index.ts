d3be188cfb097d0599558f2b5770e2754ced890d
Zach Kelling <z@zeekay.io>
2023-02-18 14:49:33 -0600

    Add new SDK

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,15 +1,3 @@
-import JSBI from 'jsbi'
-
-// Export JSBI
-export { JSBI }
-
-export * from './constants'
-export * from './errors'
-export * from './entities'
-export * from './functions'
-export * from './router'
-export * from './enums'
-export * from './router'
+export * from './actions'
+export * from './utils'
 export * from './types'
-export * from './utils/MultiRouterMath'
-export * from './limitorder'
---
+++ sdk/src/utils/request.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosRequestConfig, AxiosRequestHeaders } from 'axios'
+import { version } from '../../package.json'
+import { getClient } from '../'
+
+export function request(config: AxiosRequestConfig = {}) {
+  const client = getClient()
+  const currentReservoirChain = client.currentChain()
+  const headers: AxiosRequestHeaders = {
+    'Content-Type': 'application/json',
+    'x-rkc-version': version,
+  }
+  if (currentReservoirChain?.apiKey) {
+    headers['x-api-key'] = currentReservoirChain.apiKey
+  }
+  return axios.request({ headers: headers, ...config })
+}
