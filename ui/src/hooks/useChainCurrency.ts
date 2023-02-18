0a98288844a6e6aa890209996f16694fe5d6f132
Zach Kelling <z@zeekay.io>
2023-02-18 14:49:40 -0600

    Add new ui package

---
+++ ui/src/hooks/useChainCurrency.ts
@@ -0,0 +1,39 @@
+import { getClient } from '@reservoir0x/reservoir-sdk'
+import { constants } from 'ethers'
+import { Chain, goerli, mainnet, useNetwork } from 'wagmi'
+
+export default function (chainId?: number) {
+  const { chains } = useNetwork()
+  return getChainCurrency(chains, chainId)
+}
+
+export const getChainCurrency = (chains: Chain[], chainId?: number) => {
+  const client = getClient()
+  const reservoirChain = chainId
+    ? client.chains.find((chain) => chain.id === chainId)
+    : client.currentChain()
+
+  let chain = chains.find((chain) => reservoirChain?.id === chain.id)
+
+  if (!chain && chains.length > 0) {
+    chain = chains[0]
+  }
+
+  const ETHChains: number[] = [mainnet.id, goerli.id]
+
+  if (!chain || !chain.nativeCurrency || ETHChains.includes(chain.id)) {
+    return {
+      name: 'Ethereum',
+      symbol: 'ETH',
+      decimals: 18,
+      address: constants.AddressZero,
+      chainId: chain?.id || mainnet.id,
+    }
+  } else {
+    return {
+      ...chain.nativeCurrency,
+      address: constants.AddressZero,
+      chainId: chain.id,
+    }
+  }
+}
