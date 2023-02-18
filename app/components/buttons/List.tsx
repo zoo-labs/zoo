3d533003259949db2110bff8d4e0ed962b61c9ee
Zach Kelling <z@zeekay.io>
2023-02-18 15:07:08 -0600

    Switch to new marketplace

---
+++ app/components/buttons/List.tsx
@@ -0,0 +1,106 @@
+import { ListModal, ListStep, useTokens } from '@reservoir0x/reservoir-kit-ui'
+import { Button } from 'components/primitives'
+import {
+  cloneElement,
+  ComponentProps,
+  ComponentPropsWithoutRef,
+  FC,
+  ReactNode,
+  useContext,
+} from 'react'
+import { CSS } from '@stitches/react'
+import { SWRResponse } from 'swr'
+import { useAccount, useNetwork, useSigner, useSwitchNetwork } from 'wagmi'
+import { useConnectModal } from '@rainbow-me/rainbowkit'
+import { ToastContext } from 'context/ToastContextProvider'
+import { useMarketplaceChain } from 'hooks'
+
+type ListingCurrencies = ComponentPropsWithoutRef<
+  typeof ListModal
+>['currencies']
+
+type Props = {
+  token?: ReturnType<typeof useTokens>['data'][0]
+  buttonCss?: CSS
+  buttonChildren?: ReactNode
+  buttonProps?: ComponentProps<typeof Button>
+  mutate?: SWRResponse['mutate']
+}
+
+const List: FC<Props> = ({
+  token,
+  buttonCss,
+  buttonChildren,
+  buttonProps,
+  mutate,
+}) => {
+  const { isDisconnected } = useAccount()
+  const { openConnectModal } = useConnectModal()
+  const { addToast } = useContext(ToastContext)
+
+  const marketplaceChain = useMarketplaceChain()
+  const { switchNetworkAsync } = useSwitchNetwork({
+    chainId: marketplaceChain.id,
+  })
+
+  const { data: signer } = useSigner()
+  const { chain: activeChain } = useNetwork()
+
+  const isInTheWrongNetwork = Boolean(
+    signer && marketplaceChain.id !== activeChain?.id
+  )
+
+  let listingCurrencies: ListingCurrencies = undefined
+
+  const tokenId = token?.token?.tokenId
+  const contract = token?.token?.contract
+
+  const trigger = (
+    <Button css={buttonCss} color="primary" {...buttonProps}>
+      {buttonChildren}
+    </Button>
+  )
+
+  if (isDisconnected || isInTheWrongNetwork) {
+    return cloneElement(trigger, {
+      onClick: async () => {
+        if (switchNetworkAsync && activeChain) {
+          const chain = await switchNetworkAsync(marketplaceChain.id)
+          if (chain.id !== marketplaceChain.id) {
+            return false
+          }
+        }
+
+        if (!signer) {
+          openConnectModal?.()
+        }
+      },
+    })
+  } else
+    return (
+      <ListModal
+        trigger={trigger}
+        collectionId={contract}
+        tokenId={tokenId}
+        currencies={listingCurrencies}
+        onClose={(data, stepData, currentStep) => {
+          if (mutate && currentStep == ListStep.Complete) mutate()
+        }}
+        onListingError={(err: any) => {
+          if (err?.code === 4001) {
+            addToast?.({
+              title: 'User canceled transaction',
+              description: 'You have canceled the transaction.',
+            })
+            return
+          }
+          addToast?.({
+            title: 'Could not list token',
+            description: 'The transaction was not completed.',
+          })
+        }}
+      />
+    )
+}
+
+export default List
---
+++ app/stitches.config.ts
@@ -0,0 +1,290 @@
+import {
+  crimson,
+  slate,
+  crimsonDark,
+  violetDark,
+  slateDark,
+  greenDark,
+  green,
+  violetDarkA,
+  whiteA,
+  redDark,
+  red,
+  blackA,
+  violet,
+  violetA,
+} from '@radix-ui/colors'
+import { createStitches } from '@stitches/react'
+import type * as Stitches from '@stitches/react'
+import { reset } from 'utils/css/reset'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+})
+
+export const { createTheme, keyframes, styled, globalCss, getCssText } =
+  createStitches({
+    theme: {
+      colors: {
+        ...crimson,
+        ...violet,
+        ...violetA,
+        ...slate,
+        ...red,
+        ...whiteA,
+        ...blackA,
+        ...green,
+
+        //Aliases
+
+        //Primary
+        primary1: '$violet1',
+        primary2: '$violet2',
+        primary3: '$violet3',
+        primary4: '$violet4',
+        primary5: '$violet5',
+        primary6: '$violet6',
+        primary7: '$violet7',
+        primary8: '$violet8',
+        primary9: '$violet9',
+        primary10: '$violet10',
+        primary11: '$violet11',
+        primary12: '$violet12',
+
+        //Secondary
+        secondary1: '$violetA1',
+        secondary2: '$violetA2',
+        secondary3: '$violetA3',
+        secondary4: '$violetA4',
+        secondary5: '$violetA5',
+        secondary6: '$violetA6',
+        secondary7: '$violetA7',
+        secondary8: '$violetA8',
+        secondary9: '$violetA9',
+        secondary10: '$violetA10',
+        secondary11: '$violetA11',
+        secondary12: '$violetA12',
+
+        //Gray
+        gray1: '$slate1',
+        gray2: '$slate2',
+        gray3: '$slate3',
+        gray4: '$slate4',
+        gray5: '$slate5',
+        gray6: '$slate6',
+        gray7: '$slate7',
+        gray8: '$slate8',
+        gray9: '$slate9',
+        gray10: '$slate10',
+        gray11: '$slate11',
+        gray12: '$slate12',
+
+        //Red
+        red1: '$crimson1',
+        red2: '$crimson2',
+        red3: '$crimson3',
+        red4: '$crimson4',
+        red5: '$crimson5',
+        red6: '$crimson6',
+        red7: '$crimson7',
+        red8: '$crimson8',
+        red9: '$crimson9',
+        red10: '$crimson10',
+        red11: '$crimson11',
+        red12: '$crimson12',
+
+        neutralBg: 'white',
+        neutralBgSubtle: 'white',
+        panelShadow: 'rgba(0,0,0,0.1)',
+        panelBg: '$gray2',
+        panelBorder: 'transparent',
+      },
+      space: {
+        1: '4px',
+        2: '8px',
+        3: '12px',
+        4: '16px',
+        5: '32px',
+        6: '64px',
+      },
+      fontSizes: {},
+      fontWeights: {},
+      fonts: {
+        body: inter.style.fontFamily,
+        button: '$body',
+      },
+      lineHeights: {},
+      letterSpacings: {},
+      sizes: {},
+      radii: {},
+      shadows: {},
+      transitions: {},
+      breakpoints: {
+        sm: 100,
+      },
+    },
+    utils: {
+      // MARGIN
+      m: (value: Stitches.PropertyValue<'margin'>) => ({
+        margin: value,
+      }),
+      mx: (value: Stitches.PropertyValue<'margin'>) => ({
+        marginLeft: value,
+        marginRight: value,
+      }),
+      my: (value: Stitches.PropertyValue<'margin'>) => ({
+        marginTop: value,
+        marginBottom: value,
+      }),
+      mt: (value: Stitches.PropertyValue<'margin'>) => ({
+        marginTop: value,
+      }),
+      mb: (value: Stitches.PropertyValue<'margin'>) => ({
+        marginBottom: value,
+      }),
+      ml: (value: Stitches.PropertyValue<'margin'>) => ({
+        marginLeft: value,
+      }),
+      mr: (value: Stitches.PropertyValue<'margin'>) => ({
+        marginRight: value,
+      }),
+
+      // PADDING
+      p: (value: Stitches.PropertyValue<'padding'>) => ({
+        padding: value,
+      }),
+      px: (value: Stitches.PropertyValue<'padding'>) => ({
+        paddingLeft: value,
+        paddingRight: value,
+      }),
+      py: (value: Stitches.PropertyValue<'padding'>) => ({
+        paddingTop: value,
+        paddingBottom: value,
+      }),
+      pt: (value: Stitches.PropertyValue<'padding'>) => ({
+        paddingTop: value,
+      }),
+      pb: (value: Stitches.PropertyValue<'padding'>) => ({
+        paddingBottom: value,
+      }),
+      pl: (value: Stitches.PropertyValue<'padding'>) => ({
+        paddingLeft: value,
+      }),
+      pr: (value: Stitches.PropertyValue<'padding'>) => ({
+        paddingRight: value,
+      }),
+      // DIMENSIONS
+      w: (value: Stitches.PropertyValue<'width'>) => ({
+        width: value,
+      }),
+      h: (value: Stitches.PropertyValue<'height'>) => ({
+        height: value,
+      }),
+      size: (value: Stitches.PropertyValue<'width'>) => ({
+        width: value,
+        height: value,
+      }),
+      // GRID
+      colSpan: (value: number | 'full') => {
+        if (value === 'full') {
+          return {
+            gridColumn: '1 / -1',
+          }
+        }
+        return {
+          gridColumn: `span ${value} / span ${value}`,
+        }
+      },
+    },
+    media: {
+      sm: '(min-width: 600px)',
+      md: '(min-width: 900px)',
+      lg: '(min-width: 1200px)',
+      xl: '(min-width: 1400px)',
+      bp300: '(min-width: 300px)',
+      bp400: '(min-width: 400px)',
+      bp500: '(min-width: 500px)',
+      bp600: '(min-width: 600px)',
+      bp700: '(min-width: 700px)',
+      bp800: '(min-width: 800px)',
+      bp900: '(min-width: 900px)',
+      bp1000: '(min-width: 1000px)',
+      bp1100: '(min-width: 1100px)',
+      bp1200: '(min-width: 1200px)',
+      bp1300: '(min-width: 1300px)',
+      bp1400: '(min-width: 1400px)',
+      motion: '(prefers-reduced-motion)',
+      hover: '(any-hover: hover)',
+      dark: '(prefers-color-scheme: dark)',
+      light: '(prefers-color-scheme: light)',
+    },
+  })
+
+export const globalReset = globalCss(reset)
+
+export const darkTheme = createTheme({
+  colors: {
+    ...crimsonDark,
+    ...violetDark,
+    ...violetDarkA,
+    ...slateDark,
+    ...greenDark,
+    ...whiteA,
+    ...redDark,
+    ...blackA,
+
+    //Aliases
+
+    //Primary
+    primary1: '$violet1',
+    primary2: '$violet2',
+    primary3: '$violet3',
+    primary4: '$violet4',
+    primary5: '$violet5',
+    primary6: '$violet6',
+    primary7: '$violet7',
+    primary8: '$violet8',
+    primary9: '$violet9',
+    primary10: '$violet10',
+    primary11: '$violet11',
+    primary12: '$violet12',
+
+    //Secondary
+    secondary1: '$violetA1',
+    secondary2: '$violetA2',
+    secondary3: '$violetA3',
+    secondary4: '$violetA4',
+    secondary5: '$violetA5',
+    secondary6: '$violetA6',
+    secondary7: '$violetA7',
+    secondary8: '$violetA8',
+    secondary9: '$violetA9',
+    secondary10: '$violetA10',
+    secondary11: '$violetA11',
+    secondary12: '$violetA12',
+
+    //Gray
+    gray1: '$slate1',
+    gray2: '$slate2',
+    gray3: '$slate3',
+    gray4: '$slate4',
+    gray5: '$slate5',
+    gray6: '$slate6',
+    gray7: '$slate7',
+    gray8: '$slate8',
+    gray9: '$slate9',
+    gray10: '$slate10',
+    gray11: '$slate11',
+    gray12: '$slate12',
+
+    accent: '#7000FF',
+
+    neutralBgSubtle: '$gray3',
+    neutralBg: '$gray1',
+
+    panelBg: '$gray3',
+    panelBorder: '$slate7',
+    panelShadow: 'transparent',
+  },
+})
