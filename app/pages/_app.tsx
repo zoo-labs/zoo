99f16d8e492a8f8fe8368109bd502dbe450274b3
Zach Kelling <z@zeekay.io>
2023-05-19 03:04:33 -0500

    Fix theme

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -3,7 +3,7 @@ import AnalyticsProvider, {
 } from 'components/AnalyticsProvider'
 initializeAnalytics()
 
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 import type { AppContext, AppProps } from 'next/app'
 import { default as NextApp } from 'next/app'
 import { ThemeProvider, useTheme } from 'next-themes'
