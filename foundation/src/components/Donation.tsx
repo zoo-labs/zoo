92a75d0129b62dfa8859f4d0e71148dea9017a2d
woobin <woobinpro@gmail.com>
2023-05-25 17:06:44 -0700

    foundation-woo0525

---
+++ foundation/src/components/Donation.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+function Donation() {
+    const comments = [
+        {
+          title: "$50",
+          comment: "Can go towards planting native species and restoring habitats and water sources.",
+          type: "normal"
+        },
+        {
+          title: "$100",
+          comment: "Fund staff and volunteers working hands-on to protect and care for endangered animals.",
+          type: "normal"
+        },
+        {
+          title: "Custom",
+          comment: "85% of your donation funds program activities and 15% goes towards operational costs.",
+          type: "custom"
+        }
+      ];
+  return (
+    <div className="bg-black py-52">
+        <p className='text-gray-300 text-sm text-center'>Fuel Conservation Impact</p>
+        <h1 className='text-white text-center md:text-3xl xl:text-5xl max-md:text-2xl mt-5 mb-12'>Select your donation</h1>
+      <div className='flex max-md:flex-col xl:px-56 lg:px-40 px-32 space-x-16'>
+      {comments.map((data, index) => (
+        <div className='max-md:w-full flex flex-col items-center justify-between w-1/3 px-16 py-16 border rounded-xl border-white'>
+            <h1 className='text-white md:text-xl xl:text-2xl max-md:text-xl max-md:my-5'>{data.title}</h1>
+            <p className='text-white md:text-md lg:text-lg xl:text-xl max-md:pb-10 py-10'>{data.comment}</p>
+            { data.type == 'normal' ? (
+                <a className='flex items-center cursor-pointer text-white md:text-sm lg:text-md xl:text-lg max-md:pb-10'>
+                    <span className='pr-[15px]'>Donate</span>
+                    <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M6 5.5L1.19924 10.5L0 9.24901L3.59962 5.5L6.08905e-06 1.751L1.19924 0.5L6 5.5Z" fill="white"/>
+                    </svg>
+                </a>
+            ) : (
+                <div className='flex flex-col w-full'>
+                    <div className='flex items-center text-white md:text-md lg:text-lg xl:text-xl pb-2'>
+                        <span>$</span>
+                        <input className='pl-3 border-no bg-transparent outline-none w-full text-sm' placeholder='Enter your donation amount'/>
+                        <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M6 5.5L1.19924 10.5L0 9.24901L3.59962 5.5L6.08905e-06 1.751L1.19924 0.5L6 5.5Z" fill="white"/>
+                        </svg>
+                    </div>
+                    <hr />
+                </div>
+            )
+            }
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
+
+export default Donation;
