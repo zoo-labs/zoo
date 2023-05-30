9178c7092d564580cce3fd9027034bdae1fd6ffe
woobin <woobinpro@gmail.com>
2023-05-30 07:12:48 -0700

    finished responsive design

--- foundation/src/components/Campaign.tsx
+++ foundation/src/components/Campaign.tsx
@@ -3,13 +3,13 @@ function Campaign() {
   return (
     <div className="bg-black">
         <div className='w-full text-center items-center flex flex-col justify-between pb-20'>
-            <p className='text-white md:text-sm lg:text-md xl:text-lg max-md:pb-10 pb-5 pt-16'>Bring your community TO the Zoo foundation</p>
-            <h1 className='text-white md:text-4xl xl:text-6xl max-md:text-3xl max-md:my-5'>Create your own campaign.</h1>
+            <p className='text-white md:text-sm lg:text-md xl:text-lg max-md:text-lg max-md:px-24 pb-5 pt-16'>Bring your community TO the Zoo foundation</p>
+            <h1 className='text-white md:text-4xl xl:text-6xl max-md:text-4xl max-md:my-5'>Create your own campaign.</h1>
             
-            <p className='text-white md:text-lg lg:text-xl xl:text-2xl max-md:pb-10 pb-8 pt-8'>A self-tailored campaign to raise funds for<br /> these animals by leveraging your network. </p>
+            <p className='text-white md:text-lg lg:text-xl xl:text-2xl max-md:text-lg pb-10 pt-8'>A self-tailored campaign to raise funds for<br /> these animals by leveraging your network. </p>
             <a
                 href="#"
-                className="text-black w-[150px] hover:bg-gray-700 bg-white hover:text-white px-6 py-1 rounded-full border-white border-2 text-lg font-medium hidden md:block"
+                className="text-black w-[150px] hover:bg-gray-700 bg-white hover:text-white px-6 py-1 rounded-full border-white border-2 text-lg font-medium md:block"
             >
                 Learn More
             </a>
--- foundation/src/pages/animals/Header.tsx
+++ foundation/src/pages/animals/Header.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 function Header() {
   return (
-    <div className="bg-black px-64">
-      <div className="flex items-center justify-between py-20">
-        <div className='relative md:w-1/2 pr-32'>
+    <div className="bg-black md:px-64">
+      <div className="flex max-md:flex-col items-center justify-between py-20">
+        <div className='relative md:w-1/2 max-md:w-full md:pr-32'>
             <Image
                 className='intro-bg border rounded-xl p-3'
                 src="/images/trading_card1.png"
@@ -12,26 +12,26 @@ function Header() {
                 height='1000'
                 alt=''
             />
-            <div className='absolute top-4 right-36 w-[45px] h-[45px] flex justify-center bg-white rounded-full'>
+            <div className='absolute top-4 md:right-36 max-md:right-4 w-[45px] h-[45px] flex justify-center bg-white rounded-full'>
                 <svg width="15" height="45" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12.7586 2.72001H3.53201L4.52469 1.74131C4.78597 1.48362 4.88806 1.108 4.79244 0.756034C4.69675 0.403999 4.41795 0.129101 4.06091 0.0347546C3.70395 -0.059523 3.32299 0.0411294 3.06163 0.298754L0.303024 3.01872C0.109057 3.21004 0 3.46949 0 3.74C0 4.01051 0.109052 4.26997 0.303024 4.46128L3.06163 7.18124C3.32299 7.43886 3.70395 7.53951 4.06091 7.44524C4.41795 7.35089 4.69676 7.076 4.79244 6.72396C4.88806 6.372 4.78598 5.99637 4.52469 5.73868L3.53201 4.75998H12.7586C14.1305 4.75998 15.4461 5.29732 16.4162 6.2537C17.3862 7.21014 17.9311 8.50728 17.9311 9.86001C17.9311 11.2127 17.3862 12.5098 16.4162 13.4663C15.4462 14.4227 14.1306 14.96 12.7586 14.96H6.55177C6.18216 14.96 5.84071 15.1545 5.65587 15.47C5.4711 15.7856 5.4711 16.1745 5.65587 16.49C5.84071 16.8056 6.18216 17 6.55177 17H12.7586C16.7516 17 20 13.7972 20 9.8601C20 5.92304 16.7516 2.72019 12.7586 2.72019V2.72001Z" fill="black"/>
                 </svg>
             </div>
         </div>
-        <div className='w-1/2 max-md:w-full flex flex-col justify-between'>
-            <h1 className='text-white md:text-5xl xl:text-6xl max-md:text-3xl max-md:my-5 md:pb-16'>Select any trading card to start learning.</h1>
-            <p className='text-white md:text-lg lg:text-xl xl:text-2xl max-md:pb-10 md:pb-10'>Discover our vibrant, fact-packed Zoo trading cards – a virtual treasure trove of animal knowledge and endangered species insights!</p>
+        <div className='w-1/2 max-md:w-full flex flex-col justify-between max-md:px-4'>
+            <h1 className='text-white md:text-5xl xl:text-6xl max-md:text-3xl max-md:my-5 md:pb-16'>Amur Leopard</h1>
+            <p className='text-white md:text-lg lg:text-xl xl:text-2xl max-md:pb-10 md:pb-10'>The Amur leopard is one of 10 subspecies of leopard, and one of the world’s rarest cats, with an estimated population of under 70 individuals left in the wild. They all live in a small area that hugs Russia’s far eastern border with China.<br /><br />Many think of leopards stalking savannas of Africa but in the Russian Far East, a rare subspecies has adapted to life in the temperate forests that make up the northern-most part of the species’ range.</p>
             
-            <div className='flex items-center md:pt-10 md:space-x-8'>
+            <div className='flex items-center md:pt-10 space-x-8'>
                 <a
                     href="#"
-                    className="text-black hover:bg-gray-700 bg-white hover:text-white px-12 py-1 rounded-full border-white border-2 text-lg font-medium hidden md:block"
+                    className="text-black hover:bg-gray-700 bg-white hover:text-white px-12 py-1 rounded-full border-white border-2 text-lg font-medium md:block"
                 >
                     Buy $25
                 </a>
                 <a
                     href="#"
-                    className="text-white hover:bg-gray-700 bg-black hover:text-black px-6 py-1 rounded-full border-white border-2 text-lg font-medium hidden md:block"
+                    className="text-white hover:bg-gray-700 bg-black hover:text-black px-6 py-1 rounded-full border-white border-2 text-lg font-medium md:block"
                 >
                     + Cart
                 </a>
@@ -39,14 +39,14 @@ function Header() {
         </div>
         
       </div>
-      <div className='flex items-center justify-center space-x-8 collect-link'>
-        <a href="/" className="text-white px-3 py-2  text-sm font-medium">Red Wolf</a>
-        <a href="/" className="text-white px-3 py-2 active text-sm font-medium">Nubian Giraffe</a>
-        <a href="/" className="text-white px-3 py-2  text-sm font-medium">Amur Leopard</a>
-        <a href="/" className="text-white px-3 py-2  text-sm font-medium">Pygmy Hippo</a>
-        <a href="/" className="text-white px-3 py-2  text-sm font-medium">Siberian Tiger</a>
-        <a href="/" className="text-white px-3 py-2  text-sm font-medium">Sumatran Elephant</a>
-        <a href="/" className="text-white px-3 py-2  text-sm font-medium">Javan Rhino</a>
+      <div className='max-md:hidden flex items-center justify-center space-x-8 collect-link'>
+        <a href="/animals/red_wolf" className="text-white px-3 py-2  text-sm font-medium">Red Wolf</a>
+        <a href="/animals/nubian_giraffe" className="text-white px-3 py-2 active text-sm font-medium">Nubian Giraffe</a>
+        <a href="/animals/amur_leopard" className="text-white px-3 py-2  text-sm font-medium">Amur Leopard</a>
+        <a href="/animals/pygmy_hippo" className="text-white px-3 py-2  text-sm font-medium">Pygmy Hippo</a>
+        <a href="/animals/siberian_tiger" className="text-white px-3 py-2  text-sm font-medium">Siberian Tiger</a>
+        <a href="/animals/sumatran_elephant" className="text-white px-3 py-2  text-sm font-medium">Sumatran Elephant</a>
+        <a href="/animals/javen_rhino" className="text-white px-3 py-2  text-sm font-medium">Javan Rhino</a>
       </div>
     </div>
   );
