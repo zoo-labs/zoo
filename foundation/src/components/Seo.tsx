b4ce2a6694e7b1a917e4d43537c9af94175299b0
woobin <woobinpro@gmail.com>
2023-05-30 00:07:07 -0700

    Added the package name

--- foundation/src/components/Seo.tsx
+++ foundation/src/components/Seo.tsx
@@ -3,8 +3,8 @@ import { useRouter } from 'next/router';
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Zoo Foundation',
+  siteName: 'Zoo Foundation',
   description:
     'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
