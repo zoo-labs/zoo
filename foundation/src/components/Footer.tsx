c7e9014d70f0de0df1483df9b2fedba1fc3e4396
woobin <woobinpro@gmail.com>
2023-05-31 15:50:00 -0700

    added some links

--- foundation/src/components/Footer.tsx
+++ foundation/src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import Link from 'next/link';
 import {FaTelegram, FaTwitter, FaInstagram, FaDiscord, FaYoutube} from "react-icons/fa";
 import {BsMedium} from "react-icons/bs"
 function Footer() {
@@ -11,36 +11,36 @@ function Footer() {
             <p className='text-lg text-white'>ZOO FOUNDATION</p>
             <p className='text-md text-white'>All rights reserved.© 2023 Zoo Labs Foundation. </p>
             <div className='flex items-center space-x-2 text-white'>
-                <a href="#"> <FaTwitter /> </a>
-                <a href="#"> <FaTelegram /> </a>
-                <a href="#"> <FaInstagram /> </a>
-                <a href="#"> <FaDiscord /> </a>
-                <a href="#"> <BsMedium /> </a>
-                <a href="#"> <FaYoutube /> </a>
+                <Link href="#"> <FaTwitter /> </Link>
+                <Link href="#"> <FaTelegram /> </Link>
+                <Link href="#"> <FaInstagram /> </Link>
+                <Link href="#"> <FaDiscord /> </Link>
+                <Link href="#"> <BsMedium /> </Link>
+                <Link href="#"> <FaYoutube /> </Link>
             </div>
         </div>
         <div className='flex flex-col space-y-8'>
-            <a href="/about" className='text-lg text-white'>About</a>
-            <a href="/getinvolved" className='text-lg text-white'>Get Involved</a>
-            <a href="/donation" className='text-lg text-white'>Donate</a>
-            <a href="#" className='text-lg text-white'>Volunteer</a>
-            <a href="#" className='text-lg text-white'>Initiatives</a>
+            <Link href="/about" className='text-lg text-white'>About</Link>
+            <Link href="/getinvolved" className='text-lg text-white'>Get Involved</Link>
+            <Link href="/donation" className='text-lg text-white'>Donate</Link>
+            <Link href="#" className='text-lg text-white'>Volunteer</Link>
+            <Link href="#" className='text-lg text-white'>Initiatives</Link>
         </div>
         <div className='flex flex-col space-y-8'>
-            <a href="/collect" className='text-lg text-white'>Digital Collectibles</a>
-            <a href="/animals/red_wolf" className='text-lg text-white'>Red Wolf</a>
-            <a href="/animals/nubian_giraffe" className='text-lg text-white'>Nubian Giraffe</a>
-            <a href="/animals/amur_leopard" className='text-lg text-white'>Amur Leopard</a>
-            <a href="/animals/sumatran_elephant" className='text-lg text-white'>Sumatran Elephant</a>
+            <Link href="/collect" className='text-lg text-white'>Digital Collectibles</Link>
+            <Link href="/animals/red_wolf" className='text-lg text-white'>Red Wolf</Link>
+            <Link href="/animals/nubian_giraffe" className='text-lg text-white'>Nubian Giraffe</Link>
+            <Link href="/animals/amur_leopard" className='text-lg text-white'>Amur Leopard</Link>
+            <Link href="/animals/sumatran_elephant" className='text-lg text-white'>Sumatran Elephant</Link>
         </div>
         <div className='flex flex-col space-y-8'>
-            <a href="/animals/javen_rhino" className='text-lg text-white'>Javan Rhino</a>
-            <a href="/animals/pygmy_hippo" className='text-lg text-white'>Pygmy Hippo </a>
-            <a href="/animals/siberian_tiger" className='text-lg text-white'>Siberian Tiger</a>
+            <Link href="/animals/javen_rhino" className='text-lg text-white'>Javan Rhino</Link>
+            <Link href="/animals/pygmy_hippo" className='text-lg text-white'>Pygmy Hippo </Link>
+            <Link href="/animals/siberian_tiger" className='text-lg text-white'>Siberian Tiger</Link>
         </div>
         <div className='flex flex-col space-y-8'>
-            <a href="/terms" className='text-lg text-white'>Terms of Use</a>
-            <p className='text-md text-white'>zoolabs.io</p>
+            <Link href="/terms" className='text-lg text-white'>Terms of Use</Link>
+            <Link href="https://zoolabs.io" className='text-md text-white'>zoolabs.io</Link>
             <p className='text-md text-white'>hello@zoo.ngo</p>
         </div>
       </div>
@@ -49,26 +49,26 @@ function Footer() {
       <div className='flex flex-col items-center justify-center'>
         <p className='text-xl text-white'>ZOO LABS</p>
         <div className='flex items-center pt-8 space-x-16'>
-          <a href="/" className='text-lg text-white'>Home</a>
-          <a href="/about" className='text-lg text-white'>About</a>
-          <a href="/getinvolved" className='text-lg text-white'>Get Involved</a>
+          <Link href="/" className='text-lg text-white'>Home</Link>
+          <Link href="/about" className='text-lg text-white'>About</Link>
+          <Link href="/getinvolved" className='text-lg text-white'>Get Involved</Link>
         </div>
         <div className='flex items-center pt-6 space-x-16'>
-          <a href="/collect" className='text-lg text-white'>Collect</a>
-            <a href="/donation" className='text-lg text-white'>Donate</a>
+          <Link href="/collect" className='text-lg text-white'>Collect</Link>
+            <Link href="/donation" className='text-lg text-white'>Donate</Link>
         </div>
         <div className='flex items-center space-x-2 text-white pt-10'>
-                <a href="#"> <FaTwitter /> </a>
-                <a href="#"> <FaTelegram /> </a>
-                <a href="#"> <FaInstagram /> </a>
-                <a href="#"> <FaDiscord /> </a>
-                <a href="#"> <BsMedium /> </a>
-                <a href="#"> <FaYoutube /> </a>
+                <Link href="#"> <FaTwitter /> </Link>
+                <Link href="#"> <FaTelegram /> </Link>
+                <Link href="#"> <FaInstagram /> </Link>
+                <Link href="#"> <FaDiscord /> </Link>
+                <Link href="#"> <BsMedium /> </Link>
+                <Link href="#"> <FaYoutube /> </Link>
         </div>
         <hr className='w-full bg-white my-4'/>
         <div className='flex items-center space-x-16'>
-          <a href="#" className='text-lg text-white '>Privacy Policy</a>
-            <a href="/terms" className='text-lg text-white'>Term of service</a>
+          <Link href="#" className='text-lg text-white '>Privacy Policy</Link>
+            <Link href="/terms" className='text-lg text-white'>Term of service</Link>
         </div>
         <p className='text-lg text-white pt-6 pb-16'>© 2023 Zoo Labs Foundation</p>
       </div>
--- foundation/src/pages/collect/TradingCard.tsx
+++ foundation/src/pages/collect/TradingCard.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 function TradingCard() {
   return (
     <div className="bg-black pb-32 md:px-64 px-4">
@@ -18,12 +18,12 @@ function TradingCard() {
             <h1 className='text-white md:text-5xl xl:text-6xl max-md:text-3xl max-md:my-5 md:pb-16 max-md:hidden'>Select any trading card to start learning.</h1>
             <p className='text-white md:text-lg lg:text-xl xl:text-2xl max-md:pb-10 md:pb-10'>Discover our vibrant, fact-packed Zoo trading cards – a virtual treasure trove of animal knowledge and endangered species insights!</p>
             
-            <a className='flex items-center md:pt-10 cursor-pointer text-white md:text-sm lg:text-md xl:text-lg max-md:pb-10'>
+            <Link href='/collect' className='flex items-center md:pt-10 cursor-pointer text-white md:text-sm lg:text-md xl:text-lg max-md:pb-10'>
                 <span className='pr-[15px]'>View All Cards</span>
                 <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M6 5.5L1.19924 10.5L0 9.24901L3.59962 5.5L6.08905e-06 1.751L1.19924 0.5L6 5.5Z" fill="white"/>
                 </svg>
-            </a>
+            </Link>
         </div>
       </div>
     </div>
