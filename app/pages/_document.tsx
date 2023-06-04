f01a066d5176d41b7d6e16b1fedd426ecbeb3565
zooqueen <antje@lux.finance>
2023-06-03 20:35:59 -0700

    Update theme and logo

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,303 +1,65 @@
-import {
-  crimson,
-  slate,
-  crimsonDark,
-  indigoDark,
-  slateDark,
-  greenDark,
-  green,
-  indigoDarkA,
-  whiteA,
-  redDark,
-  red,
-  blackA,
-  indigo,
-  indigoA,
-} from '@radix-ui/colors'
-import { createStitches } from '@stitches/react'
-import type * as Stitches from '@stitches/react'
-import { reset } from 'utils/css/reset'
-//import { Inter } from 'next/font/google'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+import { getCssText } from '../stitches.config'
 
-//const inter = Inter({
-//  subsets: ['latin'],
-//})
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
 
-// CONFIGURABLE: Here you can update all your theming (outside of ReservoirKit which can be configured in the app.tsx)
-// The theme colors are all already hooked up to stitches scales, so you just need to swap them.
-// Don't forget to check the dark mode themes below.
-// More on Stitches theme tokens: https://stitches.dev/docs/tokens
-// More on Radix color scales: https://www.radix-ui.com/docs/colors/palette-composition/the-scales
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+        </Head>
 
-export const { createTheme, keyframes, styled, globalCss, getCssText } =
-  createStitches({
-    theme: {
-      colors: {
-        ...crimson,
-        ...indigo,
-        ...indigoA,
-        ...slate,
-        ...red,
-        ...whiteA,
-        ...blackA,
-        ...green,
-        ...indigo,
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        //Aliases
+        {/* Meta tags */}
+        <meta name="keywords" content="nft, ethereum, protocol" />
+        <meta name="keywords" content="NFT, API, Protocol" />
 
-        //Primary
-        primary1: '$indigo1',
-        primary2: '$indigo2',
-        primary3: '$indigo3',
-        primary4: '$indigo4',
-        primary5: '$indigo5',
-        primary6: '$indigo6',
-        primary7: '$indigo7',
-        primary8: '$indigo8',
-        primary9: '$indigo9',
-        primary10: '$indigo10',
-        primary11: '$indigo11',
-        primary12: '$indigo12',
+        {/* Favicon */}
+        <link
+          rel="shortcut icon"
+          type="image/svg"
+          href="/ZooLogo.svg"
+        />
 
-        //Secondary
-        secondary1: '$indigoA1',
-        secondary2: '$indigoA2',
-        secondary3: '$indigoA3',
-        secondary4: '$indigoA4',
-        secondary5: '$indigoA5',
-        secondary6: '$indigoA6',
-        secondary7: '$indigoA7',
-        secondary8: '$indigoA8',
-        secondary9: '$indigoA9',
-        secondary10: '$indigoA10',
-        secondary11: '$indigoA11',
-        secondary12: '$indigoA12',
+        {/* Reservoir meta tags */}
+        <meta property="reservoir:title" content="ZOO Market" />
+        <meta property="reservoir:icon" content="/ZooLogo.svg" />
+        <meta
+          property="reservoir:token-url-mainnet"
+          content="/collection/ethereum/${contract}/${tokenId}"
+        />
+        <meta
+          property="reservoir:token-url-goerli"
+          content="/collection/goerli/${contract}/${tokenId}"
+        />
+        <meta
+          property="reservoir:token-url-polygon"
+          content="/collection/polygon/${contract}/${tokenId}"
+        />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
 
-        //Gray
-        gray1: '$slate1',
-        gray2: '$slate2',
-        gray3: '$slate3',
-        gray4: '$slate4',
-        gray5: '$slate5',
-        gray6: '$slate6',
-        gray7: '$slate7',
-        gray8: '$slate8',
-        gray9: '$slate9',
-        gray10: '$slate10',
-        gray11: '$slate11',
-        gray12: '$slate12',
-
-        //Red
-        red1: '$crimson1',
-        red2: '$crimson2',
-        red3: '$crimson3',
-        red4: '$crimson4',
-        red5: '$crimson5',
-        red6: '$crimson6',
-        red7: '$crimson7',
-        red8: '$crimson8',
-        red9: '$crimson9',
-        red10: '$crimson10',
-        red11: '$crimson11',
-        red12: '$crimson12',
-
-        neutralBg: 'white',
-        neutralBgSubtle: 'white',
-        panelShadow: 'rgba(0,0,0,0.1)',
-        panelBg: '$gray2',
-        panelBorder: 'transparent',
-        dropdownBg: 'white',
-        sidebarOverlay: 'black',
-      },
-      space: {
-        1: '4px',
-        2: '8px',
-        3: '12px',
-        4: '16px',
-        5: '32px',
-        6: '64px',
-      },
-      fontSizes: {},
-      fontWeights: {},
-      fonts: {
-        body: 'Inter',
-        //body: inter.style.fontFamily,
-        button: '$body',
-      },
-      lineHeights: {},
-      letterSpacings: {},
-      sizes: {},
-      radii: {},
-      shadows: {},
-      transitions: {},
-      breakpoints: {
-        sm: 100,
-      },
-    },
-    utils: {
-      // MARGIN
-      m: (value: Stitches.PropertyValue<'margin'>) => ({
-        margin: value,
-      }),
-      mx: (value: Stitches.PropertyValue<'margin'>) => ({
-        marginLeft: value,
-        marginRight: value,
-      }),
-      my: (value: Stitches.PropertyValue<'margin'>) => ({
-        marginTop: value,
-        marginBottom: value,
-      }),
-      mt: (value: Stitches.PropertyValue<'margin'>) => ({
-        marginTop: value,
-      }),
-      mb: (value: Stitches.PropertyValue<'margin'>) => ({
-        marginBottom: value,
-      }),
-      ml: (value: Stitches.PropertyValue<'margin'>) => ({
-        marginLeft: value,
-      }),
-      mr: (value: Stitches.PropertyValue<'margin'>) => ({
-        marginRight: value,
-      }),
-
-      // PADDING
-      p: (value: Stitches.PropertyValue<'padding'>) => ({
-        padding: value,
-      }),
-      px: (value: Stitches.PropertyValue<'padding'>) => ({
-        paddingLeft: value,
-        paddingRight: value,
-      }),
-      py: (value: Stitches.PropertyValue<'padding'>) => ({
-        paddingTop: value,
-        paddingBottom: value,
-      }),
-      pt: (value: Stitches.PropertyValue<'padding'>) => ({
-        paddingTop: value,
-      }),
-      pb: (value: Stitches.PropertyValue<'padding'>) => ({
-        paddingBottom: value,
-      }),
-      pl: (value: Stitches.PropertyValue<'padding'>) => ({
-        paddingLeft: value,
-      }),
-      pr: (value: Stitches.PropertyValue<'padding'>) => ({
-        paddingRight: value,
-      }),
-      // DIMENSIONS
-      w: (value: Stitches.PropertyValue<'width'>) => ({
-        width: value,
-      }),
-      h: (value: Stitches.PropertyValue<'height'>) => ({
-        height: value,
-      }),
-      size: (value: Stitches.PropertyValue<'width'>) => ({
-        width: value,
-        height: value,
-      }),
-      // GRID
-      colSpan: (value: number | 'full') => {
-        if (value === 'full') {
-          return {
-            gridColumn: '1 / -1',
-          }
-        }
-        return {
-          gridColumn: `span ${value} / span ${value}`,
-        }
-      },
-    },
-    media: {
-      sm: '(min-width: 600px)',
-      md: '(min-width: 900px)',
-      lg: '(min-width: 1200px)',
-      xl: '(min-width: 1400px)',
-      bp300: '(min-width: 300px)',
-      bp400: '(min-width: 400px)',
-      bp500: '(min-width: 500px)',
-      bp600: '(min-width: 600px)',
-      bp700: '(min-width: 700px)',
-      bp800: '(min-width: 800px)',
-      bp900: '(min-width: 900px)',
-      bp1000: '(min-width: 1000px)',
-      bp1100: '(min-width: 1100px)',
-      bp1200: '(min-width: 1200px)',
-      bp1300: '(min-width: 1300px)',
-      bp1400: '(min-width: 1400px)',
-      bp1500: '(min-width: 1500px)',
-      motion: '(prefers-reduced-motion)',
-      hover: '(any-hover: hover)',
-      dark: '(prefers-color-scheme: dark)',
-      light: '(prefers-color-scheme: light)',
-    },
-  })
-
-export const globalReset = globalCss(reset)
-
-export const darkTheme = createTheme({
-  colors: {
-    ...crimsonDark,
-    ...indigoDark,
-    ...indigoDarkA,
-    ...slateDark,
-    ...greenDark,
-    ...whiteA,
-    ...redDark,
-    ...blackA,
-
-    //Aliases
-
-    //Primary
-    primary1: '$indigo1',
-    primary2: '$indigo2',
-    primary3: '$indigo3',
-    primary4: '$indigo4',
-    primary5: '$indigo5',
-    primary6: '$indigo6',
-    primary7: '$indigo7',
-    primary8: '$indigo8',
-    primary9: '$indigo9',
-    primary10: '$indigo10',
-    primary11: '$indigo11',
-    primary12: '$indigo12',
-
-    //Secondary
-    secondary1: '$indigoA1',
-    secondary2: '$indigoA2',
-    secondary3: '$indigoA3',
-    secondary4: '$indigoA4',
-    secondary5: '$indigoA5',
-    secondary6: '$indigoA6',
-    secondary7: '$indigoA7',
-    secondary8: '$indigoA8',
-    secondary9: '$indigoA9',
-    secondary10: '$indigoA10',
-    secondary11: '$indigoA11',
-    secondary12: '$indigoA12',
-
-    //Gray
-    gray1: '$slate1',
-    gray2: '$slate2',
-    gray3: '$slate3',
-    gray4: '$slate4',
-    gray5: '$slate5',
-    gray6: '$slate6',
-    gray7: '$slate7',
-    gray8: '$slate8',
-    gray9: '$slate9',
-    gray10: '$slate10',
-    gray11: '$slate11',
-    gray12: '$slate12',
-
-    accent: '#2517FF',
-
-    neutralBgSubtle: '$gray3',
-    neutralBg: '$gray1',
-
-    panelBg: '$gray3',
-    panelBorder: '$slate7',
-    panelShadow: 'transparent',
-    dropdownBg: '$gray3',
-    sidebarOverlay: 'black',
-  },
-})
+export default MyDocument
