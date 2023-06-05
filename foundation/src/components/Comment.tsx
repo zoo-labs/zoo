778cd975678cad3959f3a63b03859049e6abc4a9
woobin <woobinpro@gmail.com>
2023-06-04 22:48:40 -0700

    fixed github issues

--- foundation/src/components/Comment.tsx
+++ foundation/src/components/Comment.tsx
@@ -32,8 +32,8 @@ function Comment() {
       {comments.map((data, index) => (
         <div className='max-md:w-full flex flex-col  w-1/4 px-5'>
             <h1 className='text-white md:text-3xl xl:text-5xl max-md:text-2xl'>{data.title}</h1>
-            <p className='text-white md:text-md lg:text-lg xl:text-2xl py-10 max-md:py-4'>{data.comment}</p>
-            <Link href={data.href} className='flex items-center text-white md:text-md lg:text-lg xl:text-2xl max-md:pb-10'>
+            <p className='text-white md:text-md 2xl:min-h-[220px] xl:min-h-[240px] md:min-h-[225px] lg:text-lg xl:text-2xl py-10 max-md:py-4'>{data.comment}</p>
+            <Link href={data.href} className='flex items-center min-h-[56px] text-white md:text-md lg:text-lg xl:text-2xl max-md:pb-10'>
                 <span className='pr-[15px]'>{data.link}</span>
                 <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M6 5.5L1.19924 10.5L0 9.24901L3.59962 5.5L6.08905e-06 1.751L1.19924 0.5L6 5.5Z" fill="white"/>
--- foundation/src/components/intro/Intro.tsx
+++ foundation/src/components/intro/Intro.tsx
@@ -18,7 +18,7 @@ function Intro({breadcrumbs,title,comment}: {
             <p className='text-white max-md:hidden pr-20 pt-16 2xl:pt-18 pb-8 md:text-sm md:pb-20 lg:text-lg lg:pb-24 xl:text-2xl xl:pb-32'>Supporting endangered species via human endeavors and virtual animal sales.</p>
         </div>
       </div>
-      <div className='max-md:flex hidden justify-between pt-20 z-10'>
+      <div className='max-md:flex hidden justify-between pt-10 z-10'>
         <div className='w-1/2 max-md:w-full max-md:z-10 max-md:pt-[280px] flex flex-col justify-between  max-md:pl-15 max-md:pr-15 pl-10 md:pl-32 pr-8'>
             <p className='text-white md:text-sm lg:text-lg xl:text-2xl'>{breadcrumbs}</p>
             <h1 className='text-white md:text-7xl xl:text-9xl max-md:text-5xl max-md:my-5'>{title}</h1>
@@ -26,14 +26,17 @@ function Intro({breadcrumbs,title,comment}: {
             <hr className='max-md:hidden'/>
             <p className='text-white max-md:hidden pb-8 md:text-sm md:pb-20 lg:text-lg lg:pb-24 xl:text-2xl xl:pb-32'>Supporting endangered species via human endeavors and virtual animal sales.</p>
         </div>
-        <div className='md:w-1/2 max-md:absolute max-md:w-4/5 max-md:right-0 max-md:z-0'>
-            <Image
+        <div className='md:w-1/2 max-md:absolute max-md:w-[800px] max-md:right-0 max-md:z-0'>
+            {/* <Image
                 className='intro-bg float-right'
                 src='/images/intro_bg.png'
                 width='1000'
                 height='1000'
                 alt=''
-            />
+            /> */}
+            <video src="/videos/Giraffe_teen.webm" autoPlay loop className="w-full">
+              
+            </video>
         </div>
       </div>
       
