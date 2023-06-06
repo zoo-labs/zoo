f6bed1a20183162e52f6b912067a345fd711b6a6
zooqueen <antje@lux.finance>
2023-06-05 18:35:03 -0700

    another word

--- foundation/src/components/animals/Carousel.tsx
+++ foundation/src/components/animals/Carousel.tsx
@@ -21,7 +21,7 @@ function Carosuel() {
     <div className="bg-black text-center xl:px-52 lg:px-32 md:px-16 px-6 pt-32">
         <p className='text-gray-300 text-lg md:text-center text-left'>3D Metaverse Animals</p>
         <h1 className='text-white md:text-center text-left md:text-3xl xl:text-6xl max-md:text-2xl mt-5 mb-8'>Emotional Intelligence with AI</h1>
-        <p className='text-gray-300 text-sm md:text-center text-left xl:text-xl pb-16 xl:px-52 lg:px-32 md:px-16'>The future upgrades will have powers, allow them to speak with you. play with you and much more to come!</p>
+        <p className='text-gray-300 text-sm md:text-center text-left xl:text-xl pb-16 xl:px-52 lg:px-32 md:px-16'>The future upgrades will have powers, allow them to speak with you. play with you and much more for you to decide!</p>
         <Slider {...settings} className='w-1/2 max-md:w-full m-auto p-8 border rounded-xl border-white mb-20'>
 
           {animals.map((data, index) => (
