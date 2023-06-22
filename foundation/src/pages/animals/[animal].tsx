f2785dc12b952e8f3994918ea32839a7e4dd7284
woobin <woobinpro@gmail.com>
2023-06-21 18:24:38 -0700

    Changed the donation&collect pages

--- foundation/src/pages/animals/[animal].tsx
+++ foundation/src/pages/animals/[animal].tsx
@@ -12,7 +12,9 @@ import Content from '@/components/animals/Content';
 import FutureUpgrades from '@/components/FutureUpgrades';
 import CoreHeader from '@/components/animals/CoreHeader';
 import Carousel from '@/components/animals/Carousel';
+import { Elements } from '@stripe/react-stripe-js';
 import animals from "@/components/animals/animals.json";
+import { loadStripe } from '@stripe/stripe-js';
 
 import NotFoundPage from "../404";
 
@@ -21,6 +23,7 @@ export default function AnimalPage() {
   const [animalRoute, setAnimalRoute] = useState(router.query.animal);
   const [animal, setAnimal] = useState(animals.find((animal) => animal.route === router.query.animal));
   const [animal_index, setAnimalIndex] = useState(animals.findIndex(p => p.route == router.query.animal));
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY || '');
   React.useEffect(() => {
     if (router.isReady) {
       setAnimalRoute(router.query.animal);
@@ -34,9 +37,11 @@ export default function AnimalPage() {
         { animal == undefined ? <NotFoundPage /> : <>
         <Seo />
         <Navbar />
+        <Elements stripe={stripePromise}>
         {/* <CoreHeader index={animal_index}/>
         <FutureUpgrades /> */}
-        <Header title={animal.name} content={animal.description.head} front={animal.card_front} back={animal.card_back} front_m={animal.card_front_mp4} back_m={animal.card_back_mp4}/>
+        <Header title={animal.name} content={animal.description.head} front={animal.card_front} back={animal.card_back} front_m={animal.card_front_mp4} back_m={animal.card_back_mp4} route={animal.route}/>
+        </Elements>
         <Avatars list={animal.avatars} linkFlag={false}/>
         <Content title={animal.description.subtitle} content={animal.description.desc} />
         <Carousel />
