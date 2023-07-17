3ba27144a5b04d05cb8e7861127b0d57e4656e2c
zooqueen <antje@lux.finance>
2023-07-16 19:07:12 -0700

    Fix various bugs

--- core/state/application/updater.ts
+++ core/state/application/updater.ts
@@ -19,50 +19,5 @@ export default function Updater(): null {
     blockNumber: null,
   })
 
-  const blockNumberCallback = useCallback(
-    (blockNumber: number) => {
-      setState((state) => {
-        if (chainId === state.chainId) {
-          if (typeof state.blockNumber !== 'number') return { chainId, blockNumber }
-          return {
-            chainId,
-            blockNumber: Math.max(blockNumber, state.blockNumber),
-          }
-        }
-        return state
-      })
-    },
-    [chainId, setState]
-  )
-
-  // attach/detach listeners
-  useEffect(() => {
-    if (!library || !chainId || !windowVisible) return undefined
-
-    setState({ chainId, blockNumber: null })
-
-    library
-      .getBlockNumber()
-      .then(blockNumberCallback)
-      .catch((error) => console.error(`Failed to get block number for chainId: ${chainId}`, error))
-
-    library.on('block', blockNumberCallback)
-    return () => {
-      library.removeListener('block', blockNumberCallback)
-    }
-  }, [dispatch, chainId, library, blockNumberCallback, windowVisible])
-
-  const debouncedState = useDebounce(state, 100)
-
-  useEffect(() => {
-    if (!debouncedState.chainId || !debouncedState.blockNumber || !windowVisible) return
-    dispatch(
-      updateBlockNumber({
-        chainId: debouncedState.chainId,
-        blockNumber: debouncedState.blockNumber,
-      })
-    )
-  }, [windowVisible, dispatch, debouncedState.blockNumber, debouncedState.chainId])
-
   return null
 }
