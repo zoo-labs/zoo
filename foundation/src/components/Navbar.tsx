5ed88a420a6f4e1723c73353c2c5e7abb915cf22
Zach Kelling <z@zeekay.io>
2025-08-16 16:13:57 -0500

    Update navigation menu: Change Wanderlust to Connect and reorder items
    - Renamed 'Wanderlust' menu item to 'Connect'
    - Reordered menu items to: Animals, Connect, Get Involved, Mission
    - Applied changes to both desktop and mobile navigation

--- foundation/src/components/Navbar.tsx
+++ foundation/src/components/Navbar.tsx
@@ -64,16 +64,10 @@ function Navbar() {
                       Animals
                   </Link>
                   <Link
-                      href="/about"
-                      className="text-gray-300  hover:text-white px-3 py-2  text-md font-medium"
-                  >
-                      Mission
-                  </Link>
-                  <Link
                       href="/experiences"
                       className="text-gray-300  hover:text-white px-3 py-2  text-md font-medium"
                   >
-                      Wanderlust
+                      Connect
                   </Link>
                   <Link
                       href="/getinvolved"
@@ -82,6 +76,12 @@ function Navbar() {
                     <>
                       Get Involved
                     </>
+                  </Link>
+                  <Link
+                      href="/about"
+                      className="text-gray-300  hover:text-white px-3 py-2  text-md font-medium"
+                  >
+                      Mission
                   </Link>
                   {/* Add more links here */}
                   </div>
@@ -105,18 +105,11 @@ function Navbar() {
               >
                 Animals
               </Link>
-              
-              <Link
-                href="/about"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Mission
-              </Link>
               <Link
                 href="/experiences"
                 className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
               >
-                Wanderlust
+                Connect
               </Link>
               <Link
                 href="/getinvolved"
@@ -125,6 +118,12 @@ function Navbar() {
                 <>
                   Get Involved
                 </>
+              </Link>
+              <Link
+                href="/about"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Mission
               </Link>
               <Link
                 href="/donation"
